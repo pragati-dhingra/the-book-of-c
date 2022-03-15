2bcbfa8c1005e9c9835e15e1004e003ad014a8ed
umangrastogig <101452375+umangrastogig@users.noreply.github.com>
2022-03-15 16:28:42 +0530

    Update pg095_q002.c

--- pg095_q002.c
+++ pg095_q002.c
@@ -8,6 +8,27 @@ int main (int argc, char* argv[] ){
   * Output: One of the following depending on input value
   *         Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Incorrect input 
   * Note that output is case sensitive */
-  
+  int day_of_week=0;
+	printf("Enter the day of week in number\n");
+	scanf("%d", &day_of_week);
+	switch (day_of_week)
+	{
+	case 1: printf("\nMonday");
+		break;
+	case 2: printf("\nTuesday");
+		break;
+	case 3: printf("\nWednesday");
+		break;
+	case 4: printf("\nThursday");
+		break;
+	case 5: printf("\nFriday");
+		break;
+	case 6: printf("\nSaturday");
+		break;
+	case 7: printf("\nSunday");
+		break;
+	default: printf("\nIncorrect input");
+		break;
+	}
   return 0
 }
