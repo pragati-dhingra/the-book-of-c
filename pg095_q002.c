aa157020af29a7a331de9b056cf8832c0dccd012
pragati-dhingra <38684074+pragati-dhingra@users.noreply.github.com>
2022-03-15 06:35:03 +0530

    Print day of the week

---
+++ pg095_q002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[] ){
+  /* Write a program using switch case that takes day of the week (1-7) as user input and prints the name of the day.
+  * E.g. Monday for 1, Tuesday for 2, and so on. 
+  * In case user input is outside the range 1-7, program should print an error message using default case. 
+  * Input: one integer value. You are not expected to check for non-integer values.
+  * Output: One of the following depending on input value
+  *         Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Incorrect input 
+  * Note that output is case sensitive */
+  
+  return 0
+}
