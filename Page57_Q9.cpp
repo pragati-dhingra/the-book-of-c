56a7b4c9a594ef1a89630a02de077aea9e072b88
Sonika-Nagar <101444180+Sonika-Nagar@users.noreply.github.com>
2022-03-15 15:49:23 +0530

    Add files via upload

---
+++ Page57_Q9.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(int arg, char * argv[]){
+	int num1=0, num2=0, i;
+ 	printf("Enter 1st positive number:");
+	scanf("%d",&num1);
+	printf("Enter 2nd positive number:");
+	scanf("%d",&num2);
+	if(num1%num2==0){
+ 		printf("num1 is a factor of num2");
+	}
+	else{
+ 		printf("num1 is a factor of num2");
+	}
+return 0;
+}
---
+++ area of circle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(int argc,char* argv[])
+{
+	double area=0, radius=2, cir=0;
+	cir=2*3.14*radius;
+	area=3.14*radius*radius;
+	printf("area= %f cm\n",area);
+	printf("Circumferenec= %f cm\n",cir);	
+
+	return(0);	
+}
