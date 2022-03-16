0d9b802aabcd5ba2d822854ad8ee1e1bb0590661
pragya-agarwal <71170692+pragya-agarwal@users.noreply.github.com>
2022-03-16 13:34:03 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -5,7 +5,13 @@ int main (int argc, char* argv[] ){
    * Input one double value followed by a newline
    * Output one double value with two decimal places on a line by itself - no newline.
    * Do not print any extra strings/prompts */
-  
-  
+  double inch,cm;
+		printf("Enter length in Inch:\n");
+		scanf("%lf", &inch);
+		
+
+		cm = inch * 2.54;
+		printf("Equivalent length in Centimeters = %0.2lf", cm);
+		  
   return 0;
 }
