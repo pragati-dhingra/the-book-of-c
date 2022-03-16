040e4141b904b42eb9c850a740a78a697d0d90e7
kanikachauhan20 <101620676+kanikachauhan20@users.noreply.github.com>
2022-03-16 19:33:44 +0530

    Update pg095_q002.c

--- pg095_q002.c
+++ pg095_q002.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[] ){
+  
+    int week;
+  printf("Enter week number(1-7): ");
+    scanf("%d", &week);
+    
+    switch(week)
+    {
+        case 1: 
+            printf("Monday");
+            break;
+        case 2: 
+            printf("Tuesday");
+            break;
+        case 3: 
+            printf("Wednesday");
+            break;
+        case 4: 
+            printf("Thursday");
+            break;
+        case 5: 
+            printf("Friday");
+            break;
+        case 6: 
+            printf("Saturday");
+            break;
+        case 7: 
+            printf("Sunday");
+            break;
+        default: 
+            printf("Invalid input! Please enter week number between 1-7.");
+    }
   /* Write a program using switch case that takes day of the week (1-7) as user input and prints the name of the day.
   * E.g. Monday for 1, Tuesday for 2, and so on. 
   * In case user input is outside the range 1-7, program should print an error message using default case. 
