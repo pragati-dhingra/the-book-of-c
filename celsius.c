a03e1a0093accca0a39f6ee5044beb782188be9f
ShipraGautam <101443814+ShipraGautam@users.noreply.github.com>
2022-03-23 15:20:06 +0530

    Delete celsius.c

--- celsius.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-    int main(int argc, char * argv[])
-    {
-    	double celsius=42.2, fahrenheit=0;
-    	fahrenheit=((celsius * 9)/5) + 32;
-    	printf(" The value of %f celsius in fahrenheit is %f \n", celsius, fahrenheit);
-    	return 0;
-	}
