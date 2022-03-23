691f09c4ffd3e76e03ae69870586573384a0dbfc
SwatiGoel123 <101451657+SwatiGoel123@users.noreply.github.com>
2022-03-23 15:35:59 +0530

    Update pg095_q002.c

--- pg095_q002.c
+++ pg095_q002.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main (int argc, char* argv[] ){
   /* Write a program using switch case that takes day of the week (1-7) as user input and prints the name of the day.
   * E.g. Monday for 1, Tuesday for 2, and so on. 
@@ -8,6 +7,25 @@ int main (int argc, char* argv[] ){
   * Output: One of the following depending on input value
   *         Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Incorrect input 
   * Note that output is case sensitive */
-  
-  return 0
+  int day_of_week =0;
+  scanf("%d",&day_of_week);
+   switch(day_of_week)
+  {
+    case 1: printf("Monday");
+            break;
+    case 2: printf("Tuesday");
+            break;
+    case 3: printf("Wednesday");
+            break;
+    case 4: printf("Thursday");
+            break;
+    case 5: printf("Friday");
+            break;
+    case 6: printf("Saturday");
+            break;
+    case 7: printf("Sunday");
+            break; 
+    default: printf("Incorrect input");
+   }
+ return 0;
 }
