5f9831ca5c6bbe8bd249f16c038cfc7ea935f020
kanikachauhan20 <101620676+kanikachauhan20@users.noreply.github.com>
2022-03-23 15:52:30 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
+
   /* Q: Read value in inches and converts it into cms. 1 inch = 2.54 cms 
    * Input one double value followed by a newline
    * Output one double value with two decimal places on a line by itself - no newline.
    * Do not print any extra strings/prompts */
-  
-  
+
+#include<conio.h>
+int main()
+{
+    float inch, cm;
+    printf("Enter length in Inch: ");
+    scanf("%f", &inch);
+    cm = inch * 2.54;
+    printf("Equivalent length in Centimeters = %0.2f", cm);
+    getch();
+
   return 0;
 }
