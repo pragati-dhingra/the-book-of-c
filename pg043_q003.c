342fb8be291175a0aeb3fdc85601b384a8f47705
Sonika-Nagar <101444180+Sonika-Nagar@users.noreply.github.com>
2022-03-23 15:24:42 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -5,6 +5,7 @@ int main (int argc, char* argv[] ){
    * Input one double value followed by a newline
    * Output one double value with two decimal places on a line by itself - no newline.
    * Do not print any extra strings/prompts */
+  
   double height_inch=0;
   double height_cm=0;
   printf("Enter Height:");
