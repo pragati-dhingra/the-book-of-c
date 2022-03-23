8dbeaaa7e0df240968255f05bf89ef0b718250a2
ShivaliTyagi <101444334+ShivaliTyagi@users.noreply.github.com>
2022-03-23 18:52:50 +0530

    Update pg098_q006.c

--- pg098_q006.c
+++ pg098_q006.c
@@ -19,5 +19,35 @@ There should be no newlines after the printed output
 Winners post 2015 are not required.
 */
   
+  int year_cwc = 0;
+	scanf("%d", &year_cwc);
+	switch (year_cwc)
+	{
+	case 1975: printf("West Indies");
+		break;
+	case 1979: printf("West Indies");
+		break;
+	case 1983: printf("India");
+		break;
+	case 1987: printf("Australia");
+		break;
+	case 1992: printf("Pakistan");
+		break;
+	case 1996: printf("Sri Lanka");
+		break;
+	case 1999: printf("Australia");
+		break;
+	case 2003: printf("Australia");
+		break;
+	case 2007: printf("Australia");
+		break;
+	case 2011: printf("India");
+		break;
+	case 2015: printf("Australia");
+		break;
+	default: printf("Invalid input");
+		break;
+	}
+
   return 0
 }
