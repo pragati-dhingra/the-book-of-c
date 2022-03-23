ececbd61d302dc9b4ca0f67f9fc8e0c16474198d
kanikachauhan20 <101620676+kanikachauhan20@users.noreply.github.com>
2022-03-23 16:18:31 +0530

    Update pg001_q001.c

--- pg001_q001.c
+++ pg001_q001.c
@@ -5,6 +5,12 @@ int main (int argc, char* argv[] ){
    * Update this program to print "Hello World!" without the quotes
    * Once updated, submit a pull request. 
    * You should receive a comment with results on your PR and email notification (unless you have disabled notifications) */
+  double inch=0,cm=0;
+	//printf("enter value in inches");
+	scanf("%lf",&inch);
+	cm=inch*2.54;
+	printf("%.2lf",cm);
+  
   
   return 0
 }
