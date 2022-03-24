67f358eb2a45bbc1cd42783f74f4e620494c6761
harshvardhan2520 <101621531+harshvardhan2520@users.noreply.github.com>
2022-03-24 10:39:14 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[] ){
+int month;
+scanf("%d", &month);
+switch(mno) {
+		case 1 : printf("January\n"); break;
+		case 2 : printf("February\n"); break;
+		case 3 : printf("March\n"); break;
+		case 4 : printf("April\n"); break;
+		case 5 : printf("May\n"); break;
+		case 6 : printf("June\n"); break;
+		case 7 : printf("July\n"); break;
+		case 8 : printf("August\n"); break;
+		case 9 : printf("September\n"); break;
+		case 10 : printf("October\n"); break;
+		case 11 : printf("November\n"); break;
+		case 12 : printf("December\n"); break;
+		default : printf("Input a number between 1 to 12.");
+	}
   /* Write a program that reads the month number (1-12) and prints its equivalent name (January for 1, February for 2, and so on).
   Input: One integer on a line, e.g. 4
   Output: Correponding name of month, e.g. April
