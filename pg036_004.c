b01081c722fc6992f1df7fc5987ad4961d72b781
Dr. Amit Kumar <65865744+awadheshamit@users.noreply.github.com>
2022-03-24 09:35:01 +0530

    Add files via upload

---
+++ pg036_004.c
@@ -0,0 +1,13 @@
+/*Program to swap contents of variable*/
+#include<stdio.h>
+void main()
+{
+    int x=0,y=0, sum=0;
+    x=40;
+    y=60;
+    sum=x+y;
+    
+    printf("Sum=%d", x,y);
+    return;
+
+}
