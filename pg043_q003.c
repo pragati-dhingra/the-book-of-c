814eb3281d5ea574756061437e5f92528e940c1e
Sonika-Nagar <101444180+Sonika-Nagar@users.noreply.github.com>
2022-03-24 13:31:01 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+
 int main (int argc, char* argv[] ){
+  /* Q: Read value in inches and converts it into cms. 1 inch = 2.54 cms 
+   * Input one double value followed by a newline
+   * Output one double value with two decimal places on a line by itself - no newline.
+   * Do not print any extra strings/prompts */
   double height_inch=0;
   double height_cm=0;
   printf("Enter Height:");
