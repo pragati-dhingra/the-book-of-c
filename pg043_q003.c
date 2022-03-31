1cd1cc615da0493fd791c7c516dea808e585fb7b
Sonika-Nagar <101444180+Sonika-Nagar@users.noreply.github.com>
2022-03-31 10:18:33 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -7,7 +7,6 @@ int main (int argc, char* argv[] ){
    * Do not print any extra strings/prompts */
   double height_inch=0;
   double height_cm=0;
-  printf("Enter Height:");
   scanf("%lf",&height_inch);
   height_cm=height_inch*2.54;
   printf("Height in cm= %f\n",height_cm);
