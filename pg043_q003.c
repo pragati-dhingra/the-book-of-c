61549bfbab1d76d67031a58c4679fa6a68dce834
VinaySingh13 <101443086+VinaySingh13@users.noreply.github.com>
2022-04-01 11:48:03 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 int main (int argc, char* argv[] ){
-  /* Q: Read value in inches and converts it into cms. 1 inch = 2.54 cms 
+  /* Q: Read value in inches and converts it into cms. 1 inch = 2.54 cms
    * Input one double value followed by a newline
    * Output one double value with two decimal places on a line by itself - no newline.
    * Do not print any extra strings/prompts */
-  
-  
-  return 0;
+  double inch=0, cm=0;
+    scanf("%lf", &inch);
+    cm = inch * 2.54;
+    printf("%.2f", cm);
+   return 0;
 }
