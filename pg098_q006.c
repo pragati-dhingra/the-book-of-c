5d14bcb19c2aa1e0be433a96670c425275742ff9
sarikatyagi26 <101451781+sarikatyagi26@users.noreply.github.com>
2022-04-01 14:07:24 +0530

    Update pg098_q006.c

--- pg098_q006.c
+++ pg098_q006.c
@@ -18,6 +18,47 @@ If world cup was not played in the year entered by the user, program should prin
 There should be no newlines after the printed output
 Winners post 2015 are not required.
 */
+  int year = 0;
+    printf("Enter year in which World cup was held : ");
+    scanf("%d",&year);
+ 
+    switch(year) {
+        case 1975:
+            printf("West Indies won the world cup in 1975");
+            break;
+        case 1979:
+            printf("West Indies won the world cup in 1979");
+            break;
+        case 1983:
+            printf("India won the world cup in 1983");
+            break;
+        case 1987:
+            printf("Australia won the world cup in 1987");
+            break;
+        case 1992:
+            printf("Pakistan won the world cup in 1992");
+            break;
+        case 1996:
+            printf("Sri Lanka won the world cup in 1996");
+            break;
+        case 1999:
+            printf("Australia won the world cup in 1999");
+            break;
+        case 2003:
+            printf("Australia won the world cup in 2003");
+            break;
+        case 2007:
+            printf("Australia won the world cup in 2007");
+            break;
+        case 2011:
+            printf("India won the world cup in 2011");
+            break;
+        case 2015:
+            printf("Australia won the world cup in 2015");
+            break;
+        default:
+            printf("There was no world cup played in %d", year);
+    }
   
-  return 0
+  return 0;
 }
