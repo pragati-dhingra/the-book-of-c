f992b8c1e71b2ded04adbf78b4e882a74f672c2e
sandeepkec <101444259+sandeepkec@users.noreply.github.com>
2022-04-01 14:39:18 +0530

    Update pg081_q015.c

--- pg081_q015.c
+++ pg081_q015.c
@@ -11,21 +11,20 @@ int main (int argc, char* argv[] ){
   
   int month = 0, day = 0;
   
-  scanf("%d%d", &month ,&day);
-   if ( month < 1 || month > 12 || day < 1 || day > 7) {
+  scanf("%d %d", &month ,&day);
+   if ( month < 1|| month > 12|| day<1||day > 7) {
         printf("-1");
     } else {
         if (2==month) {
             printf("Month has 4 Sundays");
         } else {
-            if ((1==month|| 3==month|| 5==month|| 7==month ||
-                8==month || 10==month || 12==month)) {
-                if((5==day || 6==day || 7==day)) {
+            if ((1==month||3==month||5==month||7==month||8==month||10==month||12==month)) {
+                if((5==day||6==day||7==day)) {
                     printf("Month has 5 Sundays");
                 } else {
                     printf("Month has 4 Sundays");
                 }
-            } else if (6==day || 7==day) {
+            } else if (6==day||7==day) {
                 printf("Month has 5 Sundays");
             } else {
                 printf("Month has 4 Sundays");
