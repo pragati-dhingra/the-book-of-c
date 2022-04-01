51c879c8951e684d788e3f6bca42711d97e1a614
anujkec <101444151+anujkec@users.noreply.github.com>
2022-04-01 14:28:51 +0530

    Update pg095_q003.c

--- pg095_q003.c
+++ pg095_q003.c
@@ -19,41 +19,40 @@ There should be no newlines after the printed output
 Winners post 2015 are not required.
 */
   int year = 0;
-    printf("Enter year of world cup: ");
     scanf("%d",&year);
      switch(year) {
         case 1975:
-            printf("West Indies won the world cup in 1975");
+            printf("West Indies");
             break;
         case 1979:
-            printf("West Indies won the world cup in 1979");
+            printf("West Indies");
             break;
         case 1983:
-            printf("India won the world cup in 1983");
+            printf("India");
             break;
         case 1987:
-            printf("Australia won the world cup in 1987");
+            printf("Australia");
             break;
         case 1992:
-            printf("Pakistan won the world cup in 1992");
+            printf("Pakistan");
             break;
         case 1996:
-            printf("Sri Lanka won the world cup in 1996");
+            printf("Sri Lanka");
             break;
         case 1999:
-            printf("Australia won the world cup in 1999");
+            printf("Australia");
             break;
         case 2003:
-            printf("Australia won the world cup in 2003");
+            printf("Australia");
             break;
         case 2007:
-            printf("Australia won the world cup in 2007");
+            printf("Australia");
             break;
         case 2011:
-            printf("India won the world cup in 2011");
+            printf("India");
             break;
         case 2015:
-            printf("Australia won the world cup in 2015");
+            printf("Australia");
             break;
         default:
             printf("Invalid input");
