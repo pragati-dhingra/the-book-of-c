572977a419087fcc1441399acad7356406c69d67
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-04-01 17:57:45 +0530

    Update pg095_q002.c

--- pg095_q002.c
+++ pg095_q002.c
@@ -1,41 +1,32 @@
 #include <stdio.h>
-int main (int argc, char* argv[] ){
+
+int main (int argc, char* argv[] ) {
   /* Write a program using switch case that takes day of the week (1-7) as user input and prints the name of the day.
   * E.g. Monday for 1, Tuesday for 2, and so on. 
   * In case user input is outside the range 1-7, program should print an error message using default case. 
   * Input: one integer value. You are not expected to check for non-integer values.
   * Output: One of the following depending on input value
   *         Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Incorrect input 
-  * Note that output is case sensitive. There should be no newline at the end of input. 
-  * For example, if input was 1, output should be simply Monday */
-    int Day=0;
-    printf("Enter day of week (1 to 7): ");
-    scanf("%d",&Day);
- 
-    switch(Day) {
-        case 1:
-            printf("Monday\n");
+  * Note that output is case sensitive */
+  int day=0;
+  scanf("%d",&day);
+  switch(day)
+  {
+    case 1: printf("Monday");
             break;
-        case 2:
-            printf("Tuesday\n");
+    case 2: printf("Tuesday");
             break;
-        case 3:
-            printf("Wednesday\n");
+    case 3: printf("Wednesday");
             break;
-        case 4:
-            printf("Thursday\n");
+    case 4: printf("Thursday");
             break;
-        case 5:
-            printf("Friday\n");
+    case 5: printf("Friday");
             break;
-        case 6:
-            printf("Saturday\n");
+    case 6: printf("Saturday");
             break;
-        case 7:
-            printf("Sunday\n");
-            break;
-        default:
-            printf("Invalid input\n");
-    }
-    return 0;
-}
+    case 7: printf("Sunday");
+            break; 
+    default: printf("Incorrect input");
+   }
+ return 0;
+ }
