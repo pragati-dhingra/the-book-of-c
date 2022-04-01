4302c097aebebb6847e5fa5f17726cc9b192c835
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-04-01 17:55:21 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
+int main (int argc, char* argv[] ) {
   /* Write a program that reads the month number (1-12) and prints its equivalent name (January for 1, February for 2, and so on).
   Input: One integer on a line, e.g. 4
   Output: Correponding name of month, e.g. April
   Please do not use abbreviations. First character is capital, rest are small case,  no trailing newline
   In case of invalid input, output should be "Invalid" (without quotes) 
   Output is case sensitive */
-    int month = 0;
+  int month = 0;
  
-    printf("Enter month: ");
     scanf("%d", &month);
- 
-    if(month < 1 || month > 12) {
-        printf("Invalid input\n");
-    } else if(month == 1){
-        printf("January\n");
+    if(month == 1){
+        printf("January");
     }else if(month == 2){
-        printf("February\n");
+        printf("February");
     }else if(month == 3){
-        printf("March\n");
+        printf("March");
     }else if(month == 4){
-        printf("April\n");
+        printf("April");
     }else if(month == 5){
-        printf("May\n");
+        printf("May");
     }else if(month == 6){
-        printf("June\n");
+        printf("June");
     }else if(month == 7){
-        printf("July\n");
+        printf("July");
     }else if(month == 8){
-        printf("August\n");
+        printf("August");
     }else if(month == 9){
-        printf("September\n");
+        printf("September");
     }else if(month == 10){
-        printf("October\n");
+        printf("October");
     }else if(month == 11){
-        printf("November\n");
+        printf("November");
     }else if(month == 12){
-        printf("December\n");
-    }
- 
-    return 0;
-}
-
+        printf("December");
+    }else if(month < 1 || month > 12) {
+        printf("Invalid");
+ 	  }
+ }
