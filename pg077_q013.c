419c44f2d77b8d91027b3e2b78953602d31132b0
anujkec <101444151+anujkec@users.noreply.github.com>
2022-04-01 13:47:51 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -14,27 +14,27 @@ int main (int argc, char* argv[] ){
         printf("Invalid");
     } else if(month == 1){
         printf("January");
-    }else if(month == 2){
+    }else if(month ==2){
         printf("February");
-    }else if(month == 3){
+    }else if(month ==3){
         printf("March");
-    }else if(month == 4){
+    }else if(month ==4){
         printf("April");
-    }else if(month == 5){
+    }else if(month ==5){
         printf("May");
-    }else if(month == 6){
+    }else if(month ==6){
         printf("June");
-    }else if(month == 7){
+    }else if(month ==7){
         printf("July");
-    }else if(month == 8){
+    }else if(month ==8){
         printf("August");
-    }else if(month == 9){
+    }else if(month ==9){
         printf("September");
-    }else if(month == 10){
+    }else if(month ==10){
         printf("October");
-    }else if(month == 11){
+    }else if(month ==11){
         printf("November");
-    }else if(month == 12){
+    }else if(month ==12){
         printf("December");
     }
  
