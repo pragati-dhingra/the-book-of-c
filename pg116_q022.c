883183be94a49700b856a64cc118a0db89f468b4
rashmisharma3011 <101451636+rashmisharma3011@users.noreply.github.com>
2022-04-02 16:31:46 +0530

    Update pg116_q022.c

--- pg116_q022.c
+++ pg116_q022.c
@@ -9,6 +9,27 @@ int main (int argc, char* argv[] ){
    * Output --> result of computation with no trailing newline 
    * No error checking is expected. Operands are int. Handle 4 arithmetic operators + - * /
    * Only integer calculator is expected. Do not create float calculator */
-  
+  int operand1 = 0, operand2 = 0;
+  char operator = 0;
+  printf("Enter operator: ");
+  scanf("%c", &operator);
+  printf("Enter first operand: ");
+  scanf("%d", &operand1);
+  printf("Enter second operand: ");
+  scanf("%d", &operand2);
+  switch(operator) {
+    case '+':
+      printf("%d", operand1 + operand2);
+      break;
+       case '-':
+      printf("%d", operand1 - operand2);
+      break;
+       case '*':
+      printf("%d", operand1 * operand2);
+      break;
+       case '/':
+      printf("%d", operand1 / operand2);
+      break;
+  } 
   return 0;
 }
