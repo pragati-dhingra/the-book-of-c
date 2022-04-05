cbf8784f3d4e87a179106fb153597b7e4c2a2c9c
ShivaniKakria <101454020+ShivaniKakria@users.noreply.github.com>
2022-04-05 14:31:48 +0530

    Update pg108_q012.c

--- pg108_q012.c
+++ pg108_q012.c
@@ -6,10 +6,16 @@ int main(int argc, char * argv[]) {
     /* Add checks for both percent and average to be in 0 to 100 range, both inclusive 
      * If found out of range then print "Invalid input" and skip further processing */
 
-    if (percent > average) {
-        printf("Qualify");
+    if (memory_used < 0 || memory_used > phone_memory) {
+        printf("Invalid input");
     } else {
-        printf("Oops");
+        percent_used = (memory_used*100.0)/phone_memory;
+ 
+        if (percent_used > 90) {
+            printf("Running out of storage, cleanup now\n");
+        } else {
+            printf("Still have %ld bytes free",phone_memory-memory_used);
+        }
     }
     return 0;
 }
