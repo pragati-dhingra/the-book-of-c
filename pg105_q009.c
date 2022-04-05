046980211fecde4c0df999366b98c20c34ca4e30
ShipraGautam <101443814+ShipraGautam@users.noreply.github.com>
2022-04-05 14:23:25 +0530

    Update pg105_q009.c

--- pg105_q009.c
+++ pg105_q009.c
@@ -9,16 +9,16 @@ int main (int argc, char* argv[] ){
     /* Add check: if user input was less than 0 or it was more than phone_memory then print "Invalid input" */
     /* In case of invalid input, rest of processing should be skipped */
   
-    percent_used = (memory_used*100.0)/phone_memory;
-
-    if (percent_used > 90) {
-        /* Print Out of memory message */
-        printf("OOM");
-    } else {
-        /* Print number of free bytes */
-        printf("%ld",phone_memory-memory_used);
-    }
-    
-  
+    if (memory_used < 0 || memory_used > phone_memory) {
+        printf("Invalid input");
+    } /*else {
+        percent_used = (memory_used*100.0)/phone_memory;*/
+ 
+        if (percent_used > 90) {
+            printf("Running out of storage, cleanup now\n");
+        } else {
+            printf("Still have %ld bytes free",phone_memory-memory_used);
+        }
+   
     return 0;
 }
