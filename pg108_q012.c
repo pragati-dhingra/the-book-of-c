4b00e1232e73768fac80f6a4d5d565913bea6065
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-04-05 10:49:23 +0530

    Update pg108_q012.c

--- pg108_q012.c
+++ pg108_q012.c
@@ -1,7 +1,16 @@
+#include <stdio.h>
+ 
 int main(int argc, char * argv[]) {
     float percent = 0, average = 0;
-    
-    scanf("%f %f",&percent, &average);
+    printf("Enter your percent score: ");
+    scanf("%f",&percent);
+    printf("Enter class average score: ");
+    scanf("%f",&average);
+ 
+    if (percent < 0 || percent > 100 || average < 0 || average > 100) {
+        printf("Invalid input\n");
+    } else {
+
     
     /* Add checks for both percent and average to be in 0 to 100 range, both inclusive 
      * If found out of range then print "Invalid input" and skip further processing */
