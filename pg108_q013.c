3b0b0f720bdf4ce7db43c55f786bf0bc01bff621
ShipraGautam <101443814+ShipraGautam@users.noreply.github.com>
2022-04-05 16:04:15 +0530

    Update pg108_q013.c

--- pg108_q013.c
+++ pg108_q013.c
@@ -11,7 +11,7 @@ int main(int argc, char * argv[]) {
         printf("Invalid input");
     } else 
          {
-        if (percent > average && percent > 80) {
+        if (percent > average && percent >= 80) {
             printf("Qualify");
         } else {
             printf("Not qualify");
