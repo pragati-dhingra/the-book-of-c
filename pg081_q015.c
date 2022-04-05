84a035cfd01751da790f97a3b4a104e933485306
Singhal-Shaili <95026201+Singhal-Shaili@users.noreply.github.com>
2022-04-05 14:14:42 +0530

    Update pg081_q015.c

--- pg081_q015.c
+++ pg081_q015.c
@@ -8,25 +8,31 @@ int main (int argc, char* argv[] ){
           Second integer is day of week, 1 for Monday, 2 for Tuesday ...
   Output: One integer showing number of sundays in that month. No newline to be printed
           In case of invalid input, print -1 as output */
- int month = 0, date = 0, day = 0;
-    
-    scanf("%d %d", &month, &day);
-   if ( month < 1 || month > 12 ||
-             day < 1 || day > 7) {
-        printf("-1");
-   } else {
-         if (2 == month) {
-            printf("4");
-         } else {
-             if ((1 == month|| 3 == month||
-                     5 == month|| 7 == month ||
-                     8 == month || 10 == month ||
-                   12 == month) && (5 == day ||
-                            6 == day || 7 == day)) {
-                 printf("5");
-             }
-        }
-     }
 
+    scanf("%d %d", &month, &day);
+    
+    
+ 
+    if ( month < 1 || month > 12 || day < 1 || day > 7) {
+        printf("-1");
+    } else {
+        if (2 == month) {
+            printf("4");
+        } else {
+            if ((1 == month|| 3 == month|| 5 == month|| 7 == month ||
+                8 == month || 10 == month || 12 == month)) {
+                if((5 == day || 6 == day || 7 == day)) {
+                    printf("5");
+                } else {
+                    printf("4");
+                }
+            } else if (6 == day || 7 == day) {
+                printf("5");
+            } else {
+                printf("4");
+            }
+        }
+    }
+    
      return 0;
  }
