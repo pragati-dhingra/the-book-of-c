deb35ba4ebc82c60c3068d4f74b670e9b325627a
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-04-05 11:41:30 +0530

    Update pg116_q022.c

--- pg116_q022.c
+++ pg116_q022.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
+int main (int argc, char* argv[] ) {
   /* Write a calculator program using switch case 
    * Input format --> operand operator operand
    * E.g 
@@ -9,27 +9,24 @@ int main (int argc, char* argv[] ){
    * Output --> result of computation with no trailing newline 
    * No error checking is expected. Operands are int. Handle 4 arithmetic operators + - * /
    * Only integer calculator is expected. Do not create float calculator */
-  int operand1 = 0, operand2 = 0;
+    int operand1 = 0, operand2 = 0;
     char operator = 0;
-  scanf("%d", &operand1);
-   scanf("%c", &operator);
-  scanf("%d" ,&operand2);
-switch(operator) {
+  scanf("%d %c %d", &operand1,&operator,&operand2);
+switch(operator)  {
         case '+':
-            printf("%d\n", operand1 + operand2);
+            printf("%d", operand1 + operand2);
             break;
         case '-':
-            printf("%d\n", operand1 - operand2);
+            printf("%d", operand1 - operand2);
             break;
         case '*':
-            printf("%d\n", operand1 * operand2);
+            printf("%d", operand1 * operand2);
             break;
         case '/':
-            printf("%d\n", operand1 / operand2);
+            printf("%d", operand1 / operand2);
             break;
         default:
             printf("Unrecognised operator");
-    }  
-  return 0;
-}
+     }   return 0;
+ }
   
