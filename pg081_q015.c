92256bec689108f44f70a07a10f5aa982f8a1811
Singhal-Shaili <95026201+Singhal-Shaili@users.noreply.github.com>
2022-04-05 13:52:26 +0530

    Update pg081_q015.c

--- pg081_q015.c
+++ pg081_q015.c
@@ -11,30 +11,27 @@ int main (int argc, char* argv[] ){
  int month = 0, date = 0, day = 0;
  
     
-    scanf("%d %d", &month, &day);
+    scanf("%d %d", &month,&day);
    
     
  
     if ( month < 1 || month > 12 || day < 1 || day > 7) {
         printf("-1");
-    } else {
+    }
+  else {
         if (2 == month) {
             printf("4");
-        } else {
+        }
+    else {
             if ((1 == month|| 3 == month|| 5 == month|| 7 == month ||
                 8 == month || 10 == month || 12 == month)&&(5==day ||6==day||7==day)) {
                  
                     printf("5");
                 
                 }
-            } else if (6 == day || 7 == day) {
-                printf("5");
-            } else {
-                printf("4");
-            }
-        }
-    }
-    
+            } 
+   
+  }
 return 0;
 }
 
