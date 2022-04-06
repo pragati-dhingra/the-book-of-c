7b264fbdcdf70867e8d38b510f6d4ef8a57e5472
Prashant Naresh <69414240+nareshprashant@users.noreply.github.com>
2022-04-06 12:20:59 +0530

    Update pg081_q015.c

--- pg081_q015.c
+++ pg081_q015.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
-
-int main (int argc, char* argv[] ){
-  /* Write program that reads the month and first day of the month (1 for Monday, 2 for Tuesday, and so on)
-  from the user and prints the number of Sundays in that month.
-  Input: two integers on a single line separated by one space, e.g. 4 6
-          First integer is month, 1 for Jan, 2 for Feb, ... 12 for Jan
-          Second integer is day of week, 1 for Monday, 2 for Tuesday ...
-  Output: One integer showing number of sundays in that month. No newline to be printed
-          In case of invalid input, print -1 as output */
-  
-  return 0
-}
+	
+	int main (int argc, char* argv[] ){
+	  /* Write a program that reads the month number (1-12) and prints its equivalent name (January for 1, February for 2, and so on).
+	  Input: One integer on a line, e.g. 4
+	  Output: Correponding name of month, e.g. April
+	  Please do not use abbreviations. First character is capital, rest are small case,  no trailing newline
+	  In case of invalid input, output should be "Invalid" (without quotes) 
+	  Output is case sensitive */
+	  int month = 0;
+	     
+	    scanf("%d", &month);
+	     if(month < 1 || month > 12) {
+	        printf("Invalid");
+	    } else if(month==1){
+	        printf("January");
+	    }else if(month==2){
+	        printf("February");
+	    }else if(month==3){
+	        printf("March");
+	    }else if(month==4){
+	        printf("April");
+	    }else if(month==5){
+	        printf("May");
+	    }else if(month==6){
+	        printf("June");
+	    }else if(month==7){
+	        printf("July");
+	    }else if(month==8){
+	        printf("August");
+	    }else if(month==9){
+	        printf("September");
+	    }else if(month==10){
+	        printf("October");
+	    }else if(month==11){
+	        printf("November");
+	    }else if(month==12){
+	        printf("December");
+	    }
+	 
+	  return 0;
+	}
