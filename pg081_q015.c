73e7ceb9e62d6f884bcb0ac3bc1d48e2a5cee802
Prashant Naresh <69414240+nareshprashant@users.noreply.github.com>
2022-04-06 12:23:57 +0530

    Update pg081_q015.c

--- pg081_q015.c
+++ pg081_q015.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main (int argc, char* argv[] ){
   /* Write program that reads the month and first day of the month (1 for Monday, 2 for Tuesday, and so on)
   from the user and prints the number of Sundays in that month.
@@ -9,5 +8,27 @@ int main (int argc, char* argv[] ){
   Output: One integer showing number of sundays in that month. No newline to be printed
           In case of invalid input, print -1 as output */
   
-  return 0
+  int month = 0, day = 0;
+  
+  scanf("%d %d", &month ,&day);
+   if ( month < 1|| month > 12|| day<1||day > 7) {
+        printf("-1");
+    } else {
+        if (2==month) {
+            printf("4");
+        } else {
+            if ((1==month||3==month||5==month||7==month||8==month||10==month||12==month)) {
+                if((5==day||6==day||7==day)) {
+                    printf("5");
+                } else {
+                    printf("4");
+                }
+            } else if (6==day||7==day) {
+                printf("5");
+            } else {
+                printf("4");
+            }
+        }
+    }
+   return 0;
 }
