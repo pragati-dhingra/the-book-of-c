f591294d66a3d4a3b6f071e1e9856093b0554318
Prashant Naresh <69414240+nareshprashant@users.noreply.github.com>
2022-04-06 12:07:31 +0530

    Update pg116_q022.c

--- pg116_q022.c
+++ pg116_q022.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
-
-int main (int argc, char* argv[] ){
-  /* Write a calculator program using switch case 
-   * Input format --> operand operator operand
-   * E.g 
-   * 10 + 20
-   * 100 / 10
-   * Output --> result of computation with no trailing newline 
-   * No error checking is expected. Operands are int. Handle 4 arithmetic operators + - * /
-   * Only integer calculator is expected. Do not create float calculator */
-  
-  return 0;
-}
+	
+	int main (int argc, char* argv[] ){
+	  /* Write a calculator program using switch case 
+	   * Input format --> operand operator operand
+	   * E.g 
+	   * 10 + 20
+	   * 100 / 10
+	   * Output --> result of computation with no trailing newline 
+	   * No error checking is expected. Operands are int. Handle 4 arithmetic operators + - * /
+	   * Only integer calculator is expected. Do not create float calculator */
+	  int operand1 = 0, operand2 = 0;
+	    char operator = 0;
+	 scanf("%d %c %d", &operand1,&operator,&operand2);
+	     switch(operator) {
+	        case '+':
+	            printf("%d", operand1 + operand2);
+	            break;
+	        case '-':
+	            printf("%d", operand1 - operand2);
+	            break;
+	        case '*':
+	            printf("%d", operand1 * operand2);
+	            break;
+	        case '/':
+	            printf("%d", operand1 / operand2);
+	            break;
+	        default:
+	            printf("Unrecognised operator\n");
+	    }
+	    return 0;
+	}
