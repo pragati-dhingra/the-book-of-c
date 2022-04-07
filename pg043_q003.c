f3f285c83c202fd67f1fffd5a87271b2ff865245
Dr. Amit Kumar <65865744+awadheshamit@users.noreply.github.com>
2022-04-07 14:54:02 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -5,7 +5,11 @@ int main (int argc, char* argv[] ){
    * Input one double value followed by a newline
    * Output one double value with two decimal places on a line by itself - no newline.
    * Do not print any extra strings/prompts */
-  
+  double _d_inches=0;
+  printf("Enter value in inch to be converted into entimeters:\n");
+  scanf("%lf",&_d_inches);
+  printf("Value in Centimeters : \n");
+  printf("%.2f cms", _d_inches*2.54);
   
   return 0;
 }
