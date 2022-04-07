e87fc19c1c352fcfdbc9415cfd4e7b155284ebd9
surbhivpandey <101626124+surbhivpandey@users.noreply.github.com>
2022-04-07 15:44:24 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -3,7 +3,7 @@
 int main (int argc, char* argv[] ){
   double inches=0;
   scanf("%lf",&inches);
-  printf (" %.2f", 2.54 * Inches);
+  printf ("%.2f", 2.54 * Inches);
   /* Q: Read value in inches and converts it into cms. 1 inch = 2.54 cms 
    * Input one double value followed by a newline
    * Output one double value with two decimal places on a line by itself - no newline.
