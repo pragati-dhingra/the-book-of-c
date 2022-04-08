cb090c765d76b7cee3b24e857703269640dc9227
kanikachauhan20 <101620676+kanikachauhan20@users.noreply.github.com>
2022-04-08 20:36:27 +0530

    Update pg113_q019.c

--- pg113_q019.c
+++ pg113_q019.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
 int main (int argc, char* argv[] ){
     /* Print the following string including the quotes
        "In C \\ prints \ on console"
      */
-  
+    char c='"';
+    printf("%cIn C \\\\ prints \\ on console%c",c,c);
     return 0;
 }
