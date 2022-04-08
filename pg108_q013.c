bb94f1450beb2f3e992811f80b8ff3d89acd8694
Dr. Shweta Rani <38777866+shwetasaxena2610@users.noreply.github.com>
2022-04-08 21:42:36 +0530

    Update pg108_q013.c

--- pg108_q013.c
+++ pg108_q013.c
@@ -1,16 +1,20 @@
 int main(int argc, char * argv[]) {
     float percent = 0, average = 0;
-    
+        float epsilon = 0.00001;
+
     scanf("%f %f",&percent, &average);
     
     /* Add checks for both percent and average to be in 0 to 100 range, both inclusive 
      * If found out of range then print "Invalid input" and skip further processing 
      * Modify the below qualifying criteria to include additional condition of percent >= 80 */
 
-    if (percent > average) {
+   if((percent < 0-epsilon || percent> 100+epsilon) || (average< 0-epsilon ||  average >100+epsilon)){
+       printf("Invalid input");
+}else{
+       if (percent>=80 && percent>average) {
         printf("Qualify");
     } else {
         printf("Oops");
     }
-    return 0;
+}  return 0;
 }
