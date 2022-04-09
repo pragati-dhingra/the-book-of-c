4076c4bc08532f8261f4b1581bcf5980903a7c8d
Singhal-Shaili <95026201+Singhal-Shaili@users.noreply.github.com>
2022-04-09 21:46:57 +0530

    Update pg146_q009.c

--- pg146_q009.c
+++ pg146_q009.c
@@ -13,7 +13,7 @@ int num = 0, i = 0, sum = 0;
         for (i=1; i <= num; i++) {
             sum += i;
         }
-        printf("%d ",  sum);
+        printf("%d",  sum);
     }
     return 0;
 }
