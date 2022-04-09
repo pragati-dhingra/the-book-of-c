5337ab4c14d934563ec1c6262efe1de5c645b551
pragati-dhingra <38684074+pragati-dhingra@users.noreply.github.com>
2022-04-09 10:38:58 +0530

    Print all factors of a number

--- pg156_q023.c
+++ pg156_q023.c
@@ -4,7 +4,7 @@ int main (int argc, char* argv[] ){
     /* Write a program to print all factors of a number
     * input -> a positive integer
     * Expected output, if number entered is less than 1 print "Invalid input" without quotes
-    * If valid input was received then print all factors of that number on a single line separated by a single space
+    * If valid input was received then print all factors of that number in increasing order on a single line separated by a single space
     * E,g. input -> 7
     * output -> "1 7 ", without quotes
     * input -> 10
