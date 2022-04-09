7725b03c10f3df18f0ca0060385f48c790ceed05
pragati-dhingra <38684074+pragati-dhingra@users.noreply.github.com>
2022-04-09 09:19:11 +0530

    Print first 5 Fibonacci numbers

---
+++ pg146_q010.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[] ){
+    /* Print first 5 fibonacci numbers using for loop
+    * All numbers should appear on a single line separated by a single space
+    * Expected output -> "0 1 1 2 3 " without quotes. Note there is one space character after each number */
+
+    return 0;
+}
