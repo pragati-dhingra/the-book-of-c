ef2dbaa1e12ed2d8278dcfae782ce9f600c64474
shalini311 <58061627+shalini311@users.noreply.github.com>
2022-04-09 10:16:07 +0530

    Update pg143_q004.c

--- pg143_q004.c
+++ pg143_q004.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[] ){
+    int num=0;
     /* Print table of 2 using a for loop
     * All numbers should appear on a single line separated by a single space
     * Expected output -> "2 4 6 8 10 12 14 16 18 20 " without quotes. 
     * Note there is one space character after each number */
-
+    for(num=2;num<=20;num=num+2)
+    {
+        printf("%d ",&num);
+    }   
     return 0;
 }
