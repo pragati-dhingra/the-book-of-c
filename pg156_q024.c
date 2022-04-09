5f498081cc97bbcc627222364becb3fa788c8207
pragati-dhingra <38684074+pragati-dhingra@users.noreply.github.com>
2022-04-09 11:18:48 +0530

    Print sum of all digits of a number

---
+++ pg156_q024.c
@@ -0,0 +1,11 @@
+int main (int argc, char* argv[] ){
+    /* Write a program to print sum of all digits of a number
+    * input -> an integer
+    * Expected output -> print sum of all digits of that number on a single line with no trailing newline
+    * E,g. input -> 7
+    * output -> 7
+    * input -> -110
+    * output -> 2  */
+
+    return 0;
+}
