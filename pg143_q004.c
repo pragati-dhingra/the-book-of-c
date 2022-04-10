4a2cc4ea21408c16ff107f9345866e52cb2aa413
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-04-10 18:14:52 +0530

    Update pg143_q004.c

--- pg143_q004.c
+++ pg143_q004.c
@@ -5,6 +5,11 @@ int main (int argc, char* argv[] ){
     * All numbers should appear on a single line separated by a single space
     * Expected output -> "2 4 6 8 10 12 14 16 18 20 " without quotes. 
     * Note there is one space character after each number */
+    int i=1, mul=0;
+    for(; i<=10; i++){
+    	mul=2*i;
+        printf(" %d", mul);
+    }
 
     return 0;
 }
