24098237f405c79e1e2d6186b7c48c571e1e5336
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-04-10 20:49:41 +0530

    Update pg156_q024.c

--- pg156_q024.c
+++ pg156_q024.c
@@ -5,7 +5,17 @@ int main (int argc, char* argv[] ){
     * E,g. input -> 7
     * output -> 7
     * input -> -110
-    * output -> 2  */
-
-    return 0;
-}
+    * output -> 2  */ 
+    int n,sum=0,m;    
+    printf("Enter a number:");    
+    scanf("%d",&n);    
+    while(n>0)
+    {
+        m=n%10;    
+        sum=sum+m;    
+        n=n/10;    
+    }
+    printf("Sum is=%d",sum);
+    return 0;  
+}   
+    
