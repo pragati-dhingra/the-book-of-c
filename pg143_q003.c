2dd7d8f3705be662ff44e52ccb3c8e52ca81756c
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-04-10 18:47:54 +0530

    Update pg143_q003.c

--- pg143_q003.c
+++ pg143_q003.c
@@ -1,13 +1,14 @@
-
-     int main (int argc, char* argv[] ){
+#include<stdio.h>
+int main (int argc, char* argv[] ) {
     /* Print numbers 10 to 1 using a for loop
     * All numbers should appear on a single line separated by a single space
     * Expected output -> "10 9 8 7 6 5 4 3 2 1 " without quotes. 
     * Note there is one space character after each number */
+    
     int i=0;
-    for(i=10; i>0; i--)
-    {
-        printf("%d\t", i);
-    }
+    for(i=10; i>0; i--) {
+        printf("%d ", i);
+     }
+  printf("\n");
     return 0;
-}
+ }
