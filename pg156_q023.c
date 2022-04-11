f0198f4868606d218930fef3442ede2b7540578c
surbhivpandey <101626124+surbhivpandey@users.noreply.github.com>
2022-04-11 14:28:24 +0530

    Update pg156_q023.c

--- pg156_q023.c
+++ pg156_q023.c
@@ -10,6 +10,13 @@ int main (int argc, char* argv[] ){
     * input -> 10
     * output -> "1 2 5 10 ", without quotes
     * Note that there is one space after each number */
-
+int i=1, n;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+{
+    if(n%i==0)
+    printf("%d ",i);
+  
+}
     return 0;
 }
