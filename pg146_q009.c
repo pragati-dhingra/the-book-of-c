61620c8256f400473bbc0d86e38c9a3c8d441b5f
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-04-11 10:36:56 +0530

    Update pg146_q009.c

--- pg146_q009.c
+++ pg146_q009.c
@@ -5,16 +5,15 @@ int main (int argc, char* argv[] ){
     * Validate that input should be in the range 1 to 1000 - both inclusive
     * If input is outside of this range then print "Invalid input" and skip rest of processing
     * If input was valid then, using a for loop, print the sum of first n natural numbers on a line by itself with no trailing newline */
-    int num = 0, i = 0, sum = 0;
-    printf("Enter a number [1 to 1000]: ");
-    scanf("%d", &num);
-    if (num < 0||num > 1001) {
+    int n=0, i=0, sum=0;
+    scanf("%d", &n);
+    if (n < 0||n > 1001) {
         printf("Invalid Input\n");
     } else {
-        for (i=1; i <= num; i++) {
+        for (i=1; i <= n; i++) {
             sum += i;
         }
-        printf("Sum of 1 to %d is %d\n", num, sum);
+        printf("Sum of 1 to %d is %d\n", sum);
     }
     return 0;
 }
