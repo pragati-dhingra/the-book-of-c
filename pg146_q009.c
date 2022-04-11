5f7760f7baadf76e9205eb67c41c87d31cab3a47
anujkec <101444151+anujkec@users.noreply.github.com>
2022-04-11 02:00:41 -0700

    Update pg146_q009.c

--- pg146_q009.c
+++ pg146_q009.c
@@ -8,10 +8,10 @@ int main (int argc, char* argv[] ){
     int n= 0, i = 0, sum = 0;
     printf("Enter a number [1-1000]: ");
     scanf("%d", &n);
-    if (n<1||n>1000) {
+    if (n<0||n>1001) {
         printf("Invalid input");
     } else {
-        for (i=1; i <=n; i++) {
+        for (i=1;i<=n;i++) {
             sum += i;
         }
         printf("%d",sum);
