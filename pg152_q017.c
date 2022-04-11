a8937d73142f93c0acd018b3e9c0af711a380d45
sandeepkec <101444259+sandeepkec@users.noreply.github.com>
2022-04-11 01:50:36 -0700

    Update pg152_q017.c

--- pg152_q017.c
+++ pg152_q017.c
@@ -8,6 +8,25 @@ int main (int argc, char* argv[] ){
     * E.g. 
     * 4
     * Yes */
+    int num = 0, i = 1;
+ 
+    printf("Enter a number >1: ");
+    scanf("%d", &num);
+ 
+    if (num < 1) {
+        printf("Invalid Input\n");
+    } else {
+        while (i*i <= num) {
+            if(i*i == num) {
+                 printf("Yes");
+                 break;
+            }
+            i++;
+        }
+        if(num != i*i) {
+            printf("No");
+        }
+    }
 
     return 0;
 }
