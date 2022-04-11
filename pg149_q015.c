7f94a935d695bfbad108f609702cb83b731d1959
sandeepkec <101444259+sandeepkec@users.noreply.github.com>
2022-04-11 02:15:35 -0700

    Update pg149_q015.c

--- pg149_q015.c
+++ pg149_q015.c
@@ -5,16 +5,14 @@ int main (int argc, char* argv[] ){
     * and prints the number of digits in that number on a line by itself with no trailing  newline
     * E.g. if input was 25089 then output should be 5 */
 int num = 0, digits = 0;
- 
-    printf("Enter a number: ");
     scanf("%d", &num);
-    printf("%d ", num);
- 
-    if (0 == num) {
-        digits = 1;
-    } else {
+    if (0==num) {
+        digits=1;
+    } else 
+    
+    {
         while(num) {
-            num /= 10;
+            num/=10;
             digits++;
         }
     }
