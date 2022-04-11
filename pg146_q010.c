54687c1906e5be35ed7b8789aba1d6fe69d0d2d6
surbhivpandey <101626124+surbhivpandey@users.noreply.github.com>
2022-04-11 11:10:37 +0530

    Update pg146_q010.c

--- pg146_q010.c
+++ pg146_q010.c
@@ -5,5 +5,14 @@ int main (int argc, char* argv[] ){
     * All numbers should appear on a single line separated by a single space
     * Expected output -> "0 1 1 2 3 " without quotes. Note there is one space character after each number */
 
+  int nextTerm=0, t1=0, t2=1, i=0;
+  nextTerm=t1+t2;
+  printf("%d %d",t1, t2);
+    for (i = 3; i <= 5; i++) {
+    printf(" %d", nextTerm);
+    t1 = t2;
+    t2 = nextTerm;
+    nextTerm = t1 + t2;
+  }
     return 0;
 }
