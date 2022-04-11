2c634abce00f1371facd995a0cc86eb001f9319d
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-04-11 10:31:46 +0530

    Update pg146_q009.c

--- pg146_q009.c
+++ pg146_q009.c
@@ -8,7 +8,7 @@ int main (int argc, char* argv[] ){
     int num = 0, i = 0, sum = 0;
     printf("Enter a number [1 to 1000]: ");
     scanf("%d", &num);
-    if (num < 1 || num > 1000) {
+    if (num < 0||num > 1001) {
         printf("Invalid Input\n");
     } else {
         for (i=1; i <= num; i++) {
