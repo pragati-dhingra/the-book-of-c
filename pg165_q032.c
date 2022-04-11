0a8e8bbd181159bcac516d9e36896bf90350516c
anujkec <101444151+anujkec@users.noreply.github.com>
2022-04-11 03:29:13 -0700

    Update pg165_q032.c

--- pg165_q032.c
+++ pg165_q032.c
@@ -8,6 +8,28 @@ int main (int argc, char* argv[] ){
     * output -> 2
     * input -> 9
     * output -> 23 */
-
+    int nth = 0, num = 2, count = 0, i = 0;
+    printf("Enter a number (5-100) ");
+    scanf("%d", &nth);
+     if(nth<=1||nth>100) {
+        printf("Invalid input\n");
+    } else {
+        do {
+            for(i=2; i<=num/2; i++) {
+                if(num%i==0) {
+                   break;
+                }
+            }
+            if(i>num/2) {
+                printf("%d", num);
+                count++;
+            }
+            if (count == nth) {
+                printf("%d",num);
+                break;
+            }
+            num++;
+        } while(1);
+    }
     return 0;
 }
