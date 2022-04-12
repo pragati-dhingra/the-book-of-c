02e3fd04af9320c5aacf3d8aae303263aec5662a
Sonika-Nagar <101444180+Sonika-Nagar@users.noreply.github.com>
2022-04-12 10:08:28 +0530

    Update pg146_q009.c

--- pg146_q009.c
+++ pg146_q009.c
@@ -13,7 +13,7 @@ int main (int argc, char* argv[] ){
         for (i=1; i <= num; i++) {
             sum += i;
         }
-        printf("%d ", sum);
+        printf("%d", sum);
     }
     return 0;
 }
