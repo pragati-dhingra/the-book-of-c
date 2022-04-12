eb413fcc9d73000b076d40a4f84da1246c9d9911
ShipraGautam <101443814+ShipraGautam@users.noreply.github.com>
2022-04-12 10:18:55 +0530

    Update pg143_q003.c

--- pg143_q003.c
+++ pg143_q003.c
@@ -5,7 +5,7 @@ int main (int argc, char* argv[] ){
     * Note there is one space character after each number */
   int i=0;
     for(i=10; i>0; i--){
-        printf("%d", i);
+        printf("%d ", i);
     }
     return 0;
 }
