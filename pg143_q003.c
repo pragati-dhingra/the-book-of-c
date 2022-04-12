ca5e043a8d0415a3a198f468f2cb94da522ef2e9
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-04-12 09:26:21 +0530

    Update pg143_q003.c

--- pg143_q003.c
+++ pg143_q003.c
@@ -4,7 +4,7 @@ int main (int argc, char* argv[] ){
     * Expected output -> "10 9 8 7 6 5 4 3 2 1 " without quotes. 
     * Note there is one space character after each number */
     int i=0;
-    for(i=10;i>=1;i++)
+    for(i=10;i>=1;i--)
         printf("%d ",i);
   
     return 0;
