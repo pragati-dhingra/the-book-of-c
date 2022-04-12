2917c31fb23c76e71404be7721af8a8a043980ee
ShipraGautam <101443814+ShipraGautam@users.noreply.github.com>
2022-04-12 11:39:04 +0530

    Update pg156_q023.c

--- pg156_q023.c
+++ pg156_q023.c
@@ -10,6 +10,16 @@ int main (int argc, char* argv[] ){
     * input -> 10
     * output -> "1 2 5 10 ", without quotes
     * Note that there is one space after each number */
-
+int num = 0, i = 1;
+    scanf("%d", &num);
+       do {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+        i++;
+    }while(i<=num/2);
+ 
+    printf("%d ", num);
+ 
     return 0;
 }
