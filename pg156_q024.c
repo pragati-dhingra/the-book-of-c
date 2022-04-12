10f1e1539a100a99a55fbb52d193653a408b33b5
shalini127 <72406579+shalini127@users.noreply.github.com>
2022-04-12 13:20:59 +0530

    Update pg156_q024.c

--- pg156_q024.c
+++ pg156_q024.c
@@ -6,6 +6,15 @@ int main (int argc, char* argv[] ){
     * output -> 7
     * input -> -110
     * output -> 2  */
-
+    int num = 0, sum = 0;
+ 
+    scanf("%d", &num);
+ 
+    do {
+        sum += num%10;
+        num /= 10;
+    }while(num);
+ 
+    printf("%d", sum);
     return 0;
 }
