8e406f8f305499535d0a4bb5106513b54de01c29
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-04-13 15:23:26 +0530

    Update pg156_q023.c

--- pg156_q023.c
+++ pg156_q023.c
@@ -12,18 +12,19 @@ int main (int argc, char* argv[] ){
     * Note that there is one space after each number */
      int num = 0, i = 1;
     
-    scanf("%d",&num);
+    scanf("%d", &num);
     if(num<1)
     	printf("Invalid input");
-    else
-    {
+    else if(num>1) {
 	    do {
         if (num % i == 0) {
-            printf("%d ",i);
+            printf("%d ", i);
         }
         i++;
     }while(i<=num/2);
-    }
-    printf("%d ",num);
+ }else {
+ 
+    printf("%d", num); }
+ 
     return 0;
   }
