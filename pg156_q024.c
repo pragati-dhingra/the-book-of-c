d950da2d48fb35b7375e392659e2d03755d34105
ShipraGautam <101443814+ShipraGautam@users.noreply.github.com>
2022-04-13 09:29:54 +0530

    Update pg156_q024.c

--- pg156_q024.c
+++ pg156_q024.c
@@ -6,6 +6,17 @@ int main (int argc, char* argv[] ){
     * output -> 7
     * input -> -110
     * output -> 2  */
-
+int num = 0, sum = 0;
+ if(num<0){
+     num=num*-1;
+ }
+    scanf("%d", &num);
+ 
+    do {
+        sum += num%10;
+        num /= 10;
+    }while(num);
+ 
+    printf("%d", sum);
     return 0;
 }
