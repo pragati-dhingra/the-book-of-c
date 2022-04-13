1cc48b875047c1b7cd2d40121d40edae1cfc05f1
shalini127 <72406579+shalini127@users.noreply.github.com>
2022-04-13 18:31:11 +0530

    Update pg156_q023.c

--- pg156_q023.c
+++ pg156_q023.c
@@ -11,23 +11,19 @@ int main (int argc, char* argv[] ){
     * output -> "1 2 5 10 ", without quotes
     * Note that there is one space after each number */
 int num = 0, i = 1;
+ scanf("%d", &num);
+    if(num<1){
+        printf("Invalid input");
+    }
 
-
-    scanf("%d", &num);
-    if(num<1)
-    	printf("Invalid input");
-    else if(num>1) {
-	    do {
+ do {
         if (num % i == 0) {
             printf("%d ", i);
         }
         i++;
     }while(i<=num/2);
+ 
     printf("%d ", num);
- }else {
-
-    printf("%d ", num); }
-
+ 
     return 0;
 }
-  }
