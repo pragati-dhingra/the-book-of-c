b7854207b3cdbfa3eb7f7b2f55f3ce69faa59cfe
shalini127 <72406579+shalini127@users.noreply.github.com>
2022-04-13 14:14:58 +0530

    Update pg156_q021.c

--- pg156_q021.c
+++ pg156_q021.c
@@ -4,6 +4,12 @@ int main (int argc, char* argv[] ){
     /* Write a program to print table of 7 using do while loop
     * Expected output is "7 14 21 28 35 42 49 56 63 70 " without quotes.
     * Note that there is one space after each number */
-
+    int i=1;
+    table=0;
+do{
+table=7*i;
+    printf("%d",table);
+    i++;
+}while(i<=10)
     return 0;
 }
