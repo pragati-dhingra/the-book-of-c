95f3b635f8c83e00481a4ff14de0fd7ab9654010
manu-singh-github <68248625+manu-singh-github@users.noreply.github.com>
2022-04-13 15:32:34 +0530

    Update pg149_q015.c

--- pg149_q015.c
+++ pg149_q015.c
@@ -4,6 +4,13 @@ int main (int argc, char* argv[] ){
     /* Write a program that read an input integer from user
     * and prints the number of digits in that number on a line by itself with no trailing  newline
     * E.g. if input was 25089 then output should be 5 */
+   long long n;
+  int count = 0;
+  scanf("%lld", &n);
+  do {
+    n /= 10;
+    ++count;
+  } while (n != 0);
 
-    return 0;
+  printf("Number of digits: %d", count);
 }
