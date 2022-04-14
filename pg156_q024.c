fddc9c9319828c0d53d7c4c77ad20effb603f21e
SwatiGoel123 <101451657+SwatiGoel123@users.noreply.github.com>
2022-04-14 13:48:26 +0530

    Update pg156_q024.c

--- pg156_q024.c
+++ pg156_q024.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+
 int main (int argc, char* argv[] ){
     /* Write a program to print sum of all digits of a number
     * input -> an integer
@@ -6,6 +8,14 @@ int main (int argc, char* argv[] ){
     * output -> 7
     * input -> -110
     * output -> 2  */
+    int number =0,sum=0;
+    scanf("%d",&number);
+        while(number !=0)
+        {
+            sum=sum+number%10;
+            number=number/10;
+        }
+        printf("%d",sum);
 
     return 0;
 }
