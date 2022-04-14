c8e5b9fb1127070b262ed898da546a0bf2cdc217
ShivaniKakria <101454020+ShivaniKakria@users.noreply.github.com>
2022-04-14 15:07:19 +0530

    Update pg143_q004.c

--- pg143_q004.c
+++ pg143_q004.c
@@ -5,6 +5,10 @@ int main (int argc, char* argv[] ){
     * All numbers should appear on a single line separated by a single space
     * Expected output -> "2 4 6 8 10 12 14 16 18 20 " without quotes. 
     * Note there is one space character after each number */
-
+int i=1;
+    for(; i<=10; i++){
+        printf("%d ", 2*i);
+    }
     return 0;
+    
 }
