1e8661edbfff53f8e5da25cbd2909b8dd28add80
SilkiKharaliya <101621747+SilkiKharaliya@users.noreply.github.com>
2022-04-15 12:01:45 +0530

    Update pg146_q009.c

--- pg146_q009.c
+++ pg146_q009.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[] ){
+    int i,j=0, n;
+    scanf("%d",&n);
+    if(n<1 || n>1000){
+    printf("Invalid input");
+    return 0;
+    }
+    for(i=1;i<=n;i++){
+    j=j+i;
+    printf("%d",j);
+    }
+    
     /* Write a program to read an input integer n from user
     * Validate that input should be in the range 1 to 1000 - both inclusive
     * If input is outside of this range then print "Invalid input" and skip rest of processing
