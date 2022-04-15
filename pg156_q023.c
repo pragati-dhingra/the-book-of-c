1102008307bc52a177adff8c40807bd28311b8f8
VinaySingh13 <101443086+VinaySingh13@users.noreply.github.com>
2022-04-16 00:47:09 +0530

    Update pg156_q023.c

--- pg156_q023.c
+++ pg156_q023.c
@@ -10,12 +10,17 @@ int main (int argc, char* argv[] ){
     * input -> 10
     * output -> "1 2 5 10 ", without quotes
     * Note that there is one space after each number */
-int num = 0, sum = 0;
-	    scanf("%d", &num);
-	     do {
-	        sum += num%10;
-	        num /= 10;
-	    }while(num);
-	    printf("%d", sum);	
+int main(int argc, char * argv[]) {
+    int num = 0, i = 1;
+    printf("Enter a number>1: ");
+    scanf("%d", &num);
+    printf("Factors are");
+    do {
+        if (num % i==0) {
+            printf("%d ",i);
+        }
+        i++;
+    }while(i<=num/2);
+    printf("%d\n",num);
 	    return 0;
 	}
