12105c82009c3877c430f7b49c59af67b1aa51d4
shalika-210 <66405566+shalika-210@users.noreply.github.com>
2022-04-15 18:27:16 +0530

    Update pg165_q032.c

--- pg165_q032.c
+++ pg165_q032.c
@@ -8,6 +8,29 @@ int main (int argc, char* argv[] ){
     * output -> 2
     * input -> 9
     * output -> 23 */
+    int n = 0, num = 2, count = 0, i = 0;
+    
+    scanf("%d", &n);
+ 
+    if(n < 1 || n > 100) {
+        printf("Invalid input");
+    } else {
+        do {
+            for(i=2; i<=num/2; i++) {
+                if(num%i == 0) {
+                   break;
+                }
+            }
+            if(i>num/2) {
+                        count++;
+            }
+            if (count == n) {
+                printf("%d",num);
+                break;
+            }
+            num++;
+        } while(1);
+    }
 
     return 0;
 }
