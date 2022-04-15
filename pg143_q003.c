2c179d30311f0e910a4c0378fb56634ef5f06ad3
SilkiKharaliya <101621747+SilkiKharaliya@users.noreply.github.com>
2022-04-15 11:20:06 +0530

    Update pg143_q003.c

--- pg143_q003.c
+++ pg143_q003.c
@@ -1,8 +1,9 @@
-int main (int argc, char* argv[] ){
-    /* Print numbers 10 to 1 using a for loop
-    * All numbers should appear on a single line separated by a single space
-    * Expected output -> "10 9 8 7 6 5 4 3 2 1 " without quotes. 
-    * Note there is one space character after each number */
-  
+#include <stdio.h>
+ 
+int main(int argc, char * argv[]) {
+    int i=0;
+    for(i=10; i>0; i--){
+        printf("%d\n", i);
+    }
     return 0;
 }
