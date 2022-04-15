86e0b2d9001911614df142356f561641aeb1c858
Prashant Naresh <69414240+nareshprashant@users.noreply.github.com>
2022-04-15 23:28:05 +0530

    Update pg156_q024.c

--- pg156_q024.c
+++ pg156_q024.c
@@ -1,11 +1,14 @@
 int main (int argc, char* argv[] ){
-    /* Write a program to print sum of all digits of a number
-    * input -> an integer
-    * Expected output -> print sum of all digits of that number on a single line with no trailing newline
-    * E,g. input -> 7
-    * output -> 7
-    * input -> -110
-    * output -> 2  */
-
-    return 0;
-}
+    int num = 0, sum = 0;
+	 
+	    scanf("%d", &num);
+	     do {
+	        sum += num%10;
+	        num /= 10;
+	    }while(num);
+	 
+	    printf("%d", sum);
+	 
+	
+	    return 0;
+	}
