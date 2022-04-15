c99cef4cf9f3983bd8a708313e0e4b18ac0dc9ef
SilkiKharaliya <101621747+SilkiKharaliya@users.noreply.github.com>
2022-04-15 11:49:29 +0530

    Update pg143_q005.c

--- pg143_q005.c
+++ pg143_q005.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[] ){
+    
+        int i,j=0;
+        for(i=1;i<=10;i++){
+        j=i*i;
+        printf("%d ",j);
+        }
     /* Print squares of first 10 natural numbers using a for loop
     * All numbers should appear on a single line separated by a single space
     * Expected output -> "1 4 9 16 25 36 49 64 81 100 " without quotes.
