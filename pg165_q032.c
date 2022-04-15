8a698d25a16a90906e5b5e87c24433810aa948ec
VinaySingh13 <101443086+VinaySingh13@users.noreply.github.com>
2022-04-16 00:37:14 +0530

    Update pg165_q032.c

--- pg165_q032.c
+++ pg165_q032.c
@@ -8,6 +8,31 @@ int main (int argc, char* argv[] ){
     * output -> 2
     * input -> 9
     * output -> 23 */
-
+#include <stdio.h>
+ 
+int main (int argc, char * argv[]) {
+    int nth = 0, num = 2, count = 0, i = 0;
+    printf("Enter a number (1-100) ");
+    scanf("%d", &nth);
+ 
+    if(nth < 1 || nth > 100) {
+        printf("Invalid input");
+    } else {
+        do {
+            for(i=2; i<=num/2; i++) {
+                if(num%i == 0) {
+                   break;
+                }
+            }
+            if(i>num/2) {
+                count++;
+            }
+            if (count == nth) {
+                printf("%dth prime nunmber is %d",nth, num);
+                break;
+            }
+            num++;
+        } while(1);
+    }
     return 0;
 }
