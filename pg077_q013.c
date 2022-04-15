98f9171f5200b4013be1697e1f9961d31433a16d
manu-singh-github <68248625+manu-singh-github@users.noreply.github.com>
2022-04-15 11:02:21 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -7,6 +7,15 @@ int main (int argc, char* argv[] ){
   Please do not use abbreviations. First character is capital, rest are small case,  no trailing newline
   In case of invalid input, output should be "Invalid" (without quotes) 
   Output is case sensitive */
-  
+  if (percent > average) {
+   if (percent < 0 || percent > 100 || average < 0 || average > 100)
+        printf("Invalid input");
+    else {
+    if (percent > average && percent >= 80) {
+        printf("Qualify");
+    } else {
+        printf("Oops");
+    }
+    }}
   return 0
 }
