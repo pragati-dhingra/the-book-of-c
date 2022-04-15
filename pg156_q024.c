6ef12e47c382dc1794cb555c89f0872ee3745b86
rashmisharma3011 <101451636+rashmisharma3011@users.noreply.github.com>
2022-04-15 22:40:14 +0530

    Update pg156_q024.c

--- pg156_q024.c
+++ pg156_q024.c
@@ -6,6 +6,23 @@ int main (int argc, char* argv[] ){
     * output -> 7
     * input -> -110
     * output -> 2  */
-
+    int num = 0, i = 1;
+scanf("%d", &num);
+if( num < 1){
+    printf("Invalid input");
+}else if(num > 1) {
+    do {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+        i++;
+    }while(i<=num/2);
+    
+    printf("%d ", num);
+ 
+    }
+    else {
+        printf("%d ", num);
+    }
     return 0;
 }
