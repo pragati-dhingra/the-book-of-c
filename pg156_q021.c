c5bcacd7fa8c2f7ea2023fabb94442a4a9793312
SilkiKharaliya <101621747+SilkiKharaliya@users.noreply.github.com>
2022-04-15 12:37:46 +0530

    Update pg156_q021.c

--- pg156_q021.c
+++ pg156_q021.c
@@ -5,5 +5,10 @@ int main (int argc, char* argv[] ){
     * Expected output is "7 14 21 28 35 42 49 56 63 70 " without quotes.
     * Note that there is one space after each number */
 
+int i=1;
+    do {
+        printf("%d ",7*i);
+        i++;
+    }while(i<=10)
     return 0;
 }
