9c0d4b508360d7386df4fd75b0fe9221df2f2390
rashmisharma3011 <101451636+rashmisharma3011@users.noreply.github.com>
2022-04-15 22:43:56 +0530

    Update pg156_q024.c

--- pg156_q024.c
+++ pg156_q024.c
@@ -6,6 +6,18 @@ int main (int argc, char* argv[] ){
     * output -> 7
     * input -> -110
     * output -> 2  */
+   int num = 0, sum = 0;
+ 
+    scanf("%d", &num);
+    if(num<0){
+        num=num*-1;
+    }
+     do {
+        sum += num%10;
+        num /= 10;
+    }while(num);
+ 
+    printf("%d", sum);
+     return 0;
+} 
 
-    return 0;
-}
