98cfe9ea271be5e1d6f298db73c0764b063d07c2
shalini311 <58061627+shalini311@users.noreply.github.com>
2022-04-16 17:51:28 +0530

    Update pg146_q010.c

--- pg146_q010.c
+++ pg146_q010.c
@@ -4,6 +4,14 @@ int main (int argc, char* argv[] ){
     /* Print first 5 fibonacci numbers using for loop
     * All numbers should appear on a single line separated by a single space
     * Expected output -> "0 1 1 2 3 " without quotes. Note there is one space character after each number */
+int f0 = 0, f1 = 1, f = 0, i = 0;
+     printf("%d %d ", f0, f1);
+     for(i=0; i<3; i++) {
+        f = f0 + f1;
+        printf("%d ", f);
+        f0 = f1;
+        f1 = f;
+    }
 
     return 0;
 }
