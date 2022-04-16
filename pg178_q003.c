c23fc37e2002a948d60e088b3098af4a3f5cc05b
SwatiGoel123 <101451657+SwatiGoel123@users.noreply.github.com>
2022-04-16 22:52:17 +0530

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-float average(int,int,int);
+double average(int,int,int);
 
 int main (int argc, char* argv[] ){
     /* Write a program that:
@@ -12,7 +12,7 @@ int main (int argc, char* argv[] ){
 
     
     int num1=0, num2=0, num3=0;
-    float avg=0.0;
+    double avg=0.0;
     scanf("%d%d%d",&num1,&num2,&num3);
     avg = average(num1,num2,num3);
     printf("%.2f",avg);
@@ -20,7 +20,7 @@ int main (int argc, char* argv[] ){
     return 0;
 }
 
-float average(int n1,int n2,int n3)
+double average(int n1,int n2,int n3)
 {
     return ((n1+n2+n3)/3.0);
 }
