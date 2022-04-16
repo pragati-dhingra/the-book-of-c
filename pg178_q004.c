6568943d19c2d29659a24ce486102bc28b39c8b2
SwatiGoel123 <101451657+SwatiGoel123@users.noreply.github.com>
2022-04-16 22:54:03 +0530

    Update pg178_q004.c

--- pg178_q004.c
+++ pg178_q004.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-float celsius_to_fahreheit(int);
+double celsius_to_fahreheit(int);
 
 int main (int argc, char* argv[] ){
     /* Write a program that:
@@ -11,18 +11,17 @@ int main (int argc, char* argv[] ){
     * Output -> 32.00
     * (C × 9/5) + 32 = F
     */
-    int celcius=0;
-    float farenheit=0.0;
-    scanf("%d",&celcius);
+    double celcius=0.0, farenheit=0.0;
+    scanf("%lf",&celcius);
     farenheit = celsius_to_fahreheit(celcius);
     printf("%.2f",farenheit);
       
     return 0;
 }
 
-float celsius_to_fahreheit(int C)
+double celsius_to_fahreheit(int C)
 {
-    float F=0.0;
+    double F=0.0;
     F = (C * 9/5.0) + 32;
     return F;
 }
