817d6a33a1423202f508920b95aa8ed8a67b28d1
Singhal-Shaili <95026201+Singhal-Shaili@users.noreply.github.com>
2022-04-18 14:44:25 +0530

    Update pg178_q005.c

--- pg178_q005.c
+++ pg178_q005.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
-
+int max(int val1, int val2) {
+    if(val1 > val2) {
+        return val1;
+    }
+    return val2;
+}
+ 
+int max_3(int val1, int val2, int val3) {
+    int higher = 0;
+    higher = max(val1, val2);
+    higher = max(val3, higher);
+    return higher;
+}
+ 
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads three integers on a single line separated by single space character.
@@ -9,5 +22,9 @@ int main (int argc, char* argv[] ){
     * E.g. Input ->  -10 0 10
     * Output -> 10
     */
+    int a =0,b=0,c=0, var=0;
+    scanf("%d %d %d", &a,&b,&c);
+    var=max_3(a,b,c);
+    printf("%d", var);
     return 0;
 }
