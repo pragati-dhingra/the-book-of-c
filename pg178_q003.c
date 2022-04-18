0399e363f0ebb2573c00efe983b0ba8a50aaf597
Singhal-Shaili <95026201+Singhal-Shaili@users.noreply.github.com>
2022-04-18 15:13:24 +0530

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-float average(int val1, int val2, int val3) {
+float average(int a1, int a2, int a3) {
     float avg=0.0;
-    avg=(val1+val2+val3)/3.0;
+    avg=(a1+a2+a3)/3.0;
     return avg;
 }
 int main (int argc, char* argv[] ){
