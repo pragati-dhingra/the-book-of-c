323d49931a2e507ad9a10777cbe0a71f1407343c
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-04-18 22:20:57 +0530

    Update pg178_q005.c

--- pg178_q005.c
+++ pg178_q005.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
-    /* Write a program that:
-    * 1. Reads three integers on a single line separated by single space character.
-    * 2. Defines a function named max to return higher of two integer parameters. 
-    * 3. Determines max of 3 input integers by using this max function
-    * 4. Prints the highest of 3 input integers as output on a line by itself with no trailing space or newline
-    * E.g. Input ->  -10 0 10
-    * Output -> 10
-    */
    int max11(int num1, int num2) {
     if(num1 > num2) {
         return num1;
@@ -22,10 +13,20 @@ int max(int num1, int num2, int num3) {
     higher = max11(num3, higher);
     return higher;
 }
-int main(){
+int main (int argc, char* argv[] ){
+    /* Write a program that:
+    * 1. Reads three integers on a single line separated by single space character.
+    * 2. Defines a function named max to return higher of two integer parameters. 
+    * 3. Determines max of 3 input integers by using this max function
+    * 4. Prints the highest of 3 input integers as output on a line by itself with no trailing space or newline
+    * E.g. Input ->  -10 0 10
+    * Output -> 10
+    */
 	int num1=0,num2=0,num3=0;
 	scanf("%d%d%d",&num1,&num2,&num3);
 	printf("%d",max(num1,num2,num3));
 	return 0;
-}
+}	
+
+	
    
