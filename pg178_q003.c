493afe76999b1bfe08dd08b70ef4fbbbb83d49a2
sandeepkec <101444259+sandeepkec@users.noreply.github.com>
2022-04-18 02:32:38 -0700

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -10,7 +10,7 @@ float average(int val1, int val2, int val3);
     * Output -> 12.00
     */
 int val1 ,val2,val3;
- scanf("%d %d %d",&val1,&val2,&val3);
+ scanf("%d%d%d",&val1,&val2,&val3);
     printf("%.2f",average(val1,val2,val3));
         
     return 0;
