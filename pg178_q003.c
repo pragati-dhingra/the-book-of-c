63071d9554d0715f09bee98e2694e82c93ee47e5
ShipraGautam <101443814+ShipraGautam@users.noreply.github.com>
2022-04-18 13:36:16 +0530

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+float average(int val1, int val2, int val3){
+	float avg=0;
+	avg=(val1+val2+val3)/3;
+	return avg;
+}
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads three integers on a single line
@@ -8,6 +12,10 @@ int main (int argc, char* argv[] ){
     * E.g. Input ->  6 10 20
     * Output -> 12.00
     */
-
+int a=0, b=0, c=0;
+float var=0;
+    scanf("%d %d %d",&a,&b,&c);
+    var=average(a,b,c);
+        printf("%.2f",var);
     return 0;
 }
