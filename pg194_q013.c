23ef08b999f0fb618aa87bd158598904ecbe000b
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-04-18 22:13:40 +0530

    Update pg194_q013.c

--- pg194_q013.c
+++ pg194_q013.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+int sum_of_two_primes(int n);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads a natural number as input (1, 2, 3 …)
@@ -7,5 +7,41 @@ int main (int argc, char* argv[] ){
     * 3. Define a function is_sum_of_primes() that determines if a number can be expressed as sum of prime numbers
     * 4. Call is_sum_of_primes from main with the user input 
     * 5. In main, use the returned value to print "yes" if number is a sum of prime numbers else print "no" - no trailing newline */
-    return 0;
+int main()
+{
+int n, i;
+printf("Enter the number: ");
+scanf("%d", &n);
+int flag = 0;
+for(i = 2; i <= n/2; ++i)
+{
+if (sum_of_two_primes(i) == 1)
+{
+if (sum_of_two_primes(n-i) == 1)
+{
+printf("yes");
+flag = 1;
 }
+}
+}
+
+if (flag == 0)
+printf("no");
+return 0;
+}
+
+
+int sum_of_two_primes(int n)
+{
+int i, isPrime = 1;
+for(i = 2; i <= n/2; ++i)
+{
+if(n % i == 0)
+{
+isPrime = 0;
+break;
+}
+}
+return isPrime;
+}
+
