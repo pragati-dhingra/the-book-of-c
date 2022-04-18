60b0048071404a127ee0d2268e9b1064d403c96a
sandeepkec <101444259+sandeepkec@users.noreply.github.com>
2022-04-18 01:34:53 -0700

    Update pg194_q013.c

--- pg194_q013.c
+++ pg194_q013.c
@@ -1,4 +1,51 @@
 #include <stdio.h>
+#include <stdio.h>
+ 
+int is_prime(int num) {
+    int i=0;
+ 
+    for(i=2; i<=num/2; i++) {
+        if (num%i== 0) {
+            return 0;
+        }
+    }
+ 
+    return 1;
+}
+ 
+int is_sum_of_prime(int num) {
+    int i=0;
+    for(i=2; i<=num/2; i++) {
+        if(is_prime(i)) {
+            if(is_prime(num-i)) {
+                return i;
+            }
+        }
+    }
+ 
+    return 0;
+}
+ 
+int main (int argc, char * argv[]) {
+    int num = 0, prime=-1;
+ 
+    printf("Enter a number > 2: ");
+    scanf("%d", &num);
+ 
+    if (num > 2) {
+        prime = is_sum_of_prime(num);
+        if (prime) {
+            printf("yes");
+            return 0;
+        }
+    } else {
+        printf("Invalid input");
+    }
+    if(!prime) {
+        printf("No");
+    }
+    return 0;
+}
 
 int main (int argc, char* argv[] ){
     /* Write a program that:
