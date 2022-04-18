14005e8efbd49afa1fb43cd6cad7019dcd034c7c
shalini127 <72406579+shalini127@users.noreply.github.com>
2022-04-18 16:39:47 +0530

    Update pg194_q013.c

--- pg194_q013.c
+++ pg194_q013.c
@@ -7,5 +7,22 @@ int main (int argc, char* argv[] ){
     * 3. Define a function is_sum_of_primes() that determines if a number can be expressed as sum of prime numbers
     * 4. Call is_sum_of_primes from main with the user input 
     * 5. In main, use the returned value to print "yes" if number is a sum of prime numbers else print "no" - no trailing newline */
+   int num = 0, prime=-1;
+
+    printf("Enter a number > 2: ");
+    scanf("%d",&num);
+
+    if (num > 2) {
+        prime = is_sum_of_prime(num);
+        if (prime) {
+            printf("yes");
+            return 0;
+        }
+    } else {
+        printf("Invalid input");
+    }
+    if(!prime) {
+        printf("No");
+    }
     return 0;
 }
