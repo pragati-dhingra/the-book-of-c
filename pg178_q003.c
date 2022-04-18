95d97daef7b393bb11e6305195a0ed7ae920a983
sandeepkec <101444259+sandeepkec@users.noreply.github.com>
2022-04-18 01:15:23 -0700

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
-float average(int val1, int val2, int val3) {
-    return (val1 + val2+ val3)/3.0;
-}
-
-int main (int argc, char* argv[] ){
+float average(int val1, int val2, int val3); 
+ int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads three integers on a single line
     * 2. Invoke a function named average to compute  average of three numbers. The function should return the average value
@@ -14,7 +11,10 @@ int main (int argc, char* argv[] ){
     */
 int var1 ,var2,var3;
     scanf("%d%d%d",&var1,&var2,&var3);
-    printf("%0.2f",average(val1,val2,val3);
+    printf("%0.2f",average(val1,val2,val3));
         
     return 0;
+}
+    float average(int val1, int val2, int val3) {
+    return (val1 + val2+ val3)/3.0;
 }
