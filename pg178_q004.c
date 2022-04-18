d243c032fe1bb2c63de31cc10c03ee6b4aeb4f31
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-04-18 16:17:20 +0530

    Update pg178_q004.c

--- pg178_q004.c
+++ pg178_q004.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+double celsius_to_fahreheit(double cel);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads a double - temp in degree C
@@ -9,5 +9,12 @@ int main (int argc, char* argv[] ){
     * Output -> 32.00
     * (C × 9/5) + 32 = F
     */
+    double cel=0;
+    printf("%.2lf", celsius_to_fahreheit(cel));
     return 0;
 }
+ double celsius_to_fahreheit(double cel)
+ {
+     return ((cel *9/5)+32);
+ }
+
