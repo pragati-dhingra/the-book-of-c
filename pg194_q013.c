88a4dde54eaddf8c91f6a6941d37fb393ba284fb
shalini127 <72406579+shalini127@users.noreply.github.com>
2022-04-18 23:44:56 +0530

    Update pg194_q013.c

--- pg194_q013.c
+++ pg194_q013.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int is_prime(int val);
+int is_sum_of_prime(int val);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads a natural number as input (1, 2, 3 …)
@@ -7,22 +9,43 @@ int main (int argc, char* argv[] ){
     * 3. Define a function is_sum_of_primes() that determines if a number can be expressed as sum of prime numbers
     * 4. Call is_sum_of_primes from main with the user input 
     * 5. In main, use the returned value to print "yes" if number is a sum of prime numbers else print "no" - no trailing newline */
-   int num = 0, prime=-1;
+   int val=0, flag=0;
+   scanf("%d",&val);
+   if(val<1){
+   	printf("Invalid input");
+   }
+   else{
+   	flag=is_sum_of_prime(val);
+   	if(flag==0){
+	printf("no");  
+	}
+   	else{
+   		printf("yes");
+	   }
+   }
+    return 0;
+}
+int is_prime(int val) {
+    int i=0;
 
-    printf("Enter a number > 2: ");
-    scanf("%d",&num);
-
-    if (num > 2) {
-        prime = is_sum_of_prime(num);
-        if (prime) {
-            printf("yes");
+    for(i=2; i<=val/2; i++) {
+        if (val%i== 0) {
             return 0;
         }
-    } else {
-        printf("Invalid input");
     }
-    if(!prime) {
-        printf("No");
+
+    return 1;
+}
+
+int is_sum_of_prime(int val) {
+    int i=0;
+    for(i=2; i<=val/2; i++) {
+        if(is_prime(i)) {
+            if(is_prime(val-i)) {
+                return i;
+            }
+        }
     }
+
     return 0;
 }
