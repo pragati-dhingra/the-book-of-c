d1bddd15bf9a81d979bee8461dfb59d1c25e9575
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-04-18 21:48:15 +0530

    Update pg194_q012.c

--- pg194_q012.c
+++ pg194_q012.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+int is_cube(int num);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads a natural number as input (1, 2, 3 …)
@@ -7,5 +7,26 @@ int main (int argc, char* argv[] ){
     * 3. Define a function is_cube() that determines if a number is cube or not
     * 4. Call is_cube from main with the user input 
     * 5. In main, use the returned value to print "yes" if number was a cube and print "no" if number was not a cube - no trailing newline */
+    int num=0;
+	scanf("%d",&num);
+	if(num<1)
+	{
+		printf("Invalid input");
+	}
+	if(is_cube(num)==1)
+		printf("yes");
+	else
+		printf("no");
     return 0;
 }
+int num=0;
+	scanf("%d",&num);
+	if(num<1)
+	{
+		printf("Invalid input");
+	}
+	else if(is_cube(num)==1)
+		printf("yes");
+	else
+		printf("no");
+    }
