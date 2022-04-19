13f5429049dba811ed452163b905b2411b27baee
Dr. Amit Kumar <65865744+awadheshamit@users.noreply.github.com>
2022-04-19 21:58:02 +0530

    Update pg178_q005.c

--- pg178_q005.c
+++ pg178_q005.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+int max_2(int val1, int val2);
+int max_3(int val1, int val2, int val3);
 
 int main (int argc, char* argv[] ){
     /* Write a program that:
@@ -9,5 +11,24 @@ int main (int argc, char* argv[] ){
     * E.g. Input ->  -10 0 10
     * Output -> 10
     */
+   int val1,val2,val3;
+    scanf("%d %d %d",&val1,&val2,&val3);
+    int max;
+    max=max_2(val1,val2);
+    max=max_3(val1,val2,val3);
+    printf("%d",max);
     return 0;
+}
+int max_2(int val1, int val2) {
+    if(val1 > val2) {
+        return val1;
+    }
+    return val2;
+}
+
+int max_3(int val1, int val2, int val3) {
+    int higher = 0;
+    higher = max_2(val1, val2);
+    higher = max_2(val3, higher);
+    return higher;
 }
