ead4f2a7b3e678857f9a2f46916f2536c8173d9d
Dr. Amit Kumar <65865744+awadheshamit@users.noreply.github.com>
2022-04-19 22:13:22 +0530

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
-
-double average(int val1,int val2,int val3);
+float average(int a1, int a2, int a3) {
+    return((a1+a2+a3)/3.0);
+   
+}
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads three integers on a single line
-        @@ -8,6 +8,12 @@ int main (int argc, char* argv[] ){
+    * 2. Invoke a function named average to compute  average of three numbers. The function should return the average value
+    * 3. Receives the returned average value in main and prints it on a line by itself with two decimal points and no trailing newline
     * E.g. Input ->  6 10 20
     * Output -> 12.00
     */
-
-    int val1=0,val2=0,val3=0;
-    scanf("%d%d%d",&val1,&val2,&val3);
-    printf("%.2lf",average(val1,val2,val3));
+    int a=0, b=0, c=0;
+    float  val=0;
+    scanf("%d %d %d",&a,&b,&c);
+    val=average(a, b,c);
+    printf("%0.2f",val);
     return 0;
-    }
-double average(int val1,int val2,int val3)
-{
-    return( (val1+val2+val3)/3.0);
 }
