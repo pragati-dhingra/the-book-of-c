f01f9f80e2129fd96345d3e6ff7419940b3fdaf6
Dr. Amit Kumar <65865744+awadheshamit@users.noreply.github.com>
2022-04-19 21:21:12 +0530

    Update pg146_q009.c

--- pg146_q009.c
+++ pg146_q009.c
@@ -6,5 +6,18 @@ int main (int argc, char* argv[] ){
     * If input is outside of this range then print "Invalid input" and skip rest of processing
     * If input was valid then, using a for loop, print the sum of first n natural numbers on a line by itself with no trailing newline */
 
+    int n, i, sum = 0; 
+ 
+    scanf("%d", &n);  
+    if(n>=1 && n<=1000)
+    {
+    for (i = 1;i <= n; i++)  
+    {  
+        sum = sum + i; 
+    }  
+    printf("%d",sum);   
+    } 
+   else
+        printf("Invalid input");
     return 0;
 }
