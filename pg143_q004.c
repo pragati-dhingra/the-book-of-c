0e767ab0cf1049e246697e3bd79387511896a3aa
Dr. Amit Kumar <65865744+awadheshamit@users.noreply.github.com>
2022-04-20 10:14:22 +0530

    Update pg143_q004.c

--- pg143_q004.c
+++ pg143_q004.c
@@ -5,6 +5,16 @@ int main (int argc, char* argv[] ){
     * All numbers should appear on a single line separated by a single space
     * Expected output -> "2 4 6 8 10 12 14 16 18 20 " without quotes. 
     * Note there is one space character after each number */
-
+int main (int argc, char* argv[] ){
+    /* Print table of 2 using a for loop
+    * All numbers should appear on a single line separated by a single space
+    * Expected output -> "2 4 6 8 10 12 14 16 18 20 " without quotes. 
+    * Note there is one space character after each number */
+   int ctr=0;
+   printf("\n");
+   for(ctr=1;ctr<=10;ctr++)
+        printf("%d ", ctr*2);
+    return 0;
+}
     return 0;
 }
