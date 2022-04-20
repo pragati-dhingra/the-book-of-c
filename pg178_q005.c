8e79b05735b79fbfb7ca5501617d0ae93816dfa2
pragya-agarwal <71170692+pragya-agarwal@users.noreply.github.com>
2022-04-20 20:18:31 +0530

    Update pg178_q005.c

--- pg178_q005.c
+++ pg178_q005.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+int max(int,int);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads three integers on a single line separated by single space character.
@@ -9,5 +9,28 @@ int main (int argc, char* argv[] ){
     * E.g. Input ->  -10 0 10
     * Output -> 10
     */
+    int num1 = 0, num2 = 0, num3 = 0,maximum = 0;
+    scanf("%d %d %d",&num1, &num2, &num3);
+    
+    maximum = max(num1,num2);
+    maximum=max(maximum,num3);
+    
+    printf("%d",maximum);
+    
     return 0;
 }
+
+int max(int num1,int num2)
+{   
+    int maximum=0;
+    if(num1>=num2)
+    {
+        maximum = num1;
+    }
+    else
+    {
+        maximum = num2;
+    }
+    return maximum;
+}
+
