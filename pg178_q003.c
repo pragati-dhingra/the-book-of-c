b8fc93f998a8ba66d333fe93cb2c997368066e37
umangrastogig <101452375+umangrastogig@users.noreply.github.com>
2022-04-20 13:43:15 +0530

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
+double average(int val1, int val2, int val3); 
+ int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads three integers on a single line
     * 2. Invoke a function named average to compute  average of three numbers. The function should return the average value
@@ -8,6 +9,14 @@ int main (int argc, char* argv[] ){
     * E.g. Input ->  6 10 20
     * Output -> 12.00
     */
-
+int val1=0 ,val2=0,val3=0;
+ scanf("%d%d%d",&val1,&val2,&val3);
+    printf("%.2lf",average(val1,val2,val3));
+       
     return 0;
+}
+    double average(int val1, int val2, int val3) {
+    	double x=0;
+    	x=val1 + val2+ val3;
+    return (x/3.0);
 }
