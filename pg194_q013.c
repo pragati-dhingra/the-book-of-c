42933c059cbabb652fa888c8ebb8d7821610e33b
pragya-agarwal <71170692+pragya-agarwal@users.noreply.github.com>
2022-04-20 20:23:27 +0530

    Update pg194_q013.c

--- pg194_q013.c
+++ pg194_q013.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 
+int is_cube(int);
+
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads a natural number as input (1, 2, 3 …)
     * 2. If input was not a natural number then print "Invalid input" - without quotes with no trailing newline
-    * 3. Define a function is_sum_of_primes() that determines if a number can be expressed as sum of prime numbers
-    * 4. Call is_sum_of_primes from main with the user input 
-    * 5. In main, use the returned value to print "yes" if number is a sum of prime numbers else print "no" - no trailing newline */
+    * 3. Define a function is_cube() that determines if a number is cube or not
+    * 4. Call is_cube from main with the user input 
+    * 5. In main, use the returned value to print "yes" if number was a cube and print "no" if number was not a cube - no trailing newline */
+    int number = 0;
+    int flag = 0;
+    scanf("%d",&number);
+    if(number<=0)
+    {
+        printf("Invalid input");
+    }else
+    {
+        flag = is_cube(number);
+        if(flag==1)
+        printf("yes");
+        else
+        printf("no");
+    }
+}
+
+int is_cube(int num) {
+    int i = 1;
+    do {
+        if(i * i * i == num) {
+            return 1;
+        }
+        i++;
+    } while (i * i * i <= num);
+
     return 0;
 }
