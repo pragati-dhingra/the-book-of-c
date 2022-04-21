381db9aebfff0b2c8ca62748b4d78e81c244f29b
shalini311 <58061627+shalini311@users.noreply.github.com>
2022-04-21 09:27:13 +0530

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -8,6 +8,14 @@ int main (int argc, char* argv[] ){
     * E.g. Input ->  6 10 20
     * Output -> 12.00
     */
-
+int val1=0 ,val2=0,val3=0;
+ scanf("%d%d%d",&val1,&val2,&val3);
+    printf("%.2lf",average(val1,val2,val3));
+       
     return 0;
+}
+double average(int val1, int val2, int val3) {
+    	double x=0;
+    	x=val1 + val2+ val3;
+    return (x/3.0);
 }
