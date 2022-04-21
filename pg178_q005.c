1fbac94b169d18464abde6575b24a1c593fd2b57
ShivaliTyagi <101444334+ShivaliTyagi@users.noreply.github.com>
2022-04-21 09:46:01 +0530

    Update pg178_q005.c

--- pg178_q005.c
+++ pg178_q005.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+int max2(int val1, int val2) 
+{
+    if(val1 > val2) 
+    {
+        return val1;
+    }
+    return val2;
+}
 
-int main (int argc, char* argv[] ){
+int max3(int val1, int val2, int val3) 
+{
+    int higher = 0;
+    higher = max2(val1, val2);
+    higher = max2(val3, higher);
+    return higher;
+}
+
+int main (int argc, char* argv[] )
+{
     /* Write a program that:
+  /* Write a program that:
     * 1. Reads three integers on a single line separated by single space character.
     * 2. Defines a function named max to return higher of two integer parameters. 
     * 3. Determines max of 3 input integers by using this max function
@@ -9,5 +27,9 @@ int main (int argc, char* argv[] ){
     * E.g. Input ->  -10 0 10
     * Output -> 10
     */
+    int a=0, b=0, c=0, maximum=0;
+    scanf("%d %d %d",&a, &b, &c);
+    maximum=max3(a,b,c);
+    printf("%d", maximum);
     return 0;
 }
