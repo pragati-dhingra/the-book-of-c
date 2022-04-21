dbbab028b6f7518a67a443d4f1086f7f3adc54db
shalini311 <58061627+shalini311@users.noreply.github.com>
2022-04-21 12:27:41 +0530

    Update pg178_q004.c

--- pg178_q004.c
+++ pg178_q004.c
@@ -9,5 +9,16 @@ int main (int argc, char* argv[] ){
     * Output -> 32.00
     * (C × 9/5) + 32 = F
     */
+    double celcius=0.0, farenheit=0.0;
+    scanf("%lf",&celcius);
+    farenheit = celsius_to_fahreheit(celcius);
+    printf("%.2f",farenheit);
+    
     return 0;
+}
+double celsius_to_fahreheit(int C)
+{
+    double F=0.0;
+    F = (C * 9/5.0) + 32;
+    return F;
 }
