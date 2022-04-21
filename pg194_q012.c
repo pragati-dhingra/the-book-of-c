c5e151fd0d61e626ee08a91b48378d60f8c11743
ShivaliTyagi <101444334+ShivaliTyagi@users.noreply.github.com>
2022-04-21 10:00:44 +0530

    Update pg194_q012.c

--- pg194_q012.c
+++ pg194_q012.c
@@ -1,11 +1,44 @@
+
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
+int is_cube(int a)
+{
+    int i = 1;
+    do
+    {
+        if ((i * i * i) == a)
+        {
+            return 1;
+        }
+        i++;
+    } while ((i * i * i) <= a);
+}
+
+
+int main(int argc, char* argv[]) 
+{
     /* Write a program that:
     * 1. Reads a natural number as input (1, 2, 3 …)
     * 2. If input was not a natural number then print "Invalid input" - without quotes with no trailing newline
     * 3. Define a function is_cube() that determines if a number is cube or not
-    * 4. Call is_cube from main with the user input 
+    * 4. Call is_cube from main with the user input
     * 5. In main, use the returned value to print "yes" if number was a cube and print "no" if number was not a cube - no trailing newline */
+
+    int num = 0, result=0;
+
+    scanf("%d", &num);
+    if (num < 1)
+    {
+        printf("Invalid input");
+    }
+    else
+    {
+        result = is_cube(num);
+        if (result == 1)
+            printf("yes");
+        else
+            printf("no");
+    }
     return 0;
 }
+
