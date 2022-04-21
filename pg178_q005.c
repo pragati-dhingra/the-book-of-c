15c8213679c17241427d2342860eb71308c628ea
ShivaliTyagi <101444334+ShivaliTyagi@users.noreply.github.com>
2022-04-21 09:40:27 +0530

    Update pg178_q005.c

--- pg178_q005.c
+++ pg178_q005.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+int max(int a, int b)
+{
+    if(a<b)
+    { return b;}
+    else
+    { return a;}
+
+}
+
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads three integers on a single line separated by single space character.
@@ -9,5 +18,13 @@ int main (int argc, char* argv[] ){
     * E.g. Input ->  -10 0 10
     * Output -> 10
     */
+    
+    int n1, n2, n3, higher, maximum;
+
+    scanf_s("%d %d %d", &n1, &n2, &n3);
+    higher = max(n1, n2);
+    maximum = max(higher, n3);
+    printf("%d", maximum);
+     
     return 0;
 }
