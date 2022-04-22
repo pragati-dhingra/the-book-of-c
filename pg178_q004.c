19dedd5e8753e2353d57a180fa1051c365125316
Priya-20961 <86092853+Priya-20961@users.noreply.github.com>
2022-04-22 09:13:33 +0530

    Update pg178_q004.c

--- pg178_q004.c
+++ pg178_q004.c
@@ -1,13 +1,32 @@
+
+
 #include <stdio.h>
+
+float celsius_to_fahreheit(int);
+double celsius_to_fahreheit(int);
 
 int main (int argc, char* argv[] ){
     /* Write a program that:
-    * 1. Reads a double - temp in degree C
-    * 2. Invoke a function named celsius_to_fahreheit to convert this input temp to degree F. The function should return the converted temp in F
-    * 3. Receives the returned converted value in main and prints it on a line by itself with two decimal points and no trailing newline
-    * E.g. Input ->  0
+@@ -11,18 +11,17 @@ int main (int argc, char* argv[] ){
     * Output -> 32.00
     * (C × 9/5) + 32 = F
     */
+    int celcius=0;
+    float farenheit=0.0;
+    scanf("%d",&celcius);
+    double celcius=0.0, farenheit=0.0;
+    scanf("%lf",&celcius);
+    farenheit = celsius_to_fahreheit(celcius);
+    printf("%.2f",farenheit);
+
     return 0;
+}
+
+float celsius_to_fahreheit(int C)
+double celsius_to_fahreheit(int C)
+{
+    float F=0.0;
+    double F=0.0;
+    F = (C * 9/5.0) + 32;
+    return F;
 }
