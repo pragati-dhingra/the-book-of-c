134e688d82f65930ea39b8a03cf0099f7dcf53d7
ILAKAUSHIK <70168212+ILAKAUSHIK@users.noreply.github.com>
2022-04-22 12:03:14 +0530

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -7,7 +7,21 @@ int main (int argc, char* argv[] ){
     * 3. Receives the returned average value in main and prints it on a line by itself with two decimal points and no trailing newline
     * E.g. Input ->  6 10 20
     * Output -> 12.00
-    */
+
+	int n1,n2,n3;
+	void average (int a, int b, int c);
+	
+	printf("Enter three numbers:");
+	scanf("%d %d %d",&n1,&n2,&n3);
+	average(n1,n2,n3);
+	
+}
+void average (int a, int b, int c)
+{
+	float average;
+	average=(a+b+c)/3.0;
+	printf("Average=%.2f",average);
+}
 
     return 0;
 }
