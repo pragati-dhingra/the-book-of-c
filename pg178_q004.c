85e3116ccd528827d10834170031490e75c95b27
ShivaliTyagi <101444334+ShivaliTyagi@users.noreply.github.com>
2022-04-22 09:51:23 +0530

    Update pg178_q004.c

--- pg178_q004.c
+++ pg178_q004.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
+double celsius_to_fahreheit(double C);
+
+int main(int argc, char* argv[])
+{
     /* Write a program that:
     * 1. Reads a double - temp in degree C
     * 2. Invoke a function named celsius_to_fahreheit to convert this input temp to degree F. The function should return the converted temp in F
@@ -9,5 +12,15 @@ int main (int argc, char* argv[] ){
     * Output -> 32.00
     * (C × 9/5) + 32 = F
     */
+
+    double temp, F;
+    scanf("%lf", &temp);
+    F = celsius_to_fahreheit(temp);
+    printf("%.2lf", F);
     return 0;
+}
+
+double celsius_to_fahreheit(double C)
+{
+    return ((C * 9 / 5) + 32);
 }
