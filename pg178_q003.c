89837cbfd3e7a7faf977d10128f320aeaafa8770
SilkiKharaliya <101621747+SilkiKharaliya@users.noreply.github.com>
2022-04-22 16:20:02 +0530

    Update pg178_q003.c

--- pg178_q003.c
+++ pg178_q003.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[] ){
-    /* Write a program that:
-    * 1. Reads three integers on a single line
-    * 2. Invoke a function named average to compute  average of three numbers. The function should return the average value
-    * 3. Receives the returned average value in main and prints it on a line by itself with two decimal points and no trailing newline
-    * E.g. Input ->  6 10 20
-    * Output -> 12.00
-    */
+int a=0, b=0, c=0;
+    float  ret=0;
 
+    scanf("%d %d %d",&a,&b,&c);
+
+
+    ret=average(a, b,c);
+    printf("%0.2f",ret);
     return 0;
 }
