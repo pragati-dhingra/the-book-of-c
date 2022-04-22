592efa2082eaf144d00814388f6169a70db4f5f6
pragya-agarwal <71170692+pragya-agarwal@users.noreply.github.com>
2022-04-22 14:34:23 +0530

    Update pg178_q004.c

--- pg178_q004.c
+++ pg178_q004.c
@@ -9,5 +9,12 @@ int main (int argc, char* argv[] ){
     * Output -> 32.00
     * (C × 9/5) + 32 = F
     */
+    double C=0 ;
+    scanf("%lf",&C);
+
+    printf("%.2lf",celsius_to_fahreheit(C));
     return 0;
+}
+float celsius_to_fahreheit(float cel) {
+    return ((cel *9)/5) + 32;
 }
