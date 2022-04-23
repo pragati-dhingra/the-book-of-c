51fea5858abfde74dc870aa272b1a15484efa818
Priya-20961 <86092853+Priya-20961@users.noreply.github.com>
2022-04-23 10:09:31 +0530

    Update pg095_q003.c

--- pg095_q003.c
+++ pg095_q003.c
@@ -2,7 +2,6 @@
 
 int main (int argc, char* argv[] ){
   /* Write a program using switch case that takes year as input and prints the name of cricket world cup winning country for that year, 
-
 a.	1975: West Indies
 b.	1979: West Indies
 c.	1983: India
@@ -18,6 +17,34 @@ If world cup was not played in the year entered by the user, program should prin
 There should be no newlines after the printed output
 Winners post 2015 are not required.
 */
-  
-  return 0
+int year=0;
+scanf("%d",&year);
+switch(year)
+{
+    case 1975: printf("West Indies");
+                break;
+    case 1979: printf("West Indies");
+                break;    
+    case 1983: printf("India");
+                break;    
+    case 1987: printf("Australia");
+                break;    
+    case 1992: printf("Pakistan");
+                break;    
+    case 1996: printf("Sri Lanka");
+                break;
+    case 1999: printf("Australia");
+                break;
+    case 2003: printf("Australia");
+                break;
+    case 2007: printf("Australia");
+                break;
+    case 2011: printf("India");
+                break;
+    case 2015: printf("Australia");
+                break;
+    default: printf("Invalid input");
+                
+}
+    return 0;
 }
