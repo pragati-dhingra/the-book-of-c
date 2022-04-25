a56e3a3d9f239c5209a8f2d8a0e38cfa21503c3e
surbhivpandey <101626124+surbhivpandey@users.noreply.github.com>
2022-04-25 16:42:31 +0530

    Update pg178_q005.c

--- pg178_q005.c
+++ pg178_q005.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+   int max11(int num1, int num2) {
+    if(num1 > num2) {
+        return num1;
+    }
+    return num2;
+}
+ 
+int max(int num1, int num2, int num3) {
+    int higher = 0;
+    higher = max11(num1, num2);
+    higher = max11(num3, higher);
+    return higher;
+}
 
 int main (int argc, char* argv[] ){
     /* Write a program that:
@@ -9,5 +22,9 @@ int main (int argc, char* argv[] ){
     * E.g. Input ->  -10 0 10
     * Output -> 10
     */
+    	int num1=0,num2=0,num3=0;
+	scanf("%d%d%d",&num1,&num2,&num3);
+	printf("%d",max(num1,num2,num3));
+
     return 0;
 }
