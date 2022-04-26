34c43994728b2b805bf2ebce30b298930ae9073a
surbhivpandey <101626124+surbhivpandey@users.noreply.github.com>
2022-04-26 11:30:13 +0530

    Update pg156_q023.c

--- pg156_q023.c
+++ pg156_q023.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+int is_prime(int val);
+int is_sum_of_prime(int val);
 
 int main (int argc, char* argv[] ){
     /* Write a program to print all factors of a number
@@ -11,5 +13,43 @@ int main (int argc, char* argv[] ){
     * output -> "1 2 5 10 ", without quotes
     * Note that there is one space after each number */
 
+    int val=0, flag=0;
+   scanf("%d",&val);
+   if(val<1){
+   	printf("Invalid input");
+   }
+   else{
+   	flag=is_sum_of_prime(val);
+   	if(flag==0){
+	printf("no");  
+	}
+   	else{
+   		printf("yes");
+	   }
+   }
+    return 0;
+}
+int is_prime(int val) {
+    int i=0;
+ 
+    for(i=2; i<=val/2; i++) {
+        if (val%i== 0) {
+            return 0;
+        }
+    }
+ 
+    return 1;
+}
+ 
+int is_sum_of_prime(int val) {
+    int i=0;
+    for(i=2; i<=val/2; i++) {
+        if(is_prime(i)) {
+            if(is_prime(val-i)) {
+                return i;
+            }
+        }
+    }
+ 
     return 0;
 }
