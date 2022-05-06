385145568b49928ad892765bbb9479683598cc4c
ShivaniKakria <101454020+ShivaniKakria@users.noreply.github.com>
2022-05-06 10:37:35 +0530

    Update pg108_q013.c

--- pg108_q013.c
+++ pg108_q013.c
@@ -6,15 +6,16 @@ int main(int argc, char * argv[]) {
     /* Add checks for both percent and average to be in 0 to 100 range, both inclusive 
      * If found out of range then print "Invalid input" and skip further processing 
      * Modify the below qualifying criteria to include additional condition of percent >= 80 */
-    if (percent < 0 || percent > 100 || average < 0 || average > 100) {
-        printf("Invalid input\n");
+   if (percent<0||percent>100||average<0||average>100) {
+        printf("Invalid input");
+    } 
+    else {
+
+    if (percent > average && percent >= 80) {
+        printf("Qualify");
     } else {
-        if (percent > average && percent > 80) {
-            printf("qualify");
-        } else {
-            printf("not qualified");
-        }
+        printf("Oops");
     }
- 
+    }
     return 0;
 }
