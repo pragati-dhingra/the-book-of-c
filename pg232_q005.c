0d1c8710593cb1c59235d570571f1f151f29e4a9
pragati-dhingra <38684074+pragati-dhingra@users.noreply.github.com>
2022-05-08 19:58:02 +0530

    Compute average max and min in an array

--- pg232_q005.c
+++ pg232_q005.c
@@ -4,7 +4,7 @@ int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads ten int as input in an array, one int per line
     * 2. Defines three functions - compute_average, find_max, find_min that find average, max and min in an array 
-    * Prints the average of the array (two decimal places), max, and min separated by a single space and no trailing spaces or newline 
+    * Prints the average of the array (two decimal places), max, and min separated by a single space and no trailing newline 
     * For example: Input 
     * 1
     * 2
@@ -17,6 +17,6 @@ int main (int argc, char* argv[] ){
     * 9
     * 10
     * Output:
-    * 5.50 10 1 */
+    * "5.50 10 1 " without quotes*/
     return 0;
 }
