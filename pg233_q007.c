d0a427d300f6b7b4ee6416286ebeca9ae959a4ba
pragati-dhingra <38684074+pragati-dhingra@users.noreply.github.com>
2022-05-08 19:56:49 +0530

    Add two 1D arrays

--- pg233_q007.c
+++ pg233_q007.c
@@ -6,7 +6,7 @@ int main (int argc, char* argv[] ){
     * First 10 ints go to first array (in order) and next 20 ints go to second array (in order)
     * 2. Defines a function add() to add the values in both arrays and store result in third array
     * 0th index of  third array is sum of 0th index value in array 1 and 2
-    * 3. Prints contents of third array on a single line with one space after each element and no trailing spaces or newline.
+    * 3. Prints contents of third array on a single line with one space after each element and no trailing newline.
     * For example: Input 
     * 1
     * 2
