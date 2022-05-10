0ea23190a15b695f2a5daf85493f0705014762a4
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-05-10 22:05:33 +0530

    Update pg232_q006.c

--- pg232_q006.c
+++ pg232_q006.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
- int find(int a[], int n, int val);
+
+int find_val(int arr[], int size, int val);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads ten int as input in an array, one int per line
@@ -22,27 +23,28 @@ int main (int argc, char* argv[] ){
     * 17
     * Output:
     * no */
-    int a[] = {0};
-    for(int i=0;i<10;i++)
-    {
-        scanf("%d",&a[i]);
+
+    int arr[10] = {0};
+ 	int x=0,val=0;
+ 	int i=0;
+ 	 for( i=0; i<10; i++) {
+        scanf("%d",&arr[i]);
     }
     scanf("%d",&val);
-	 i=find_val(a,10,val);
-	 if(i==-1)
-	  printf("no");
-	 else
-	  printf("yes");
-	
-	return 0;
-	}
-    int find_val(int a[], int n, int val){
-    for(int i=0; i<n; i++) {
-        if(a[i] == val) {
+	x=find_val(arr,10,val);
+ 	if(x==-1){
+ 		printf("no");
+ 		}
+ 	else {
+ 		printf("yes");
+	 }
+ 	return 0;
+}
+ 	int find_val(int arr[], int size, int val){
+    for(int i=0; i<size; i++) {
+        if(arr[i] == val) {
             return i;
         }
     }
     return -1;
 }
-
-   
