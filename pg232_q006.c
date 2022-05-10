3404f94b42f1a1a9e66e0f0d90806093a02d9fb1
anujkec <101444151+anujkec@users.noreply.github.com>
2022-05-10 16:01:11 +0530

    Update pg232_q006.c

--- pg232_q006.c
+++ pg232_q006.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
-
-int main (int argc, char* argv[] ){
-    /* Write a program that:
-    * 1. Reads ten int as input in an array, one int per line
-    * 2. Reads another int on a line by itself as input value to search in the array
-    * 3. Defines a function find() to find a value in an array 
-    * Prints "yes", without quotes, if the value was found in array or 
-    * prints "no", without quotes, if value was not found in the array
-    * No trailing spaces or newline in either case. Output is case sensitive
-    * For example: Input 
-    * 1
-    * 2
-    * 3
-    * 4
-    * 5
-    * 6
-    * 7
-    * 8
-    * 9
-    * 10
-    * 17
-    * Output:
-    * no */
-    	   int arr[10] = {0};
+	 int find_val(int arr[], int size, int val);
+	int main (int argc, char* argv[] ){
+	    /* Write a program that:
+	    * 1. Reads ten int as input in an array, one int per line
+	    * 2. Reads another int on a line by itself as input value to search in the array
+	    * 3. Defines a function find() to find a value in an array 
+	    * Prints "yes", without quotes, if the value was found in array or 
+	    * prints "no", without quotes, if value was not found in the array
+	    * No trailing spaces or newline in either case. Output is case sensitive
+	    * For example: Input 
+	    * 1
+	    * 2
+	    * 3
+	    * 4
+	    * 5
+	    * 6
+	    * 7
+	    * 8
+	    * 9
+	    * 10
+	    * 17
+	    * Output:
+	    * no */
+	    
+	   int arr[10] = {0};
 	    int i=0;
 	    int val=0;
 	    for(i=0;i<10;i++)
@@ -50,5 +51,3 @@ int main (int argc, char* argv[] ){
 	        }
 	    return -1;
 	    }
-
-
