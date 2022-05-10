46479cae987e8eaeb1497745b38a00b16a53d640
sandeepkec <101444259+sandeepkec@users.noreply.github.com>
2022-05-10 14:01:17 +0530

    Update pg230_q003.c

--- pg230_q003.c
+++ pg230_q003.c
@@ -17,7 +17,7 @@ int main (int argc, char* argv[] ){
     * 1
     * Output:
     * 1.00 */
-        float arr [] = {0.0};
+        float arr [10] = {0.0};
     float average = 0.0;
  int i=0;
     for(i=0; i<10; i++) 
@@ -25,8 +25,7 @@ int main (int argc, char* argv[] ){
         scanf("%f\n",&arr[i]);
     }
     
-    
-    for(i=0; i<10; i++) {
+      for(i=0; i<10; i++) {
         average += arr[i];
     }
     average /= 10;
