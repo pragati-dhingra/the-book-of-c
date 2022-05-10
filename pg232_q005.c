fb62d1ea0bc9bfa36b439e9e91a17ecc7aaf43f1
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-05-10 20:30:45 +0530

    Update pg232_q005.c

--- pg232_q005.c
+++ pg232_q005.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 float compute_average(int marks[], int size);
+	int find_max(int marks[], int size);
+	 int find_min(int marks[], int size);
 int main (int argc, char * argv[]) {
     /* Write a program that:
     * 1. Reads ten int as input in an array, one int per line
@@ -18,29 +20,51 @@ int main (int argc, char * argv[]) {
     * 10
     * Output:
     * "5.50 10 1 " without quotes*/
-     int marks[100] = { 0 };
+     int marks[10] = { 0 };
      int i=0;
      float average = 0.0;
 
-     do {
-         printf ("Enter marks for student %d", i+1);
-         scanf("%d",&marks[i]);
-         i++;
-     }while(i<100);
-
-     average = compute_average(marks, 100);
-     printf("Average score is %f\n", average);
-     return 0;
- }
-
- float compute_average(int marks[], int size) {
-     int i=0, sum=0;
-     for(i=0; i<size; i++) {
-         sum = sum + marks[i];
-     }
-     return ((1.0*sum)/size);
- }
-
-    
-    return 0;
-}
+      int max =0;
+	    int min =0;
+	for(i=0; i<10 ;i++)
+	        {
+	            scanf("%d",&marks[i]);
+	        }
+	     average = compute_average(marks, 10);
+	     printf("%0.2f ",average);
+	     max = find_max(marks,10);
+	     min = find_min(marks,10);
+	     printf("%d %d ",max,min);    
+	    return 0;
+	}
+	
+	 float compute_average(int marks[], int size) 
+	 {
+	     int i=0, sum=0;
+	     for(i=0; i<10; i++) {
+	         sum = sum + marks[i];
+	    }
+	     return ((1.0*sum)/10);
+	 }
+	
+	    
+	    int find_max(int marks[], int size) {
+	    int max = marks[0];
+	    for(int i=1; i<10; i++) {
+	        if(marks[i] > max) {
+	            max = marks[i];
+	        }
+	    }
+	    return max;
+	}
+	 
+	int find_min(int marks[], int size)
+	{
+	    int min = marks[0];
+	    for(int i=1; i<10; i++) {
+	        if(marks[i] < min) {
+	            min = marks[i];
+	        }
+	    }
+	    return min;
+	}
