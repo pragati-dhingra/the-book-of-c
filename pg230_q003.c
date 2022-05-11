c3ad6b8b2063f3ed6dd2ed7a26aa6786fd3ac6b0
kanikachauhan20 <101620676+kanikachauhan20@users.noreply.github.com>
2022-05-11 15:51:16 +0530

    Update pg230_q003.c

--- pg230_q003.c
+++ pg230_q003.c
@@ -1,21 +1,13 @@
-#include <stdio.h>
-
-int main (int argc, char* argv[] ){
-    /* Write a program that:
-    * 1. Reads ten floats as input in an array, one float per line
-    * 2. Prints the average of the array as output with two decimal places and no trailing spaces or newline 
-    * For example: Input 
-    * 1
-    * 1
-    * 1
-    * 1
-    * 1
-    * 1
-    * 1
-    * 1
-    * 1
-    * 1
-    * Output:
-    * 1.00 */
+#include<stdio.h>
+ 
+int main (int argc, char * argv[])  {
+    float arr [] = { 1.2, 2.1, 3.4, 9.4, 10.5, 6.6, 0.7, 10, 8.8, 1.9 };
+    float average = 0.0;
+ 
+    for(int i=0; i<10; i++) {
+        average += arr[i];
+    }
+    average /= 10;
+    printf("Average of array is %f\n", average);
     return 0;
 }
