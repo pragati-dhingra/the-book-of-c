3bb809e55dda45830e1a749330e30f7caa831af5
Sonika-Nagar <101444180+Sonika-Nagar@users.noreply.github.com>
2022-05-11 13:55:32 +0530

    Update pg230_q003.c

--- pg230_q003.c
+++ pg230_q003.c
@@ -17,5 +17,15 @@ int main (int argc, char* argv[] ){
     * 1
     * Output:
     * 1.00 */
+    float arr[10];
+    float average = 0.0;
+     for(int i=0; i<10; i++) {
+       scanf("%f\n",&arr[i]);
+    }
+    for(int i=0; i<10; i++) {
+        average += arr[i];
+    }
+    average /= 10;
+    printf("%f", average);
     return 0;
 }
