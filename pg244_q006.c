5bf70904004902dd5ee16e451b2fd5129f1df1a6
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-05-11 12:25:17 +0530

    Update pg244_q006.c

--- pg244_q006.c
+++ pg244_q006.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include<conio.h>
+	int add(int *a, int *b);
+	int multiply(int *a, int *b);
+	int subtract(int *a, int *b);
+	int divide(int *a, int *b);
+	
 
 int main (int argc, char* argv[] ){
     /* Write a calculator program for integer arithmetic that supports add, multiply, subtract, and divide operations 
@@ -11,5 +17,50 @@ int main (int argc, char* argv[] ){
     * 5 / 3
     * output:
     * 1  */
-    return 0;
+        int var1=0, var2=0;
+	    int *ptr1 = NULL, *ptr2 = NULL;
+		char ch;
+		
+		scanf("%d",&var1);
+		ch=getche();
+		scanf("%d",&var2);
+		printf("\n");
+		ptr1=&var1;
+		ptr2=&var2;
+		switch(ch)
+		{
+			case '+':
+				printf("%d",add(ptr1, ptr2));
+	    		
+	    		break;
+			case '*':	
+				printf("%d",multiply(ptr1, ptr2));
+			    
+			    break;
+			case '-':
+				printf("%d",subtract(ptr1, ptr2));
+	    		
+	    		break;
+	    	case '/':
+	    		printf("%d",divide(ptr1, ptr2));
+	   			break;
+		}
+			    
+	    return 0;
+	 }
+
+ int add(int *a, int *b) {
+     return(*a + *b);
+    }		 
+
+int subtract(int *a, int *b) {
+    return(*a - *b);
 }
+ 
+int multiply(int *a, int *b) {
+    return(*a * *b);
+}
+ 
+int divide(int *a, int *b) {
+    return(*a / *b);
+}	
