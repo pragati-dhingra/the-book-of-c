af2d48e396af3d78d3f837dc1df13a70b534cb33
kanikachauhan20 <101620676+kanikachauhan20@users.noreply.github.com>
2022-05-11 15:44:26 +0530

    Update pg230_q002.c

--- pg230_q002.c
+++ pg230_q002.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
 
-int main (int argc, char* argv[] ){
-    /* Write a program that:
-    * 1. Reads ten integers as input in an array, one integer per line
-    * 2. Prints the maximum number in the array as output with no trailing spaces or newline 
-    * For example: Input 
-    * 10
-    * 89
-    * -25
-    *  0
-    * 190
-    * 65
-    * -300
-    * 90
-    * 76
-    * 1
-    * Output:
-    * 190 */
+#include<stdio.h>
+ 
+int main (int argc, char * argv[])  {
+    int arr [] = { 1, 2, 3, 4, 5, 6, 7, 10, 8, 9 };
+ 
+    /* If we initialize max to 0, program will fail if
+     * all elements of array were negative. */
+    int max = arr[0];
+ 
+    for(int i=1; i<10; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    printf("Max value in array is %d\n", max);
     return 0;
 }
