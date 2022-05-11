ec9c760f069150a7efa9673d33369432aaa37ac0
anujkec <101444151+anujkec@users.noreply.github.com>
2022-05-11 09:34:49 +0530

    Update pg248_q012.c

--- pg248_q012.c
+++ pg248_q012.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
     /* Write a function that takes an int as input parameter and an int * as output parameter. 
     * Assuming input int was n
     * Function should return sum of first n natural number
@@ -11,5 +10,16 @@ int main (int argc, char* argv[] ){
     * output:
     * "15 25" without quotes - no trailing space or newline 
     * assume all inputs will be positive numbers */
-    return 0;
+   int sum_and_square(int val, int *sum) {
+ *sum = (val * (val+1))/2;
+ printf("%d ",*sum);
+ return val*val;
 }
+int main (int argc, char* argv[] )
+{
+int n=0;
+int *sum=&n;
+scanf("%d",&n);
+printf("%d",sum_and_square(n,sum));
+return 0;
+ }
