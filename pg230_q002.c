4a1686e52c7a6ce1e545ead8433e8b23dcd16d45
Sonika-Nagar <101444180+Sonika-Nagar@users.noreply.github.com>
2022-05-11 13:50:32 +0530

    Update pg230_q002.c

--- pg230_q002.c
+++ pg230_q002.c
@@ -17,5 +17,16 @@ int main (int argc, char* argv[] ){
     * 1
     * Output:
     * 190 */
+    int arr[10];
+    int max = arr[0];
+    for(int i=0; i<10; i++) {
+       scanf("%d\n",&arr[i]);
+        }
+    for(int i=1; i<10; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    printf("%d", max);
     return 0;
 }
