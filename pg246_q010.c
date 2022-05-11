52bb506c20aef34d8327f323ac1a72cbb42723c5
anujkec <101444151+anujkec@users.noreply.github.com>
2022-05-11 09:25:59 +0530

    Update pg246_q010.c

--- pg246_q010.c
+++ pg246_q010.c
@@ -11,5 +11,22 @@ int main (int argc, char* argv[] ){
     * output:
     * "9" without quotes - no trailing space or newline 
     * assume all inputs will be positive numbers */
-    return 0;
-}
+    int next_highest_perfect_square(int val) {
+ int i = 1;
+ while(i*i <= val) {
+ i++;
+ }
+ return i*i;
+ }
+ void update_val(int *a) {
+ *a = next_highest_perfect_square(*a);
+ printf("%d",*a);
+ }
+ int main (int argc, char* argv[] )
+ {
+int val=0;
+scanf("%d",&val);
+update_val(&val);
+return 0;
+ }
+
