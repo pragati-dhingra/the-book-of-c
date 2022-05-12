32fc11fcb250f5d0e6901af1a710b90d4b644320
ShivaniKakria <101454020+ShivaniKakria@users.noreply.github.com>
2022-05-12 10:16:12 +0530

    Update pg248_q012.c

--- pg248_q012.c
+++ pg248_q012.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+int sum_and_square(int val, int *sum) {
+ *sum = (val * (val+1))/2;
+ printf("%d ",*sum);
+ return val*val;
+}
 int main (int argc, char* argv[] ){
     /* Write a function that takes an int as input parameter and an int * as output parameter. 
     * Assuming input int was n
@@ -11,5 +16,9 @@ int main (int argc, char* argv[] ){
     * output:
     * "15 25" without quotes - no trailing space or newline 
     * assume all inputs will be positive numbers */
+   int n=0;
+    int *sum=&n;
+    scanf("%d",&n);
+    printf("%d",sum_and_square(n,sum));
     return 0;
 }
