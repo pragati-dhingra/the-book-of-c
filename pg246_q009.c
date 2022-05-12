bc14ee1e036575762f2eafe198b7ac0fc12e3293
umangrastogig <101452375+umangrastogig@users.noreply.github.com>
2022-05-12 17:18:29 +0530

    Update pg246_q009.c

--- pg246_q009.c
+++ pg246_q009.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
-int main (int argc, char* argv[] ){
-    /* Write a function that swaps two ints using pointers
+void swap(int *a, int *b);
+int main (int argc, char * argv[]) {
+    int a = 10, b = 20;
+    swap(&a, &b);
+    printf("a = %d and b = %d", a, b);
+    return 0;
+}
+ /* Write a function that swaps two ints using pointers
     * Input - two integer values on a single line separated by a single space
     * output - two integer values, print the two variables in the same order in which they were read
     * for example, if scanf read in var1 and var2 then printf should print them in same order var1 followed by var2
@@ -10,5 +15,11 @@ int main (int argc, char* argv[] ){
     * 5  3
     * output:
     * "3 5" without quotes - no trailing space or newline */
-    return 0;
+   
+void swap(int *a, int *b) {
+    int temp = 0;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
+ 
