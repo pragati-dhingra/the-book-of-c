e3231eeeb1ee70820a36c8bc93a9c8eb3b98a4fe
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-05-12 13:34:00 +0530

    Update pg244_q006.c

--- pg244_q006.c
+++ pg244_q006.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-	int add(int *a, int *b);
-	int multiply(int *a, int *b);
-	int subtract(int *a, int *b);
-	int divide(int *a, int *b);
-	
+int add(int *a, int *b);
+int subtract(int *a, int *b);
+int multiply(int *a, int *b);
+int divide(int *a, int *b);
 
-int main (int argc, char* argv[] ){
+int main (int argc, char * argv[]) {
     /* Write a calculator program for integer arithmetic that supports add, multiply, subtract, and divide operations 
     * Respective operators are + * - /  
     * Each operation should be implemented as a separate function
@@ -17,50 +16,33 @@ int main (int argc, char* argv[] ){
     * 5 / 3
     * output:
     * 1  */
-       int var1=0, var2=0;
-	    int *ptr1 = NULL, *ptr2 = NULL;
-		char ch=0;
-		
-		scanf("%d ",&var1);
-		scanf(" %C ",&ch);
-		scanf("%d",&var2);
-		
-		ptr1=&var1;
-		ptr2=&var2;
-		switch(ch)
-		{
-			case '+':
-				printf("%d",add(ptr1, ptr2));
-	    		
-	    		break;
-			case '*':	
-				printf("%d",multiply(ptr1, ptr2));
-			    
-			    break;
-			case '-':
-				printf("%d",subtract(ptr1, ptr2));
-	    		
-	    		break;
-	    		case '/':
-	    			printf("%d",divide(ptr1, ptr2));
-	   			
-		}
-			    
-	    return 0;
-	 }
-
- int add(int *a, int *b) {
-     return(*a + *b);
-    }		 
-
-int subtract(int *a, int *b) {
-    return(*a - *b);
+    int num1=0, num2=0;
+    char ch=0;
+    scanf("%d", &num1);
+    scanf(" %c", &ch);
+    scanf("%d", &num2);
+    int *ptr1 = &num1;
+	int *ptr2 = &num2;
+    if(ch == '+') {
+        printf("%d",add(ptr1, ptr2));
+    } else if(ch == '*' ) {
+       printf("%d",multiply(ptr1, ptr2));
+    } else if(ch == '-') {
+        printf("%d",subtract(ptr1, ptr2));
+    } else if(ch == '/') {
+        printf("%d",divide(ptr1, ptr2));
+    } 
+	return 0;
 }
- 
-int multiply(int *a, int *b) {
-    return(*a * *b);
-}
- 
-int divide(int *a, int *b) {
-    return(*a / *b);
-}	
+	int add(int *a, int *b) {
+ 		return(*a + *b);
+	}
+	int subtract(int *a, int *b) {
+ 		return(*a - *b);
+	}
+	int multiply(int *a, int *b) {
+ 		return(*a * *b);
+	}
+	int divide(int *a, int *b) {
+ 		return(*a / *b);
+	}	
