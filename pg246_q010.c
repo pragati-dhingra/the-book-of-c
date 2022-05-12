3f1cf624342aad8ceee3b6b65b9f5bf8091b85c1
shalini311 <58061627+shalini311@users.noreply.github.com>
2022-05-12 21:19:23 +0530

    Update pg246_q010.c

--- pg246_q010.c
+++ pg246_q010.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
+int sum_square(int, int *);
 
 int main (int argc, char* argv[] ){
-    /* Write a function that takes an int pointer as argument and updates the value at that location to next highest perfect square.
-    * If input number was perfect square then it should remain unchanged
-    * Program input - an int on a single line, no trailing spaces
-    * function is called with address of this input int 
-    * Value of same variable is printed after function call
+    /* Write a function that takes an int as input parameter and an int * as output parameter. 
+    * Assuming input int was n
+    * Function should return sum of first n natural number
+    * Function should update output parameter to square of n 
+    * Calling function should simply print the returned value and output parameter value post function call
     * Example, input:
     * 5
     * output:
-    * "9" without quotes - no trailing space or newline 
+    * "15 25" without quotes - no trailing space or newline 
     * assume all inputs will be positive numbers */
+    int n = 0,output=0,sum=0;
+    scanf("%d",&n);
+    sum = sum_square(n,&output);
+    printf("%d %d",sum,output);
     return 0;
+}
+
+int sum_square(int n,int *output)
+{
+    *output = n * n;
+    return (n*(n+1)/2);
+    
 }
