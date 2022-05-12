4907b4c773322d78c305252fa4b6432e8e1e0652
Singhal-Shaili <95026201+Singhal-Shaili@users.noreply.github.com>
2022-05-12 14:51:19 +0530

    Update pg237_q009.c

--- pg237_q009.c
+++ pg237_q009.c
@@ -8,7 +8,7 @@ int main (int argc, char* argv[] ){
     * one float with two decimal places per student
     * each float separated by single space
     * no extra trailing newline.
-    * For example: Input 
+    * For example: Input
     * 1
     * 2
     * 3
@@ -28,5 +28,21 @@ int main (int argc, char* argv[] ){
     * "2.00  5.00 8.00 11.00 14.00 "
     * without quotes.
     */
+    int arr[5][3],i=0,j=0;
+    float avg[5]={0};
+    for(i=0;i<5;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+     for(i=0;i<5;i++){
+        for(j=0;j<3;j++){
+            avg[i]=avg[i]+arr[i][j];
+        } avg[i]=avg[i]/3;
+    }
+    for(i=0;i<5;i++){
+        printf("%.2f ",avg[i]);
+    }
+       
     return 0;
 }
