dfda64049a206d1013bb8646479a198757c1ca70
SilkiKharaliya <101621747+SilkiKharaliya@users.noreply.github.com>
2022-05-13 12:31:47 +0530

    Update pg244_q006.c

--- pg244_q006.c
+++ pg244_q006.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+ int add(int *a, int *b);
+int subtract(int *a, int *b);
+int multiply(int *a, int *b);
+int divide(int *a, int *b);
 int main (int argc, char* argv[] ){
     /* Write a calculator program for integer arithmetic that supports add, multiply, subtract, and divide operations 
     * Respective operators are + * - /  
@@ -11,5 +14,37 @@ int main (int argc, char* argv[] ){
     * 5 / 3
     * output:
     * 1  */
+   
+ 
+    int var1=7, var2=10;
+    char c;
+    int *ptr1 = &var1, *ptr2 = &var2;
+   scanf("%d %c %d",&var1, &c, &var2);
+   if(c=='+'){  
+ add(ptr1, ptr2);
+   }
+   if(c=='-'){  
+ subtract(ptr1, ptr2);
+   } 
+  if(c=='*'){  
+ multiply(ptr1, ptr2);
+   }
+  if(c=='/'){  
+ divide(ptr1, ptr2);
+   }
     return 0;
+}
+int add(int *a, int *b) {
+     printf("%d",*a + *b);
+ }
+int subtract(int *a, int *b) {
+    printf("%d",*a - *b);
+}
+ 
+int multiply(int *a, int *b) {
+    printf("%d",*a * *b);
+}
+ 
+int divide(int *a, int *b) {
+    printf("%d",*a / *b);
 }
