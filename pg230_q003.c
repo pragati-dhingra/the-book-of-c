107a2a4caa6493bf9a54223fea92a1e253f6c724
ILAKAUSHIK <70168212+ILAKAUSHIK@users.noreply.github.com>
2022-05-13 20:00:42 +0530

    Update pg230_q003.c

--- pg230_q003.c
+++ pg230_q003.c
@@ -17,5 +17,20 @@ int main (int argc, char* argv[] ){
     * 1
     * Output:
     * 1.00 */
+    
+    float arr [10] = {0};
+    float average = 0.0;
+    int i=0;
+    for(i=0; i<10; i++) 
+    {
+        scanf("%f",&arr[i]);
+    }
+    
+    for(i=0; i<10; i++) 
+    {
+        average += arr[i];
+    }
+    average /= 10;
+    printf("Average of array is %0.2f", average);
     return 0;
 }
