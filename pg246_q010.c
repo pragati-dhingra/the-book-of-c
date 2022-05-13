091a7a640f7d8008f4178524a303b7437d9bdd81
pragya-agarwal <71170692+pragya-agarwal@users.noreply.github.com>
2022-05-13 12:28:55 +0530

    Update pg246_q010.c

--- pg246_q010.c
+++ pg246_q010.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+int fun(int *);
 int main (int argc, char* argv[] ){
     /* Write a function that takes an int pointer as argument and updates the value at that location to next highest perfect square.
     * If input number was perfect square then it should remain unchanged
@@ -11,5 +11,27 @@ int main (int argc, char* argv[] ){
     * output:
     * "9" without quotes - no trailing space or newline 
     * assume all inputs will be positive numbers */
+    int number=0,square=0;
+    scanf("%d",&number);
+    square = fun(&number);
+    printf("%d",square);
     return 0;
+}
+
+int fun(int *ptr)
+{
+    int n=*ptr,i=1;
+
+    while(i*i < n) {
+        i++;
+    }
+    
+    if(i * i == n)
+        return n;
+    else
+    {
+        return i*i;
+    }
+    
+    
 }
