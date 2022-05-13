db501a73e23668e1d246058da6df253d855188db
VinaySingh13 <101443086+VinaySingh13@users.noreply.github.com>
2022-05-13 21:10:24 +0530

    Update pg230_q002.c

--- pg230_q002.c
+++ pg230_q002.c
@@ -17,5 +17,13 @@ int main (int argc, char* argv[] ){
     * 1
     * Output:
     * 190 */
+    int arr[]={10,89,-25,0,190,65,-300,90,76,1};
+    int max=arr[0];
+    for(int i=0;i<10;i++){
+        if (arr[i]>max){
+            max=arr[i];
+        }
+    }
+    printf("Max value in array is %d\n", max);
     return 0;
 }
