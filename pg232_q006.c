17cf31805acf1d39ca12f7b3c66fbe8e023da721
SilkiKharaliya <101621747+SilkiKharaliya@users.noreply.github.com>
2022-05-13 12:15:49 +0530

    Update pg232_q006.c

--- pg232_q006.c
+++ pg232_q006.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+int find(int arr[], int val);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads ten int as input in an array, one int per line
@@ -22,5 +22,27 @@ int main (int argc, char* argv[] ){
     * 17
     * Output:
     * no */
+    int arr[10];
+    int i=0, val1=0, index;
+    for(i=0;i<10;i++){
+        scanf("%d\n",&arr[i]);
+    } scanf("%d",&val1);
+    
+    index= find(arr,val1);
+    if(index==-1){
+        printf("no");
+    }
+    else{
+        printf("yes");
+               }
+    
     return 0;
+}
+int find(int arr[], int val){
+    for(int i=0; i<10; i++) {
+        if(arr[i] == val) {
+            return i;
+        }
+    }
+    return -1;
 }
