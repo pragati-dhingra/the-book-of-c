215afe358a1ef3dda263bcac5d74af2a96d001fa
VinaySingh13 <101443086+VinaySingh13@users.noreply.github.com>
2022-05-13 21:59:46 +0530

    Update pg230_q002.c

--- pg230_q002.c
+++ pg230_q002.c
@@ -17,5 +17,18 @@ int main (int argc, char* argv[] ){
     * 1
     * Output:
     * 190 */
+    int arr[10]={0};
+    int i=0;
+    int max=0;
+    for(i=0;i<=10;i++)
+    scanf("%d",&arr[i]);
+    max=arr[0];
+     for(int i=1; i<10; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    printf("%d", max);
     return 0;
 }
+ 
