eb42e5979a2fcd732de7cb8777a88f40317ee766
Divya-021 <90896647+Divya-021@users.noreply.github.com>
2022-05-13 23:02:06 +0530

    Update pg232_q005.c

--- pg232_q005.c
+++ pg232_q005.c
@@ -1,34 +1,7 @@
 #include <stdio.h>
- float compute_average(int marks[], int size) 
- {
-     int i=0, sum=0;
-     for(i=0; i<10; i++) {
-         sum = sum + marks[i];
-    }
-     return ((1.0*sum)/10);
- }
-
-    
-    int find_max(int marks[], int size) {
-    int max = marks[0];
-    for(int i=1; i<10; i++) {
-        if(marks[i] > max) {
-            max = marks[i];
-        }
-    }
-    return max;
-}
- 
-int find_min(int marks[], int size)
-{
-    int min = marks[0];
-    for(int i=1; i<10; i++) {
-        if(marks[i] < min) {
-            min = marks[i];
-        }
-    }
-    return 0;
-}
+float compute_average(int marks[]);
+int find_max(int marks[]);
+int find_min(int marks[]);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads ten int as input in an array, one int per line
@@ -47,19 +20,46 @@ int main (int argc, char* argv[] ){
     * 10
     * Output:
     * "5.50 10 1 " without quotes*/
-    int marks[10] = { 0 };
-    int i=0;
-    float average = 0.0;
-    int max =0;
-    int min =0;
-    for(i=0; i<10 ;i++)
-    {
-            scanf("%d",&marks[i]);
-     }
-     average = compute_average(marks, 10);
-     printf("%0.2f ",average);
-     max = find_max(marks,10);
-     min = find_min(marks,10);
-     printf("%d %d ",max,min);    
-    return 0;
+    
+int marks[10];
+     int i=0, maxx=0, minn=0;
+     float average = 0.0;
+
+     for(i=0; i<9; i++)  {
+           scanf("%d\n",&marks[i]);
+     }scanf("%d",&marks[i]);
+
+     average = compute_average(marks);
+     maxx = find_max(marks);
+     minn = find_min(marks);
+     printf("%.2f %d %d", average, maxx, minn);
+     return 0;
+}
+
+float compute_average(int marks[]) {
+     int i=0, sum=0;
+     for(i=0; i<10; i++) {
+      sum = sum + marks[i];
+}
+     return ((1.0*sum)/10);
+}
+
+int find_max(int marks[]) {
+    int max = marks[0];
+    for(int i=1; i<10; i++) {
+        if(marks[i] > max) {
+            max = marks[i];
+        }
+    }
+    return max;
+}
+ 
+int find_min(int marks[]){
+    int min = marks[0];
+    for(int i=1; i<10; i++) {
+        if(marks[i] < min) {
+            min = marks[i];
+        }
+    }
+    return min;
 }
