ab9a25dc736d84c9cd0f7d7e56c3fe99491ab734
shalini127 <72406579+shalini127@users.noreply.github.com>
2022-05-13 13:44:20 +0530

    Update pg248_q012.c

--- pg248_q012.c
+++ pg248_q012.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
+int sum_and_square(int val, int *sum) {
+    *sum = (val * (val+1))/2;
+    printf("%d",*sum);
+    return val*val;
+}
 int main (int argc, char* argv[] ){
     /* Write a function that takes an int as input parameter and an int * as output parameter. 
     * Assuming input int was n
-    * Function should return sum of first n natural number
-    * Function should update output parameter to square of n 
-    * Calling function should simply print the returned value and output parameter value post function call
-    * Example, input:
-    * 5
+@@ -11,5 +15,9 @@ int main (int argc, char* argv[] ){
     * output:
     * "15 25" without quotes - no trailing space or newline 
     * assume all inputs will be positive numbers */
     return 0;
+    int a=0, sum=0, b=0;
+    scanf("%d",&a);
+    b=sum_and_square(a,&sum);
+    printf(" %d",b);
+	return 0;
 }
