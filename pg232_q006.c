133613432d79e91b8ecc752099dc67b97959a88f
sarikatyagi26 <101451781+sarikatyagi26@users.noreply.github.com>
2022-05-13 14:43:11 +0530

    Update pg232_q006.c

--- pg232_q006.c
+++ pg232_q006.c
@@ -22,5 +22,31 @@ int main (int argc, char* argv[] ){
     * 17
     * Output:
     * no */
-    return 0;
+     int arr[10] = {0};
+    int i=0;
+    int val=0;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+ 
+ scanf("%d",&val);
+ i=find_val(arr,10,val);
+ if(i==-1)
+  printf("no");
+ else
+  printf("yes");
+
+return 0;
 }
+    int find_val(int arr[], int size, int val)
+	{ int i=0;
+    	for(i=0; i<10; i++) 
+		{
+        if(arr[i] == val) 
+		{
+            return i;
+        }
+        }
+    return -1;
+    }
