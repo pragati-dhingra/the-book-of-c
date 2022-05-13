410f3072e8a138ae54c06778ba534273c33a5275
surbhivpandey <101626124+surbhivpandey@users.noreply.github.com>
2022-05-13 10:03:44 +0530

    Update pg230_q003.c

--- pg230_q003.c
+++ pg230_q003.c
@@ -17,5 +17,16 @@ int main (int argc, char* argv[] ){
     * 1
     * Output:
     * 1.00 */
+    int i, a[10], sum=0;
+  float ave;
+        for(i=0;i<9;i++)
+        scanf("%d\n",&a[i]);
+       for(i=0;i<10;i++)
+    {
+        sum=sum+a[i];
+    }
+  ave=sum/10;
+           printf("%.2f", ave);
+
     return 0;
 }
