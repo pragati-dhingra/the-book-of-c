07d8dba93512dae8e14eb5536b71231f91ef3df1
shalini127 <72406579+shalini127@users.noreply.github.com>
2022-05-13 13:42:17 +0530

    Update pg246_q010.c

--- pg246_q010.c
+++ pg246_q010.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
+int next_highest_perfect_square(int val) {
+    int i = 1;
+    while(i*i <= val) {
+        i++;
+    }
+    return i*i;
+}
+
+int update_val(int *a) {
+    *a = next_highest_perfect_square(*a);
+    printf("%d", *a);
+}
 int main (int argc, char* argv[] ){
     /* Write a function that takes an int pointer as argument and updates the value at that location to next highest perfect square.
     * If input number was perfect square then it should remain unchanged
-    * Program input - an int on a single line, no trailing spaces
-    * function is called with address of this input int 
-    * Value of same variable is printed after function call
-    * Example, input:
-    * 5
+@@ -11,5 +22,10 @@ int main (int argc, char* argv[] ){
     * output:
     * "9" without quotes - no trailing space or newline 
     * assume all inputs will be positive numbers */
+    int b=0;
+
+    scanf("%d",&b);
+    update_val(&b);
+
     return 0;
 }
