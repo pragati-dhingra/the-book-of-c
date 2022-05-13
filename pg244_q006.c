8064bb3647d239aa645ac8d7c5bdf5bdaa1a8b24
ILAKAUSHIK <70168212+ILAKAUSHIK@users.noreply.github.com>
2022-05-13 20:09:19 +0530

    Update pg244_q006.c

--- pg244_q006.c
+++ pg244_q006.c
@@ -11,5 +11,39 @@ int main (int argc, char* argv[] ){
     * 5 / 3
     * output:
     * 1  */
-    return 0;
+    int num1=0, num2=0;
+    char operator=0;
+ 
+    
+    scanf("%d", &num1);
+    scanf(" %c", &operator);
+    scanf("%d", &num2);
+    
+   
+ int *ptr1 = &num1, *ptr2 = &num2;
+    if('+' == operator) {
+        printf("%d",add(ptr1, ptr2));
+    } else if('-' == operator) {
+       printf("%d",subtract(ptr1, ptr2));
+    } else if('*' == operator) {
+        printf("%d",multiply(ptr1, ptr2));
+    } else if('/' == operator) {
+        printf("%d",divide(ptr1, ptr2));
+    } else {
+        printf("Invalid input");
+    }
+
+return 0;
+}
+int add(int *a, int *b) {
+ return(*a + *b);
+}
+int subtract(int *a, int *b) {
+ return(*a - *b);
+}
+int multiply(int *a, int *b) {
+ return(*a * *b);
+}
+int divide(int *a, int *b) {
+ return(*a / *b);
 }
