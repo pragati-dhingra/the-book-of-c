33d751c43e94d26e41423d802a67e3921366be17
sarikatyagi26 <101451781+sarikatyagi26@users.noreply.github.com>
2022-05-13 15:44:30 +0530

    Update pg232_q005.c

--- pg232_q005.c
+++ pg232_q005.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+float compute_avg(int arr[],int size);
+int compute_max(int arr[],int size);
+int compute_min(int arr[],int size);
 
 int main (int argc, char* argv[] ){
     /* Write a program that:
@@ -18,5 +21,60 @@ int main (int argc, char* argv[] ){
     * 10
     * Output:
     * "5.50 10 1 " without quotes*/
+   int arr[10]={0},max=0,min=0;;
+    float avg=0.0;
+    int i=0;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    avg = compute_avg(arr,10);
+    max= compute_max(arr,10);
+    min = compute_min(arr,10);
+
+    printf("%.2f %d %d",avg,max,min);
+
     return 0;
+}
+
+float compute_avg(int arr[],int size)
+{
+    int i=0,sum=0;
+    float avg=0.0;
+    for(i=0;i<size;i++)
+    {
+        sum = sum +arr[i];
+    }
+    avg = sum*1.0/size;
+
+    return avg;
+}
+
+int compute_max(int arr[],int size)
+{
+    int i=0,max = 0;
+    for(i=0;i<size;i++)
+    {
+        if(arr[i]>max)
+        {
+            max = arr[i];
+
+        }
+    }
+    return max;
+}
+
+int compute_min(int arr[],int size)
+{
+     int i=0,min = arr[0];
+    for(i=0;i<size;i++)
+    {
+        if(arr[i]<min)
+        {
+            min = arr[i];
+
+        }
+    }
+    return min;
 }
