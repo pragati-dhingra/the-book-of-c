a479b9cefcf4ec2a86ae8d0d86758dd451098493
Divya-021 <90896647+Divya-021@users.noreply.github.com>
2022-05-13 13:39:42 +0530

    Update pg233_q007.c

--- pg233_q007.c
+++ pg233_q007.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#define ARRAY_SIZE 10
 
+void read_arr(int arr[], int size);
+void add_arr(int arr1[], int arr2[], int result[], int size);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads twenty int as input in two arrays, ten int per array, one int per line
@@ -32,5 +35,28 @@ int main (int argc, char* argv[] ){
     * "12 14 16 18 20 22 24 26 28 30 "
     * without quotes.
     */
+   int arr1[ARRAY_SIZE] = { 0 };
+    int arr2[ARRAY_SIZE] = { 0 };
+    int result[ARRAY_SIZE] = { 0 };
+ 
+    read_arr(arr1, ARRAY_SIZE);
+    read_arr(arr2, ARRAY_SIZE);
+    add_arr(arr1, arr2, result, ARRAY_SIZE);
+    
+    for(int i=0; i<ARRAY_SIZE; i++) {
+        printf("%d ",result[i]);
+    }
     return 0;
+}
+ 
+void read_arr(int arr[], int size) {
+    for(int i=0; i<size; i++) {
+         scanf("%d", &arr[i]);
+    }
+}
+ 
+void add_arr(int arr1[], int arr2[], int result[], int size) {
+    for(int i=0; i<size; i++) {
+        result[i] = arr1[i] + arr2[i];
+    }
 }
