d9de0605166ba424fa25e321cc7d643973852d8e
harshvardhan2520 <101621531+harshvardhan2520@users.noreply.github.com>
2022-05-14 10:01:59 +0530

    Update pg232_q006.c

--- pg232_q006.c
+++ pg232_q006.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-
+    int find_val(int arr[], int size, int val)
+	{ int i=0;
+    	for(i=0; i<size ; i++) 
+		{
+        if(arr[i] == val) 
+		{
+            return i;
+        }
+        }
+    return -1;
+    }
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads ten int as input in an array, one int per line
@@ -22,5 +32,20 @@ int main (int argc, char* argv[] ){
     * 17
     * Output:
     * no */
-    return 0;
+    int arr[10] = {0};
+    int i=0;
+    int val=0;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+ 
+ scanf("%d",&val);
+ i=find_val(arr,10,val);
+ if(i==-1)
+  printf("no");
+ else
+  printf("yes");
+
+return 0;
 }
