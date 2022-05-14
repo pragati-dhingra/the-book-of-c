e80f7331cda8db51b6eaefd09f0ed85bc6f233b3
Dr. Amit Kumar <65865744+awadheshamit@users.noreply.github.com>
2022-05-14 10:18:58 +0530

    Update pg244_q006.c

--- pg244_q006.c
+++ pg244_q006.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
+
     /* Write a calculator program for integer arithmetic that supports add, multiply, subtract, and divide operations 
     * Respective operators are + * - /  
     * Each operation should be implemented as a separate function
@@ -11,5 +11,47 @@ int main (int argc, char* argv[] ){
     * 5 / 3
     * output:
     * 1  */
-    return 0;
+    int add(int *a, int *b);
+int subtract(int *a, int *b);
+int multiply(int *a, int *b);
+int divide(int *a, int *b);
+
+int main (int argc, char * argv[]) {
+    int num1=0, num2=0;
+    char operator=0;
+ 
+    printf("enter operand1");
+    scanf("%d", &num1);
+    printf("enter operator(+,-,*,/)");
+    scanf(" %c", &operator);
+    printf("enter operand2");
+    scanf("%d", &num2);
+    
+   
+ int *ptr1 = &num1, *ptr2 = &num2;
+    if('+' == operator) {
+        printf("%d",add(ptr1, ptr2));
+    } else if('-' == operator) {
+       printf("%d",subtract(ptr1, ptr2));
+    } else if('*' == operator) {
+        printf("%d",multiply(ptr1, ptr2));
+    } else if('/' == operator) {
+        printf("%d",divide(ptr1, ptr2));
+    } else {
+        printf("Invalid input");
+    }
+
+return 0;
+}
+int add(int *a, int *b) {
+ return(*a + *b);
+}
+int subtract(int *a, int *b) {
+ return(*a - *b);
+}
+int multiply(int *a, int *b) {
+ return(*a * *b);
+}
+int divide(int *a, int *b) {
+ return(*a / *b);
 }
