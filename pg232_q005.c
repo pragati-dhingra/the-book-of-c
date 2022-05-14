1dfbf52384d234fbedd18466784200e7a5c07000
harshvardhan2520 <101621531+harshvardhan2520@users.noreply.github.com>
2022-05-14 09:55:13 +0530

    Update pg232_q005.c

--- pg232_q005.c
+++ pg232_q005.c
@@ -18,5 +18,45 @@ int main (int argc, char* argv[] ){
     * 10
     * Output:
     * "5.50 10 1 " without quotes*/
-    return 0;
+int marks[10];
+     int i=0, maxx=0, minn=0;
+     float average = 0.0;
+
+     for(i=0; i<9; i++)  {
+           scanf("%d\n",&marks[i]);
+     }scanf("%d",&marks[i]);
+
+     average = compute_average(marks);
+     maxx = find_max(marks);
+     minn = find_min(marks);
+     printf("%.2f %d %d", average, maxx, minn);
+     return 0;
+}
+
+float compute_average(int marks[]) {
+     int i=0, sum=0;
+     for(i=0; i<10; i++) {
+      sum = sum + marks[i];
+}
+     return ((1.0*sum)/10);
+}
+
+int find_max(int marks[]) {
+    int max = marks[0];
+    for(int i=1; i<10; i++) {
+        if(marks[i] > max) {
+            max = marks[i];
+        }
+    }
+    return max;
+}
+ 
+int find_min(int marks[]){
+    int min = marks[0];
+    for(int i=1; i<10; i++) {
+        if(marks[i] < min) {
+            min = marks[i];
+        }
+    }
+    return min;
 }
