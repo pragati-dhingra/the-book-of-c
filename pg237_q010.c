23448f5745a112be7ec73793160c7b87685e2b91
shalika-210 <66405566+shalika-210@users.noreply.github.com>
2022-05-16 16:15:32 +0530

    Update pg237_q010.c

--- pg237_q010.c
+++ pg237_q010.c
@@ -4,9 +4,10 @@ int main (int argc, char* argv[] ){
     /* Write a program that:
     * 1. Reads fifteen int as input in a 2D array marks[5][3] - marks of 5 students in 3 subjects
     * one int per line, marks of first student in each subject followed by marks of second student and so on
-    * 2. Prints the overall class average on a single line
-    * one float with two decimal places - average of entire 2D array
-    * no trailing spaces or newline.
+    * 2. Prints the average mark of each student on a single line
+    * one float with two decimal places per student
+    * each float separated by single space
+    * no extra trailing newline.
     * For example: Input 
     * 1
     * 2
@@ -24,6 +25,27 @@ int main (int argc, char* argv[] ){
     * 14
     * 15
     * Output:
-    *  8.00 */
+    * "2.00  5.00 8.00 11.00 14.00 "
+    * without quotes.
+    */
+    int marks[5][3]={0},i=0,j=0,sum=0;
+    float average = 0.0;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&marks[i][j]);
+        }
+    }
+    for(i=0;i<5;i++)
+    {   
+        for(j=0;j<3;j++)
+        {
+            sum = sum +marks[i][j];
+        }
+       
+    }
+        average = (sum*1.0)/15;
+        printf("%.2f ",average);
     return 0;
 }
