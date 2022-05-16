92b948e747003cd2e19b01de84f193caa67f05de
shalika-210 <66405566+shalika-210@users.noreply.github.com>
2022-05-16 16:21:04 +0530

    Update pg246_q009.c

--- pg246_q009.c
+++ pg246_q009.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void swap(int *, int *);
+
 int main (int argc, char* argv[] ){
     /* Write a function that swaps two ints using pointers
     * Input - two integer values on a single line separated by a single space
@@ -10,5 +12,19 @@ int main (int argc, char* argv[] ){
     * 5  3
     * output:
     * "3 5" without quotes - no trailing space or newline */
+    int num1=0, num2=0;
+    scanf("%d%d",&num1,&num2);
+    swap(&num1,&num2);
+    printf("%d %d",num1,num2);
     return 0;
+    
+}
+
+void swap(int *a,int *b)
+{
+    int temp=0;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+    
 }
