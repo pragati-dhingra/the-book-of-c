55ad81dca298a3871075d6e3aa5a1d582697fc8e
pragati-dhingra <38684074+pragati-dhingra@users.noreply.github.com>
2022-05-17 17:54:04 +0530

    Reading n inputs and finding average

---
+++ pg257_q021.c
@@ -0,0 +1,19 @@
+int main (int argc, char* argv[] ){
+    /* Write a program that reads in an int n where n<30. This is the number of input values.
+    * Followed by n float inputs, one on each line 
+    * and prints average of last 7 inputs on a line by itself
+    * followed by average of all n values on a line by itself
+    * If n < 7 the first output should be printed as 0
+    * Both output values should have two decimal places and no trailing space or newline  
+    * For example, input:
+    * 3
+    * 42.2
+    * 44.4
+    * 47.0
+    * Output: 
+    * 0.00
+    * 44.53
+    * Error checking value of n is not expected. 0 < n < 30 
+    */
+    return 0;
+}
