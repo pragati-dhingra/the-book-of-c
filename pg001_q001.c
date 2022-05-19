00fddfc4e2ac2246d95aeac317e35ed3b1f7c150
naman-c52 <105838649+naman-c52@users.noreply.github.com>
2022-05-19 11:04:57 +0530

    Update pg001_q001.c

--- pg001_q001.c
+++ pg001_q001.c
@@ -6,5 +6,6 @@ int main (int argc, char* argv[] ){
    * Once updated, submit a pull request. 
    * You should receive a comment with results on your PR and email notification (unless you have disabled notifications) */
   printf("Hello World!");
+  
   return 0;
 }
