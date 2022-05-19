3f4b617b99e679917864538f3fcf2c7cb56548ff
umangrastogig <101452375+umangrastogig@users.noreply.github.com>
2022-05-19 17:48:32 +0530

    Update pg270_q009.c

--- pg270_q009.c
+++ pg270_q009.c
@@ -15,5 +15,26 @@ int main (int argc, char* argv[] ){
     * If longer string is presented as input, use only first 29 chars
     * Include error checks for the value of n. It should be >0 and <30 
     * If value of n is outside this range then print output value as -2 instead of above values */
-    return 0;
+   char str1[20];
+   char str2[20];
+   int result;
+
+   //Assigning the value to the string str1
+   strcpy(str1, "hello");
+
+   //Assigning the value to the string str2
+   strcpy(str2, "helLO WORLD");
+
+   //This will compare the first 3 characters
+   result = strncmp(str1, str2, 3);
+
+   if(result > 0) {
+      printf("ASCII value of first unmatched character of str1 is greater than str2");
+   } else if(result < 0) {
+      printf("ASCII value of first unmatched character of str1 is less than str2");
+   } else {
+      printf("Both the strings str1 and str2 are equal");
+   }
+
+   return 0;
 }
