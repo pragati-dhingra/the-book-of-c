86342fbefb2899aaf7e5af671757c6d97d32e897
sandeepkec <101444259+sandeepkec@users.noreply.github.com>
2022-05-19 16:09:48 +0530

    Update pg113_q019.c

--- pg113_q019.c
+++ pg113_q019.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[] ){
-    /* Print the following string including the quotes
-       "In C \\ prints \ on console"
-     */
-       printf("\"In C \\prints \\ on console\"");
-    
+   
+    printf("\"In C \\\\ prints \\ on console\"");
+    printf("\n");
     return 0;
-}
+    }
