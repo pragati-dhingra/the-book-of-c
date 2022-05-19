88d5a5efe924bc646c1984a58fa76b2e63680a01
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-05-19 14:28:07 +0530

    Update pg264_q001.c

--- pg264_q001.c
+++ pg264_q001.c
@@ -10,5 +10,18 @@ int main (int argc, char* argv[] ){
     * I AM LEVEL 7 NINJA
     * Assume string length would be <128
     */
-  return 0;
+    char str[31] = { '\0' };
+ 
+    printf("Enter a string (up to 30 characters) : ");
+    scanf("%30[^\n]s",str);
+ 
+    for(int i=0; i<31 && str[i]; i++) {
+        if(str[i]>='a' && str[i]<='z') {
+            printf("%c",str[i]-'a'+'A');
+        } else {
+            printf("%c",str[i]);
+        }
+    }
+}
+    return 0;
 }
