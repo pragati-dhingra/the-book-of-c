4a85290d8c96555e6f856bba77f2f3e94ada0ffc
Meraj-b18 <105708866+Meraj-b18@users.noreply.github.com>
2022-05-20 12:39:01 +0530

    Update pg001_q001.c

--- pg001_q001.c
+++ pg001_q001.c
@@ -2,7 +2,7 @@
 
 int main (int argc, char* argv[] ){
   /* This is a sample question to make sure your setup is working correctly
-   * Update this program to printf("Hello World!") without the quotes
+   * Update this program to printf("Hello World!")without the quotes
    * Once updated, submit a pull request. 
    * You should receive a comment with results on your PR and email notification (unless you have disabled notifications) */
   
