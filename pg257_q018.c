cd57ed8089eabaeb1b79684bbc4b9d16744d263d
ShivaliTyagi <101444334+ShivaliTyagi@users.noreply.github.com>
2022-05-20 14:43:06 +0530

    Update pg257_q018.c

--- pg257_q018.c
+++ pg257_q018.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+int add_array(int * arr, int size) 
+{
+    int result = 0, i = 0;
+ 
+    for(i=size-1; i>=0; arr++,i--) 
+    {
+        result += *arr;
+    }
+    return result;
+}
 int main (int argc, char* argv[] ){
     /* Write a program that reads in five integers in an array, one integer per line
     * Defines a function add_array that adds all ints of the array using a countdown loop
@@ -12,5 +22,17 @@ int main (int argc, char* argv[] ){
     * Output: 
     * "38" without quotes - no trailing space or newline 
     */
+    
+   int i = 0, r = 0, a[5] = { 0 };
+
+    for (i = 0; i < 5; i++)
+    {
+        scanf_s("%d", &a[i]);
+    }
+
+    r = add_array(a, 5);
+
+    printf("%d",r);
+     
   return 0;
 }
