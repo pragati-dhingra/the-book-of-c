1f0d0528ba5a8bcc9743047bf01983a1912dcc30
anujkec <101444151+anujkec@users.noreply.github.com>
2022-05-20 16:30:45 +0530

    Update pg268_q003.c

--- pg268_q003.c
+++ pg268_q003.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
-
 int main (int argc, char* argv[] ){
-    /* Write a program that reads in a string terminated by newline from the user
-    * and prints the number of small case alphabets in the string - on a line by itself with no trailing newline
-    * For example, input:
-    * I am level 7 Ninja
-    * Output
-    * 11
-    * Assume string length would be <30 (array size). Guard against longer input strings. 
-    * If user inputs a longer string then only first 29 characters should be processed
-    */
-    return 0;
+ /* Write a program that reads in a string terminated by newline from the
+user
+ * and prints the number of small case alphabets in the string - on a
+line by itself with no trailing newline
+ * For example, input:
+ * I am level 7 Ninja
+ * Output
+ * 11
+ * Assume string length would be <30 (array size). Guard against longer
+input strings.
+ * If user inputs a longer string then only first 29 characters should be
+processed
+ */
+ char str[31] = { '\0' };
+ int count=0;
+ scanf("%30[^\n]s",str);
+ for(int i=0; i<29 && str[i]; i++) {
+ if(str[i]>='a' && str[i]<='z') {
+ count++;
+ }
+ }
+ printf("%d", count);
+ return 0;
 }
+
