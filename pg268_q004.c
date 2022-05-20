20451b6b8c8504b395b69b96f82123d9cbc384dd
Priya-20961 <86092853+Priya-20961@users.noreply.github.com>
2022-05-20 18:34:27 +0530

    Update pg268_q004.c

--- pg268_q004.c
+++ pg268_q004.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main (int argc, char* argv[] ){
     /* Write a program that reads in a string terminated by newline from the user
     * and prints the number of non-alphabet characters in the string - on a line by itself with no trailing newline
@@ -10,5 +9,17 @@ int main (int argc, char* argv[] ){
     * Assume string length would be <30 (array size). Guard against longer input strings. 
     * If user inputs a longer string then only first 29 characters should be processed
     */
+    char str[31] = { '\0' };
+    int count=0;
+
+    scanf("%30[^\n]s",str);
+
+    for(int i=0; i<29 && str[i]; i++) {
+        if(!((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))) {
+            count++;
+        }
+    }
+    printf("%d", count);
+
     return 0;
 }
