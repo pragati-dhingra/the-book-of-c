03708e0a20d68c8d248067c747d6fb3d3704c71a
priya-a14 <105715787+priya-a14@users.noreply.github.com>
2022-05-20 10:57:24 +0530

    Update pg001_q001.c

--- pg001_q001.c
+++ pg001_q001.c
@@ -2,7 +2,7 @@
 
 int main ( ){
   /* This is a sample question to make sure your setup is working correctly
-   * Update this program to printllo World!" without the quotes
+   * Update this program to print "Hello World!" without the quotes
    * Once updated, submit a pull request. "He 
    * You should receive a comment with results on your PR and email notification (unless you have disabled notifications) */
   printf("Hello World!");
