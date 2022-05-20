7be48377da9139e288a5287f1f894348de1f0c70
shalini311 <58061627+shalini311@users.noreply.github.com>
2022-05-20 21:42:26 +0530

    Update pg271_q010.c

--- pg271_q010.c
+++ pg271_q010.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
-
-int main (int argc, char* argv[] ){
-    /* Write an implementation of strcpy using while loop. 
-    * Your program reads a string on a line by itself, terminated by \n
-    * and calls a function that copies this string into destination string. The destination string is passed as a parameter by calling function
-    * Function should return pointer to destination string
-    * Function is expected to copy all chars until \0.
-    * Buffer overflow check is not expected.
-    * Use while loop for copying 
-    * Do not use library strcpy function
-    * Returned string is printed by the calling function
-    * Assume max string size of 32 chars including \0 */
-    return 0;
+char * my_strcpy (char *dest, const char *src) {
+    int i = 0;
+ 
+    /*Loop terminates when end of src string is reached*/
+    while('\0' != src[i]) {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0'; /*Terminate dest with null byte*/
+    return dest;
 }
