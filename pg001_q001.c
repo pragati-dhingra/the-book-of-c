b069d9de2b3a0bb52d0229d35c534720e96257db
kartik-b2 <105708685+kartik-b2@users.noreply.github.com>
2022-05-20 12:26:49 +0530

    Update pg001_q001.c

--- pg001_q001.c
+++ pg001_q001.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[] ){
-  /* This is a sample question to make sure your setup is working correctly
-   * Update this program to print "Hello World!" without the quotes
-   * Once updated, submit a pull request. 
-   * You should receive a comment with results on your PR and email notification (unless you have disabled notifications) */
-  
-  return 0
+ 
+  printf("Hello world!");
+  return 0;
 }
