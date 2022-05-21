4c9a98d713072ea8e8f3cd7fb4469f05362399c9
Dr. Amit Kumar <65865744+awadheshamit@users.noreply.github.com>
2022-05-21 10:08:43 +0530

    Update pg257_q021.c

--- pg257_q021.c
+++ pg257_q021.c
@@ -15,5 +15,33 @@ int main (int argc, char* argv[] ){
     * 44.53
     * Error checking value of n is not expected. 0 < n < 30 
     */
+    int n=0,i=0,x=0;
+        float arr[30]={0.0};
+        float avg = 0,sum=0;
+        scanf("%d",&n);
+        for(i=0; i<n;i++) {
+            scanf("%f",&arr[i]);
+        }
+        if(n<7)
+        {
+         printf("%.2f",avg);   
+        }
+        
+        for(i=0;i<n;i++){
+		
+		sum=sum+arr[i];
+		x++;
+		if(x==7){
+			avg=sum/7;
+			printf("%.2f",avg);
+		}
+		else{
+			continue;
+		}
+	}
+	avg=sum/n;
+	printf("%.2f",avg);
+      
+        
     return 0;
 }
