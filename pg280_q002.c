286de365f3c07e1eecf2e160dae3314257372874
umangrastogig <101452375+umangrastogig@users.noreply.github.com>
2022-05-21 14:36:25 +0530

    Update pg280_q002.c

--- pg280_q002.c
+++ pg280_q002.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+double compute_average(int *, int);
+void read_score(int *, int);
+ 
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * Reads an int n from the user - this is the number of input values user wishes to enter
@@ -8,5 +10,42 @@ int main (int argc, char* argv[] ){
     * Calls a function to read in n int values, one value on each line
     * Calls a function that computes average of these n values and returns the average
     * Calling function prints the average value with no trailing spaces or newline. Printed average should have 2 decimal places */
+
+    int *score = NULL, num_s = 0;
+    double avg = 0;
+ 
+    printf ("Please enter number of students: ");
+    scanf("%d",&num_s);
+    if (num_s <= 0) {
+        printf ("Invalid input. Terminating\n");
+        return 1;
+    }
+ 
+    score = malloc (sizeof(int) * num_s);
+    if (NULL == score) {
+        printf ("Failed to allocate memory. Terminating\n");
+        return 1;
+    }
+ 
+    read_score(score, num_s);
+    average = compute_average(score, num_s);
+    printf("Average score of the class is %f\n", average);
+ 
+    free(score);
     return 0;
+}
+ 
+double compute_average(int *score, int num){
+    int sum = 0;
+    for(int i=0; i<num; i++){
+        sum += score[i];
+    }
+    return ((double)sum)/num;
+}
+ 
+void read_score(int *score, int num) {
+    for(int i=0; i<num; i++){
+        printf("Enter score of student #%d: ",i+1);
+        scanf("%d", &score[i]);
+    }
 }
