f592c8742330f45ac50b931e561257cb6bde2be0
umangrastogig <101452375+umangrastogig@users.noreply.github.com>
2022-05-21 14:41:43 +0530

    Update pg280_q002.c

--- pg280_q002.c
+++ pg280_q002.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+double compute_average(int *, int);
+void read_score(int *, int);
 int main (int argc, char* argv[] ){
     /* Write a program that:
     * Reads an int n from the user - this is the number of input values user wishes to enter
@@ -8,5 +9,39 @@ int main (int argc, char* argv[] ){
     * Calls a function to read in n int values, one value on each line
     * Calls a function that computes average of these n values and returns the average
     * Calling function prints the average value with no trailing spaces or newline. Printed average should have 2 decimal places */
+     int *score = NULL, num = 0;
+    double average = 0;
+ 
+    scanf("%d",&num);
+    if (num<= 0) {
+        printf ("Invalid input\n");
+        return 1;
+    }
+ 
+    score = malloc (sizeof(int) * num);
+    if (NULL == score) {
+            return 1;
+    }
+    
+    read_score(score, num);
+    average = compute_average(score, num);
+    printf("%.2f", average);
+ 
+    free(score);
     return 0;
+}
+ 
+double compute_average(int *score, int num){
+    int sum = 0;
+    for(int i=0; i<num; i++){
+        sum += score[i];
+    }
+    return ((double)sum)/num;
+}
+ 
+void read_score(int *score, int num) {
+    for(int i=0; i<num; i++){
+        
+        scanf("%d", &score[i]);
+    }
 }
