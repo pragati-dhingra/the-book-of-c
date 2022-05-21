0a9f4ae602789081b9b22f3bb5864baf52352c1d
Dr. Amit Kumar <65865744+awadheshamit@users.noreply.github.com>
2022-05-21 15:33:28 +0530

    Update pg357_q005.c

--- pg357_q005.c
+++ pg357_q005.c
@@ -1,15 +1,15 @@
-/*#include <stdio.h>
+/*
 
-int main (int argc, char* argv[] ){
+
     /* Write a recursive program that:
     * Reads an int n as input, on a line by itself
     * And prints the nth Fibonacci number as output 
     * If input was negative number then 0 should be printed
     * F(0) = 0, F(1) = 1
     * Output is on a line by itself with no trailing whitespace or newline */
-    return 0;
-}*/
-    #include <stdio.h>
+   
+*/
+#include <stdio.h>
 void f(int);
 
 int main(int argc, char *argv[])
