fe7fcb19640b929e91968fd3619d3153c78d17cf
pragya-agarwal <71170692+pragya-agarwal@users.noreply.github.com>
2022-05-21 14:03:01 +0530

    Update pg271_q010.c

--- pg271_q010.c
+++ pg271_q010.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+char * my_strcpy (char *dest,  char *src);
 int main (int argc, char* argv[] ){
     /* Write an implementation of strcpy using while loop. 
     * Your program reads a string on a line by itself, terminated by \n
@@ -11,5 +11,28 @@ int main (int argc, char* argv[] ){
     * Do not use library strcpy function
     * Returned string is printed by the calling function
     * Assume max string size of 32 chars including \0 */
+    
+    char str_src[32]={'\0'};
+    char str_dest[32]={'\0'};
+    char *ptr =NULL;
+    
+    scanf("%31[^\n]s",str_src);
+    
+    ptr = my_strcpy(str_dest,str_src);
+    
+    printf("%s",str_dest);
+    
     return 0;
+}
+
+char * my_strcpy (char *dest,  char *src) {
+    int i = 0;
+ 
+    /*Loop terminates when end of src string is reached*/
+    while(src[i] !='\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0'; /*Terminate dest with null byte*/
+    return dest;
 }
