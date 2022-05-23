30c47b1737a6cc11b791915c14b1f67202a641bb
Divya-021 <90896647+Divya-021@users.noreply.github.com>
2022-05-23 13:53:01 +0530

    Update pg257_q018.c

--- pg257_q018.c
+++ pg257_q018.c
@@ -12,22 +12,25 @@ int main (int argc, char* argv[] ){
     * Output: 
     * "38" without quotes - no trailing space or newline 
     */
-    int arr[5]={0};
-	int i=0;	
-	for(i=0;i<5;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
-	printf("%d",add_array(arr,5));
-	
-	return 0;
+    #include <stdio.h>
+int sum_array(int arr[], int size);
+ 
+
+int main (int argc, char* argv[]) {
+    int arr[4] = { 0 },i=0;
+    int sum=0;
+    for(i=0; i<5; i++) {
+    scanf("%d",&arr[i]);
+    }
+    sum = sum_array(arr, 4);
+    printf("%d",sum);
 }
-int add_array(int arr[],int size)
-{
-	int i=0,sum=0;
-	for(i=size;i>=0;i--)
-	{
-		sum=sum+arr[i];
-	}
-  return 0;
+
+int sum_array(int * arr, int size) {
+    int result = 0, i = 0;
+ 
+    for(i=size; i>=0; i--,arr++) {
+        result += *arr;
+    }
+    return result;
 }
