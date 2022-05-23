af962b3ca58555bc7825bebae6775fad161756c2
aayush-a27 <105729140+aayush-a27@users.noreply.github.com>
2022-05-23 15:34:15 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
+int main (int argc, char* argv[] ) {
   /* Q: Read value in inches and converts it into cms. 1 inch = 2.54 cms 
    * Input one double value followed by a newline
    * Output one double value with two decimal places on a line by itself - no newline.
