3c0d2c2772cf8e56a10b1e2e3981f289fce50d2b
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-05-23 16:27:43 +0530

    Update pg271_q012.c

--- pg271_q012.c
+++ pg271_q012.c
@@ -1,23 +1,5 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
-    /* Write an implementation of strncpy using while loop. 
-    * Your program reads a int on a line by itself. This is the max number of chars to copy.
-    * Followed by a string on a line by itself, terminated by \n
-    * and calls a function that copies this string into destination string. The destination string is passed as a parameter by calling function
-    * Function should return pointer to destination string
-    * Function is expected to copy all chars until \0.
-    * Buffer overflow check is not expected.
-    * At most n chars should be copied where n was the number input by user
-    * Returned string is printed by the calling function
-    * Assume max string size of 32 chars including \0
-    * Include error checks for n, 0<n<32
-    * If input value was outside this range then skip the call to strncpy function and print -1
-    * Note that input string would still be read in this case
-    * Do not use strncpy library function
-    * If source string had less than n chars then simply copy up to terminating \0  */
-    #include <stdio.h>
-
 char * my_strncpy (char *dest, const char *src, unsigned int n);
 
 int main (int argc, char* argv[] ){
@@ -42,7 +24,8 @@ int main (int argc, char* argv[] ){
     int x=-1;
     
     scanf("%d",&n);  
-  
+    
+
        
     if(n>0 && n<32){
     scanf(" %30[^\n]s",str1);	
@@ -60,6 +43,5 @@ char * my_strncpy (char *dest, const char *src, unsigned int n) {
         dest[i] = src[i];
     }
     return dest;
-}
-    return 0;
+    
 }
