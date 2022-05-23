1f866afea3c68c7a06ce6a528ef3b8b4b9c94e78
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-05-23 11:17:24 +0530

    Update pg357_q005.c

--- pg357_q005.c
+++ pg357_q005.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
-
-int main (int argc, char* argv[] ){
+void f(int);
+int main(int argc, char* argv[] ){
     /* Write a recursive program that:
     * Reads an int n as input, on a line by itself
     * And prints the nth Fibonacci number as output 
     * If input was negative number then 0 should be printed
     * F(0) = 0, F(1) = 1
     * Output is on a line by itself with no trailing whitespace or newline */
+    int n = 0;
+    
+    scanf("%d", &n);
+    fflush(stdin);
+    f(n);
+    
     return 0;
 }
+void f(int n){
+    int ctr = 2;
+
+    int x1 = 0, x2 = 1, temp = 0;
+    
+    
+    if (n <= 0)
+    {
+        printf("0");
+        
+    }
+    else if(n == 1)
+    { 
+        printf("1");
+        
+    }
+    else
+    {
+
+        while (ctr < n)
+        {
+            temp = x1 + x2;
+            x1 = x2;
+            x2 = temp;
+            ctr++;
+        }
+        printf("%d", temp);
+    }
+
+}
+    
+ 
