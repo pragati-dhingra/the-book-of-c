19320487b36c748bf695daa7b4e824232967ec8b
sandeepkec <101444259+sandeepkec@users.noreply.github.com>
2022-05-23 12:13:44 +0530

    Update pg357_q005.c

--- pg357_q005.c
+++ pg357_q005.c
@@ -9,3 +9,26 @@ int main (int argc, char* argv[] ){
     * Output is on a line by itself with no trailing whitespace or newline */
     return 0;
 }
+#include <stdio.h>
+ 
+unsigned long int fibonacci(unsigned long n) {
+    if(0 == n) {
+        return 0;
+    }
+    if(1 == n) {
+        return 1;
+    }
+    return (fibonacci(n-1) + fibonacci(n-2));
+}
+ 
+int main (int argc, char *argv[]) {
+    unsigned int n = 0;
+    unsigned long result = 0;
+ 
+        scanf("%u", &n);
+ 
+    result = fibonacci(n);
+    printf("F(%u) = %lu\n", n, result);
+    return 0;
+}
+
