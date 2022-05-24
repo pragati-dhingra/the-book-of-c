01f5f6eea289efb928df848bf7fcddb288952f17
ishita2601 <105716171+ishita2601@users.noreply.github.com>
2022-05-24 15:35:04 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -5,7 +5,9 @@ int main (int argc, char* argv[] ){
    * Input one double value followed by a newline
    * Output one double value with two decimal places on a line by itself - no newline.
    * Do not print any extra strings/prompts */
-  
-  
+  double inches = 0,cms=0;
+  scanf("%lf",&inches);
+  cms=2.54*inches;
+  printf("%.2f",cms);
   return 0;
 }
