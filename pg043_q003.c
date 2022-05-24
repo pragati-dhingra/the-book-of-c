92a284cd82e1cc8a1c5e2127d556753abeb6071e
chetan-a41 <105716412+chetan-a41@users.noreply.github.com>
2022-05-24 15:34:35 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -4,18 +4,10 @@ int main (int argc, char* argv[] ){
   /* Q: Read value in inches and converts it into cms. 1 inch = 2.54 cms 
    * Input one double value followed by a newline
    * Output one double value with two decimal places on a line by itself - no newline.
-   * Do not print any extra strings/prompts * 
-   int i, sum=0;
-   printf("argc=%d\n",argc);
-  printf("Let's see what is in argv[]\n");
-  if(argc>1)
-  {
-  for(i=1; i<argc; i++)
-  {
-    printf("argv[%d] = %s\n",i,argv[i]);
-    sum+=atoi(argv[i]);       
-           }
-    printf("total=%d\n",sum);
-           }
-  return 0;
-}
+   * Do not print any extra strings/prompts */ 
+   double inches = 0,cms=0;
+  scanf("%1f" ,&inches);
+  cms=2.54*inches;
+  printf(%.2f",cms);
+         return 0;
+         }
