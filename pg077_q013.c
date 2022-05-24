bf4829474a6c747c6027f901ea512497d69f12de
shubhanshu-a32 <105715709+shubhanshu-a32@users.noreply.github.com>
2022-05-24 16:24:25 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -9,40 +9,40 @@ int main (int argc, char* argv[] ){
   Output is case sensitive */
   int n=0;
   scanf("%d",&n);
-  if(n=1)
+  if(n==1)
   {printf("January");
   }
-  else if(n=2)
+  else if(n==2)
   {printf("February");
   }
-  else if(n=3)
+  else if(n==3)
   {printf("March");
   } 
-  else if(n=4)
+  else if(n==4)
   {printf("April");
   }
-  else if(n=5)
+  else if(n==5)
   {printf("May");
   }
-  else if(n=6)
+  else if(n==6)
   {printf("June");
   }
-  else if(n=7)
+  else if(n==7)
   {printf("July");
   }
-  else if(n=8)
+  else if(n==8)
   {printf("August");
   }
-  else if(n=9)
+  else if(n==9)
   {printf("September");
   }
-  else if(n=10)
+  else if(n==10)
   {printf("October");
   }
-  else if(n=11)
+  else if(n==11)
   {printf("November");
   }
-  else if(n=12)
+  else if(n==12)
   {printf("December");
   }
   
