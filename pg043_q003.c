7b61e799126a82f4e4f4989b967552365bea401a
umang-52 <105800957+umang-52@users.noreply.github.com>
2022-05-24 15:35:46 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -6,6 +6,9 @@ int main (int argc, char* argv[] ){
    * Output one double value with two decimal places on a line by itself - no newline.
    * Do not print any extra strings/prompts */
   
-  
+  ddouble inches =0, cms=0;
+  scanf("%if",&inches);
+  cms=2.54*inches;
+  printf("%.2f",cms);
   return 0;
 }
