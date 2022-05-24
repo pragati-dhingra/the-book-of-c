aa98ab348a49abc38072dcfeb25bfe1a648ac292
shivamdhama768 <105715884+shivamdhama768@users.noreply.github.com>
2022-05-24 16:15:23 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -7,6 +7,34 @@ int main (int argc, char* argv[] ){
   Please do not use abbreviations. First character is capital, rest are small case,  no trailing newline
   In case of invalid input, output should be "Invalid" (without quotes) 
   Output is case sensitive */
-  
-  return 0
+  int month=0;
+    scanf("%d",&month);
+    if(month==1){
+      printf("january");
+    }else if(month==2){
+      printf("february");
+    }else if(month==3){
+      printf("march");
+     }else if(month==4){
+      printf("april");
+       }else if(month==5){
+      printf("may");
+    }else if(month==6){
+      printf("june");
+       }else if(month==7){
+      printf("july");
+       }else if(month==8){
+      printf("august");
+       }else if(month==9){
+      printf("september");
+       }else if(month==10){
+      printf("october");
+       }else if(month==11){
+      printf("november");
+       }else if(month==12){
+      printf("december");
+    }else if(month<1||month>12) {
+      printf("invalid");
+    }
+  return 0;
 }
