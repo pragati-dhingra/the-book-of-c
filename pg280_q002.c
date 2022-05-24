36512e7fe49407b2b96cab55e79338ee583eba13
MSSonu_KEC <87904903+MSSonu@users.noreply.github.com>
2022-05-24 20:12:21 +0530

    Update pg280_q002.c

--- pg280_q002.c
+++ pg280_q002.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+ 
+/* We need an additional parameter to determine number of elements to process.
+ * Same as we needed for array processing */
+/* Return type of compute_average should be float or double and not int */
 double compute_average(int *, int);
 void read_score(int *, int);
  
-int main (int argc, char* argv[] ){
-    /* Write a program that:
-    * Reads an int n from the user - this is the number of input values user wishes to enter
-    * If value of n < 1 then print "Invalid input" without quotes and trailing newline and exit the program
-    * Allocates memory for storing n ints
-    * Calls a function to read in n int values, one value on each line
-    * Calls a function that computes average of these n values and returns the average
-    * Calling function prints the average value with no trailing spaces or newline. Printed average should have 2 decimal places */
-
-    int *score = NULL, num_s = 0;
-    double avg = 0;
+int main (int argc, char * argv[]) {
+    int *score = NULL, num_students = 0;
+    double average = 0;
  
     printf ("Please enter number of students: ");
-    scanf("%d",&num_s);
-    if (num_s <= 0) {
+    scanf("%d",&num_students);
+    if (num_students <= 0) {
         printf ("Invalid input. Terminating\n");
         return 1;
     }
  
-    score = malloc (sizeof(int) * num_s);
+    score = malloc (sizeof(int) * num_students);
     if (NULL == score) {
         printf ("Failed to allocate memory. Terminating\n");
         return 1;
     }
  
-    read_score(score, num_s);
-    average = compute_average(score, num_s);
+    read_score(score, num_students);
+    average = compute_average(score, num_students);
     printf("Average score of the class is %f\n", average);
  
     free(score);
@@ -49,4 +46,3 @@ void read_score(int *score, int num) {
         scanf("%d", &score[i]);
     }
 }
-
