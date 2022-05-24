24773679f8f93231a6c7ddbb868985e3af04807d
sahdev-a1 <105715947+sahdev-a1@users.noreply.github.com>
2022-05-24 21:09:01 +0530

    Update pg081_q015.c

--- pg081_q015.c
+++ pg081_q015.c
@@ -10,9 +10,9 @@ int main (int argc, char* argv[] ){
           In case of invalid input, print -1 as output */
   int month,day;
   scanf("%d %d",&month,&day);
-  if(month==1 && month==3 && month==5 && month==7 && month==8 && month==10 && month==12)
+  if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12)
   {
-     if(day==5 && day==6 && day==7)
+     if(day==5 || day==6 || day==7)
      {
        printf("5");
      }
@@ -21,9 +21,9 @@ int main (int argc, char* argv[] ){
        printf("4");
      }
   }
-  else if(month==4 && month==6 && month==9 && month==11)
+  else if(month==4 || month==6 || month==9 || month==11)
   {
-     if(day==6 && day==7)
+     if(day==6 || day==7)
      {
        printf("5");
      }
@@ -35,6 +35,10 @@ int main (int argc, char* argv[] ){
   else if(month==2)
   {
     printf("4");
+  }
+  else
+  {
+    printf("-1");
   }
   return 0;
 }
