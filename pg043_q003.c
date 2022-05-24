22517f374e46379a3041383ed1173a93fecb6bee
Aryan-AS <89517035+Aryan-AS@users.noreply.github.com>
2022-05-24 15:31:07 +0530

    Update pg043_q003.c

--- pg043_q003.c
+++ pg043_q003.c
@@ -6,6 +6,12 @@ int main (int argc, char* argv[] ){
    * Output one double value with two decimal places on a line by itself - no newline.
    * Do not print any extra strings/prompts */
   
+  double inches, cms;
+  scanf("%lf",&inches);
+  cms = 2.4/inches;
+  printf("The value in cms is %lf", cms);
+  
+  
   
   return 0;
 }
