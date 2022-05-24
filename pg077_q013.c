d4d5b6030a9a87416a3ef2ac4c27339a72a9c15e
shivamdhama768 <105715884+shivamdhama768@users.noreply.github.com>
2022-05-24 16:48:23 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -7,34 +7,34 @@ int main (int argc, char* argv[] ){
   Please do not use abbreviations. First character is capital, rest are small case,  no trailing newline
   In case of invalid input, output should be "Invalid" (without quotes) 
   Output is case sensitive */
-  int month=0
-    scanf("%d",&month);
-    if(month==1){
-      printf("january");
-    }else if(month==2){
-      printf("february");
-    }else if(month==3){
-      printf("march");
-     }else if(month==4){
-      printf("april");
-       }else if(month==5){
-      printf("may");
-    }else if(month==6){
-      printf("june");
-       }else if(month==7){
-      printf("july");
-       }else if(month==8){
-      printf("august");
-       }else if(month==9){
-      printf("september");
-       }else if(month==10){
-      printf("october");
-       }else if(month==11){
-      printf("november");
-       }else if(month==12){
-      printf("december");
-    }else if(month<1||month>12) {
-      printf("invalid");
-    }
+  int month=0;
+  scanf("%d",&month);
+  if(month == 1){
+  printf("January");
+  }else if(month == 2){
+  printf("February");
+  }else if(month == 3){
+  printf("March");
+  }else if(month == 4){
+  printf("April");
+  }else if(month == 5){
+  printf("May");
+  }else if(month == 6){
+  printf("June");
+  }else if(month == 7){
+  printf("July");
+  }else if(month == 8){
+  printf("August");
+  }else if(month == 9){
+  printf("September");
+  }else if(month == 10){
+  printf("October");
+  }else if(month == 11){
+  printf("November");
+  }else if(month == 12){
+  printf("December");
+  }else {
+  printf("invalid");
+  }
   return 0;
 }
