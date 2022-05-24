33854c01ae9d4ccc6705ad93fdc0abf66e36522b
umashankar-a13 <105715812+umashankar-a13@users.noreply.github.com>
2022-05-24 04:01:57 -0700

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[] ){
-  /* Write a program that reads the month number (1-12) and prints its equivalent name (January for 1, February for 2, and so on).
+  /* Write a program that reads the month number (1-12) andprints its equivalent name (January for 1, February for 2, and so on).
   Input: One integer on a line, e.g. 4
   Output: Correponding name of month, e.g. April
   Please do not use abbreviations. First character is capital, rest are small case,  no trailing newline
