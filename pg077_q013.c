03d8b791d430bd0bac3880f74e7f0322125410eb
shubhanshu-a32 <105715709+shubhanshu-a32@users.noreply.github.com>
2022-05-24 16:34:23 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -9,41 +9,53 @@ int main (int argc, char* argv[] ){
   Output is case sensitive */
   int n=0;
   scanf("%d",&n);
-  if(n==1)
-  {printf("January");
+  if(n=1)
+  {
+    printf("January");
   }
-  else if(n==2)
-  {printf("February");
+  else if(n=2)
+  {
+    printf("February");
   }
-  else if(n==3)
-  {printf("March");
+  else if(n=3)
+  {
+    printf("March");
   } 
-  else if(n==4)
-  {printf("April");
+  else if(n=4)
+  {
+    printf("April");
   }
-  else if(n==5)
-  {printf("May");
+  else if(n=5)
+  {
+    printf("May");
   }
-  else if(n==6)
-  {printf("June");
+  else if(n=6)
+  {
+    printf("June");
   }
-  else if(n==7)
-  {printf("July");
+  else if(n=7)
+  {
+    printf("July");
   }
-  else if(n==8)
-  {printf("August");
+  else if(n=8)
+  {
+    printf("August");
   }
-  else if(n==9)
-  {printf("September");
+  else if(n=9)
+  {
+    printf("September");
   }
-  else if(n==10)
-  {printf("October");
+  else if(n=10)
+  {
+    printf("October");
   }
-  else if(n==11)
-  {printf("November");
+  else if(n=11)
+  {
+    printf("November");
   }
-  else if(n==12)
-  {printf("December");
+  else if(n=12)
+  {
+    printf("December");
   }
   
     return 0;
