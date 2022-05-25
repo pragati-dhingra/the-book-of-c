849c4b2dbd12d6f223852ef808db1cae79d97ec9
aayush-a27 <105729140+aayush-a27@users.noreply.github.com>
2022-05-25 09:47:40 +0530

    Update pg095_q002.c

--- pg095_q002.c
+++ pg095_q002.c
@@ -9,6 +9,30 @@ int main (int argc, char* argv[] ){
   *         Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Incorrect input 
   * Note that output is case sensitive. There should be no newline at the end of input. 
   * For example, if input was 1, output should be simply Monday */
+  switch(week)
+        case 1: 
+            printf("Monday");
+            break;
+        case 2: 
+            printf("Tuesday");
+            break;
+        case 3: 
+            printf("Wednesday");
+            break;
+        case 4: 
+            printf("Thursday");
+            break;
+        case 5: 
+            printf("Friday");
+            break;
+        case 6: 
+            printf("Saturday");
+            break;
+        case 7: 
+            printf("Sunday");
+            break;
+        default: 
+            printf("Invalid input! Please enter week number between 1-7.");
   
   return 0
 }
