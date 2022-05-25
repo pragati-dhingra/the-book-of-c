aee23e2b3b8d99885afe729147cc82caa37f1c1a
rahul-08-10 <105716094+rahul-08-10@users.noreply.github.com>
2022-05-25 11:02:52 +0530

    Update pg081_q015.c

--- pg081_q015.c
+++ pg081_q015.c
@@ -8,6 +8,25 @@ int main (int argc, char* argv[] ){
           Second integer is day of week, 1 for Monday, 2 for Tuesday ...
   Output: One integer showing number of sundays in that month. No newline to be printed
           In case of invalid input, print -1 as output */
+	int a;
+	scanf("%d",&a);
+	switch(a){
+		case 1: printf("Monday");
+		break;
+		case 2:printf("Tuesday");
+		break;
+		case 3:printf("Wednesday");
+		break;
+		case 4:printf("Thrusday");
+		break;
+		case 5:printf("Friday");
+		break;
+		case 6:printf("Saturday");
+		break;
+		case 7:printf("Sunday");
+		break;		
+	}
+}
   
-  return 0
+  return 0;
 }
