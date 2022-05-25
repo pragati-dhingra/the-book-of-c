c1897fad445c8bed8e50097dc817f136f4c443b7
Amishi-d16 <105700136+Amishi-d16@users.noreply.github.com>
2022-05-25 20:46:23 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
-
 int main (int argc, char* argv[] ){
-  /* Write a program that reads the month number (1-12) and prints its equivalent name (January for 1, February for 2, and so on).
-  Input: One integer on a line, e.g. 4
-  Output: Correponding name of month, e.g. April
-  Please do not use abbreviations. First character is capital, rest are small case,  no trailing newline
-  In case of invalid input, output should be "Invalid" (without quotes) 
-  Output is case sensitive */
   	  int month = 0;
 	    scanf("%d", &month);
 	     if(month < 1 || month > 12) {
