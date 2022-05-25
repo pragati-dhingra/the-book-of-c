a39e2d702723d5a9ca57d7d1cd433719996dfa2a
SilkiKharaliya <101621747+SilkiKharaliya@users.noreply.github.com>
2022-05-25 09:42:25 +0530

    Update pg257_q021.c

--- pg257_q021.c
+++ pg257_q021.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 int main (int argc, char* argv[] ){
     /* Write a program that reads in an int n where n<30. This is the number of input values.
     * Followed by n float inputs, one on each line 
@@ -15,5 +16,34 @@ int main (int argc, char* argv[] ){
     * 44.53
     * Error checking value of n is not expected. 0 < n < 30 
     */
-    return 0;
+    float arr[30]={0.0};
+int n=0,i=0,x=0;
+float sum=0.0,avg=0.0;
+scanf("%d",&n);
+if(n>0 && n<30){
+for(i=0;i<n;i++)
+{
+scanf("%f",&arr[i]);
+}
+}
+else{
+printf("Invalid input");
+}
+if(n<7){
+printf("%.2f",avg);
+}
+for(i=n-1;i>=0;i--){
+sum=sum+arr[i];
+x++;
+if(x==7){
+avg=sum/7;
+printf("%.2f",avg);
+}
+else{
+continue;
+}
+}
+avg=sum/n;
+printf("%.2f",avg);
+ return 0;
 }
