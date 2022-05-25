df477b029e6adeb77dc45078d55e7a664ee4f4d7
Vaibhav Kumar <105716162+vaibhavkumar-a05@users.noreply.github.com>
2022-05-25 09:19:19 +0530

    Update pg077_q013.c

--- pg077_q013.c
+++ pg077_q013.c
@@ -9,55 +9,38 @@ int main (int argc, char* argv[] ){
   Output is case sensitive */
   int month = 0;
   scanf("%d",&month);
-  if(month == 1)
-  {
-    printf("January");
-  }
-  else if(month == 2)
-  {
+  if(month == 1){
+  printf("January");
+  }else if(month == 2){
     printf("February");
-  }
-  else if(month == 3)
-  {
+  }else if(month == 3){
     printf("March");
-  }
-  else if(month == 4)
-  {
+  }else if(month == 4){
     printf("April");
-  }
-  else if(month == 5)
-  {
+  }else if(month == 5){
     printf("May");
-  }
-  else if(month == 6)
-  {
+  }else if(month == 6){
     printf("June");
-  }
-  else if(month == 7)
-  {
+  }else if(month == 7){
     printf("July");
-  }
-  else if(month == 8)
-  {
+  
+  }else if(month == 8){
+
     printf("August");
-  }
-  else if(month == 9)
-  {
+ 
+  }else if(month == 9){
+  
     printf("September");
-  }
-  else if(month == 10)
-  {
+  }else if(month == 10){
+  
     printf("October");
-  }
-   else if(month == 11)
-  {
+  }else if(month == 11){
+  
     printf("November");
-  }
-  else if(month == 12)
-  {
+  }else if(month == 12){
+  
     printf("December");
-  }
-  else (month < 1 || month > 12)
+  }else (month < 1 || month > 12)
   {
     printf("Invalid");
   }
