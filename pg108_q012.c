3c3c65bc6da9730d7083038627c3ddc5fcf4b43c
sahdev-a1 <105715947+sahdev-a1@users.noreply.github.com>
2022-05-24 22:11:06 -0700

    Update pg108_q012.c

--- pg108_q012.c
+++ pg108_q012.c
@@ -6,7 +6,7 @@ int main(int argc, char * argv[]) {
     /* Add checks for both percent and average to be in 0 to 100 range, both inclusive 
      * If found out of range then print "Invalid input" and skip further processing */
     if((percent>=0 && average>=0)&&(percent<=100 && average<=100))
-    {
+    { 
         if (percent > average) {
             printf("Qualify");
          } else {
