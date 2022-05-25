2f806a0924f4193e56aac57889d874cb5825205c
Singhal-Shaili <95026201+Singhal-Shaili@users.noreply.github.com>
2022-05-25 10:31:35 +0530

    Update pg257_q018.c

--- pg257_q018.c
+++ pg257_q018.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
-
+int add_array(int arr[],int size);
 int main (int argc, char* argv[] ){
-    /* Write a program that reads in five integers in an array, one integer per line
-    * Defines a function add_array that adds all ints of the array using a countdown loop
-    * For example, input:
-    * 3
-    * 7
-    * 10
-    * 15
-    * 3
-    * Output: 
-    * "38" without quotes - no trailing space or newline 
-    */
-  return 0;
+ /* Write a program that reads in five integers in an array, one integer
+per line
+ * Defines a function add_array that adds all ints of the array using a
+countdown loop
+ * For example, input:
+ * 3
+ * 7
+ * 10
+ * 15
+ * 3
+ * Output:
+ * "38" without quotes - no trailing space or newline
+ */
+ int arr[5]={0};
+int i=0;
+for(i=0;i<5;i++)
+{
+scanf("%d",&arr[i]);
+}
+printf("%d",add_array(arr,5));
+return 0;
+}
+int add_array(int arr[],int size)
+{
+int i=0,sum=0;
+for(i=size;i>=0;i--)
+{
+sum=sum+arr[i];
+}
+return sum;
 }
