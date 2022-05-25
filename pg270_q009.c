ed04f6cb51a64b06c4a7213af13df8e98b07ebba
rashmisharma3011 <101451636+rashmisharma3011@users.noreply.github.com>
2022-05-25 10:31:35 +0530

    Update pg270_q009.c

--- pg270_q009.c
+++ pg270_q009.c
@@ -15,5 +15,39 @@ int main (int argc, char* argv[] ){
     * If longer string is presented as input, use only first 29 chars
     * Include error checks for the value of n. It should be >0 and <30 
     * If value of n is outside this range then print output value as -2 instead of above values */
+ int n=7;
+    const char str1[30]={'\0'};
+    const char str2[30]={'\0'};
+    int x=-2;
+    
+    scanf("%d",&n);  
+    
+    scanf(" %30[^\n]s",str1);
+    scanf(" %30[^\n]s",str2);
+    
+    
+    if(n>0 && n<30){
+    	
+    	printf("%d",my_strncmp(str1,str2,n));
+	}else{
+		printf("%d",x);
+	}
+       
     return 0;
+}
+int my_strncmp (const char * s1, const char * s2, int n) {
+    int count = 0;
+    while ( (*s1 != '\0') && (*s1 == *s2) && count != n-1) {
+        s1++;
+        s2++;
+        count++;
+    }
+    if (*s1-*s2==0){
+    	return 0;
+	}else if(*s1>*s2){
+		return 1;
+	}else{
+		return -1;
+	}		
+	
 }
