2770e086d600cdaa0f0fee25937e121dfa4cfce5
ShivaliTyagi <101444334+ShivaliTyagi@users.noreply.github.com>
2022-05-27 10:35:19 +0530

    Update pg357_q004.c

--- pg357_q004.c
+++ pg357_q004.c
@@ -1,8 +1,38 @@
+#include<stdio.h>
+
+unsigned long int factorial(unsigned long n) 
+{
+    if(1 == n) {
+        return 1;
+    }
+    return n * factorial(n-1);
+}
+
 int main (int argc, char* argv[] ){
     /* Write a recursive program that:
     * Reads an int as input, on a line by itself
     * And prints the factorial of that number as output 
     * If input was 0 or negative number then 0 should be printed
     * Output is printed on a line by itself with no trailing whitespace or newline */
+    
+    unsigned int num = 0;
+    unsigned long result = 0;
+
+    printf("Enter a positive number: ");
+    scanf("%u", &num);
+
+    if(num == 0 ) {
+        printf("0");
+    } 
+    if(num <1){
+          printf("0");
+    }
+    if(num>1)
+        {
+
+        result = factorial(num);
+    }
+    printf("%lu",result);
+    
     return 0;
 }
