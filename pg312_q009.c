64109d95f8b89c6160112a942e8ca023ca72d1c2
shalini311 <58061627+shalini311@users.noreply.github.com>
2022-05-27 20:09:04 +0530

    Update pg312_q009.c

--- pg312_q009.c
+++ pg312_q009.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include<math.h>
 
-int main (int argc, char* argv[] ){
+struct point {
+    int x,y; 
+};
+ 
+struct square {
+    struct point vertices[4];
+};
+ 
+double square_edge (struct square sq) {
+    double x_distance = 0, y_distance = 0, edge1 = 0,edge2 =0,edge3=0,edge4=0;
+    x_distance = (sq.vertices[0].x - sq.vertices[1].x) * (sq.vertices[0].x - sq.vertices[1].x);
+    y_distance = (sq.vertices[0].y - sq.vertices[1].y) * (sq.vertices[0].y - sq.vertices[1].y);
+    edge1 = sqrt(x_distance + y_distance);
+    
+    x_distance = (sq.vertices[1].x - sq.vertices[2].x) * (sq.vertices[1].x - sq.vertices[2].x);
+    y_distance = (sq.vertices[1].y - sq.vertices[2].y) * (sq.vertices[1].y - sq.vertices[2].y);
+    edge2 = sqrt(x_distance + y_distance);
+    
+    
+    x_distance = (sq.vertices[2].x - sq.vertices[3].x) * (sq.vertices[2].x - sq.vertices[3].x);
+    y_distance = (sq.vertices[2].y - sq.vertices[3].y) * (sq.vertices[2].y - sq.vertices[3].y);
+    edge3 = sqrt(x_distance + y_distance);
+    
+    x_distance = (sq.vertices[3].x - sq.vertices[0].x) * (sq.vertices[3].x - sq.vertices[0].x);
+    y_distance = (sq.vertices[3].y - sq.vertices[0].y) * (sq.vertices[3].y - sq.vertices[0].y);
+    edge4 = sqrt(x_distance + y_distance);
+    
+    if((edge1 ==edge2)&&(edge2==edge3)&&(edge3==edge4)&&(edge4==edge1))
+    {
+        return edge1;
+    }
+    else
+    {
+                 return -1;
+    }
+    
+}
+ 
+double square_perimeter (struct square sq) {
+    return (4 * square_edge(sq));
+}
+ 
+double square_area (struct square sq) {
+    double edge = square_edge(sq);
+    return (edge * edge);
+}
+
+int main()
+{
     /* Write a program that:
     * Defines a struct point that has x and y coordinates as its members
     * Defines a struct square that stores 4 vertices of the square
@@ -13,5 +62,26 @@ int main (int argc, char* argv[] ){
     * Perimeter and area should have two decimal points
     * There should be no trailing whitespace or newline on the output line
     */
+    
+     struct square sqr = { 0 };
+     double edge =0.0;
+     
+    for(int i =0; i<4; i++) {
+                scanf("%d", &sqr.vertices[i].x);
+                scanf("%d", &sqr.vertices[i].y);
+    }
+    
+    edge = square_edge(sqr);
+    if(edge == -1)
+    {
+        printf("Invalid input");
+        return 0;
+    }
+    else
+    {
+    printf("%.2f", square_perimeter(sqr));
+    printf(" %.2f", square_area(sqr));
+    }
     return 0;
+    
 }
