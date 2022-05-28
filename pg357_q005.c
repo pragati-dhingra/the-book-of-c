e24088ef3e0b747b8a3413c678bab188034636ec
Divya-021 <90896647+Divya-021@users.noreply.github.com>
2022-05-28 15:34:07 +0530

    Update pg357_q005.c

--- pg357_q005.c
+++ pg357_q005.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
-unsigned long int fibonacci(unsigned long n) {
-   
-    if(n== 0) {
-        return 0;
-    }
-    if(n == 1) {
-        return 1;
-    }
-    return (fibonacci(n-1) + fibonacci(n-2));
-}
+int F(int);
 int main (int argc, char* argv[] ){
     /* Write a recursive program that:
     * Reads an int n as input, on a line by itself
@@ -16,16 +7,21 @@ int main (int argc, char* argv[] ){
     * If input was negative number then 0 should be printed
     * F(0) = 0, F(1) = 1
     * Output is on a line by itself with no trailing whitespace or newline */
-    unsigned int n = 0;
-    unsigned long result = 0;
- 	int x=0;
-        scanf("%u", &n);
- if(n<0)
-     printf("%d",x);
-    else{
-	
-    	result = fibonacci(n);
-    	printf("F(%u) = %lu\n", n, result);
-    }    
+     int n = 0;
+    
+    scanf("%d", &n);
+    fflush(stdin);
+    
+    n=F(n);
+    printf("%d", n);
     return 0;
+}
+int F(int n){
+   if(n <= 0){
+      return 0;
+   } else if(n == 1) {
+      return 1;
+   } else {
+      return (F(n-1) + F(n-2));
+   }
 }
