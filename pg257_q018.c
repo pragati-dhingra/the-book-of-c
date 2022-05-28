568291c2779ae92c985db83705c477fada863e56
Priya-20961 <86092853+Priya-20961@users.noreply.github.com>
2022-05-28 16:19:32 +0530

    Update pg257_q018.c

--- pg257_q018.c
+++ pg257_q018.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+int sum_array(int arr[], int size);
+
 
 int main (int argc, char* argv[] ){
     /* Write a program that reads in five integers in an array, one integer per line
@@ -13,4 +15,21 @@ int main (int argc, char* argv[] ){
     * "38" without quotes - no trailing space or newline 
     */
   return 0;
+int main (int argc, char* argv[]) {
+    int arr[4] = { 0 },i=0;
+    int sum=0;
+    for(i=0; i<5; i++) {
+    scanf("%d",&arr[i]);
+    }
+    sum = sum_array(arr, 4);
+    printf("%d",sum);
+}
+
+int sum_array(int * arr, int size) {
+    int result = 0, i = 0;
+
+    for(i=size; i>=0; i--,arr++) {
+        result += *arr;
+    }
+    return result;
 }
