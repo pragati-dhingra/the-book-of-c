f8c1f84e3c4afb5690cd0ab48822661a06563953
Vaishalikec <101563802+Vaishalikec@users.noreply.github.com>
2022-05-31 12:08:31 +0530

    Update pg312_q009.c

--- pg312_q009.c
+++ pg312_q009.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
- 
+#include<stdlib.h>
 struct point {
     int x,y; 
 };
@@ -16,7 +16,20 @@ double square_edge (struct square sq) {
     edge = sqrt(x_distance + y_distance);
     return edge;
 }
- 
+
+double valid_check (struct square sq) {
+    double x_distance = 0, y_distance = 0, edge1 = 0,edge2=0;
+    x_distance = (sq.vertices[0].x - sq.vertices[1].x) * (sq.vertices[0].x - sq.vertices[1].x);
+    y_distance = (sq.vertices[0].y - sq.vertices[1].y) * (sq.vertices[0].y - sq.vertices[1].y);
+    edge1 = sqrt(x_distance + y_distance);
+    x_distance = (sq.vertices[1].x - sq.vertices[2].x) * (sq.vertices[1].x - sq.vertices[2].x);
+    y_distance = (sq.vertices[1].y - sq.vertices[2].y) * (sq.vertices[1].y - sq.vertices[2].y);
+    edge2 = sqrt(x_distance + y_distance);
+    if(edge1==edge2)
+    	return 1;
+    else
+    	return 0;
+}
 double square_perimeter (struct square sq) {
     return (4 * square_edge(sq));
 }
@@ -29,16 +42,21 @@ double square_area (struct square sq) {
 int main (int argc, char *argv[]) {
  
     struct square sqr = { 0 };
-    printf("Enter vertices of square, clockwise\n");
- 
+     int x=0;
     for(int i =0; i<4; i++) {
-        printf("Enter x coordinate of vertex %d: ",i+1);
+    
         scanf("%d", &sqr.vertices[i].x);
-        printf("Enter y coordinate of vertex %d: ",i+1);
+    
         scanf("%d", &sqr.vertices[i].y);
     }
- 
-    printf("Perimeter of square is %f\n", square_perimeter(sqr));
-    printf("Area of square is %f\n", square_area(sqr));
+	x=valid_check(sqr);
+	if(x==1)
+	{
+	    printf("%.2f", square_perimeter(sqr));
+    	printf("%.2f", square_area(sqr));}
+    else{
+    	printf("Invalid input\n");
+		exit(0);
+	}
     return 0;
 }
