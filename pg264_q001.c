e28937141b5005aee33587c0d048e5ff63963a4b
ILAKAUSHIK <70168212+ILAKAUSHIK@users.noreply.github.com>
2022-06-02 20:21:47 +0530

    Update pg264_q001.c

--- pg264_q001.c
+++ pg264_q001.c
@@ -10,5 +10,16 @@ int main (int argc, char* argv[] ){
     * I AM LEVEL 7 NINJA
     * Assume string length would be <128
     */
+ char str[128] = { '\0' };
+ 
+    scanf("%127[^\n]s",str);
+ 
+    for(int i=0; i<127 && str[i]; i++) {
+        if(str[i]>='a' && str[i]<='z') {
+            printf("%c",str[i]-'a'+'A');
+        } else {
+            printf("%c",str[i]);
+        }
+    }
   return 0;
 }
