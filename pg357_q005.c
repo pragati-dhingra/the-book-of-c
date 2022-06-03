f1a82210ff30145c8c7f8590f7556862a92daa94
umangrastogig <101452375+umangrastogig@users.noreply.github.com>
2022-06-03 18:57:57 +0530

    Update pg357_q005.c

--- pg357_q005.c
+++ pg357_q005.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+int F(int);
 int main (int argc, char* argv[] ){
     /* Write a recursive program that:
     * Reads an int n as input, on a line by itself
@@ -7,5 +7,21 @@ int main (int argc, char* argv[] ){
     * If input was negative number then 0 should be printed
     * F(0) = 0, F(1) = 1
     * Output is on a line by itself with no trailing whitespace or newline */
+     int n = 0;
+    
+    scanf("%d", &n);
+    fflush(stdin);
+    
+    n=F(n);
+    printf("%d", n);
     return 0;
+}
+int F(int n){
+   if(n <= 0){
+      return 0;
+   } else if(n == 1) {
+      return 1;
+   } else {
+      return (F(n-1) + F(n-2));
+   }
 }
