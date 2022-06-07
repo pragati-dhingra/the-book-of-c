83831abed0735e808ea0a0c63b04a726a8b61b9b
puneet-a15 <105716145+puneet-a15@users.noreply.github.com>
2022-06-07 22:47:30 +0530

    Update pg105_q009.c

--- pg105_q009.c
+++ pg105_q009.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-
 int main (int argc, char* argv[] ){
-    long phone_memory = 8589934592;
-    long memory_used = 0;
-    double percent_used = 0;
-
-    scanf("%ld", &memory_used);
-    /* Add check: if user input was less than 0 or it was more than phone_memory then print "Invalid input" */
-    /* In case of invalid input, rest of processing should be skipped */
-    if (memory_used < 0||memory_used > phone_memory) {
+ long phone_memory = 8589934592;
+ long memory_used = 0;
+ double percent_used = 0;
+ scanf("%ld", &memory_used);
+ /* Add check: if user input was less than 0 or it was more than phone_memory then print "Invalid
+input" */
+ /* In case of invalid input, rest of processing should be skipped */
+ if (memory_used < 0||memory_used > phone_memory) {
  printf("Invalid input");
  }
  else
  {
-   percent_used = (memory_used*100.0)/phone_memory;
+ percent_used = (memory_used*100.0)/phone_memory;
  if (percent_used > 90) {
-        /* Print Out of memory message */
-        printf("OOM");
-    } else {
-        /* Print number of free bytes */
-        printf("%ld",phone_memory-memory_used);
-    }
-    return 0;
+ /* Print Out of memory message */
+ printf("OOM");
+ } else {
+ /* Print number of free bytes */
+ printf("%ld",phone_memory-memory_used);
+ }
+ }
+ return 0;
 }
