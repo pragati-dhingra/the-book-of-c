ac410e99bbeac6d70756d7e6332d480f14861df3
yash-a06 <105718562+yash-a06@users.noreply.github.com>
2022-06-08 15:53:44 +0530

    Update pg095_q002.c

--- pg095_q002.c
+++ pg095_q002.c
@@ -13,21 +13,21 @@ int day=0;
  scanf("%d",&day);
  switch(day)
  {
- case 1: printf("Monday");
+ case 1:printf("Monday");
  break;
- case 2: printf("Tuesday");
+ case 2:printf("Tuesday");
  break;
- case 3: printf("Wednesday");
+ case 3:printf("Wednesday");
  break;
- case 4: printf("Thursday");
+ case 4:printf("Thursday");
  break;
- case 5: printf("Friday");
+ case 5:printf("Friday");
  break;
- case 6: printf("Saturday");
+ case 6:printf("Saturday");
  break;
- case 7: printf("Sunday");
+ case 7:printf("Sunday");
  break; 
- default: printf("Incorrect input") 
+ default:printf("Incorrect input");
  }
   return 0;
 }
