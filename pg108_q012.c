09eab8171d5b267d6a9f16774d17cd20e8123d92
sahdev-a1 <105715947+sahdev-a1@users.noreply.github.com>
2022-06-08 09:22:24 +0530

    Update pg108_q012.c

--- pg108_q012.c
+++ pg108_q012.c
@@ -1,6 +1,7 @@
+#include<stdio.h>
 int main(int argc, char * argv[]) {
     float percent = 0, average = 0;
-    scanf("%f %f",&percent, &average);
+    scanf("%f %f",&percent, &average)
     
     /* Add checks for  both percent and average to be in 0 to 100 range, both inclusive 
     * If found out of range then print "Invalid input" and skip further processing */
