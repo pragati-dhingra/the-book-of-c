b4214a4e0d6fc41b1bcb07de06f99d15ab7c1f53
piyush-a44 <105715836+piyush-a44@users.noreply.github.com>
2022-06-16 21:03:37 +0530

    Update pg116_q022.c

--- pg116_q022.c
+++ pg116_q022.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
-  /* Write a calculator program using switch case 
-   * Input format --> operand operator operand
-   * E.g 
-   * 10 + 20
-   * 100 / 10
-   * Output --> result of computation with no trailing newline 
-   * No error checking is expected. Operands are int. Handle 4 arithmetic operators + - * /
-   * Only integer calculator is expected. Do not create float calculator */
-  
+int main() {
+
+  char op;
+  int first, second;
+  scanf("%d\t%c\t%d", &first, &op, &second);
+
+  switch (op) {
+    case '+':
+      printf("%d", first + second);
+      break;
+    case '-':
+      printf("%d",first - second);
+      break;
+    case '*':
+      printf("%d",first * second);
+      break;
+    case '/':
+      printf("%d",first / second);
+      break;
+  }
+
   return 0;
 }
+
