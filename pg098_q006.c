96295ec363c42cf6612af89d7ee8c7d723a15b93
piyush-a44 <105715836+piyush-a44@users.noreply.github.com>
2022-06-16 20:44:59 +0530

    Update pg098_q006.c

--- pg098_q006.c
+++ pg098_q006.c
@@ -1,13 +1,4 @@
 #include <stdio.h>
-
-int main (int argc, char* argv[] ){
-  /* Write a program using switch case that takes year as input and prints the name of cricket world cup winning country for that year, 
-  SUCH THAT there is ONLY ONE print statement for each winning country irrespective of number of times it has won the world cup
-a.	1975: West Indies
-b.	1979: West Indies
-c.	1983: India
-d.	1987: Australia
-e.	1992: Pakistan#include <stdio.h>
 int main (int argc, char* argv[] ) {
  /* Write a program using switch case that takes year as input and prints the name of cricket world 
 cup winning country for that year, 
@@ -56,17 +47,4 @@ break;
 printf("Invalid input");
 }
 return 0;
-}
-f.	1996: Sri Lanka
-g.	1999: Australia
-h.	2003: Australia
-i.	2007: Australia
-j.	2011: India
-k.	2015: Australia
-If world cup was not played in the year entered by the user, program should print "Invalid input"
-There should be no newlines after the printed output
-Winners post 2015 are not required.
-*/
-  
-  return 0
 }
