85c66810d3637b3109b78faf6f036eefa1cf9c50
piyush-a44 <105715836+piyush-a44@users.noreply.github.com>
2022-06-16 22:12:54 +0530

    Update pg149_q015.c

--- pg149_q015.c
+++ pg149_q015.c
@@ -4,10 +4,18 @@ int main() {
 
   int n, i=1,sum=0;
   scanf("%d",&n);
-  while(n/i!=0)
+  if(n!=0)
   {
+      while(n/i!=0)
+    {
       sum++;
       i=i*10;
+     }
+  }
+  else 
+  {
+      sum=1;
+    
   }
   printf("%d",sum);
   return 0;
