b8d9228abe91c7fb11d64c1000152274cc5b8217
piyush-a44 <105715836+piyush-a44@users.noreply.github.com>
2022-06-16 22:25:37 +0530

    Update pg152_q017.c

--- pg152_q017.c
+++ pg152_q017.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
-    /* Write a program to determine if a given input is a perfect square or not
-    * User input would be a single integer
-    * If user input was a perfect square then print Yes else print No on a line by itself with no trailing newline
-    * Output is case sensitive
-    * E.g. 
-    * 4
-    * Yes */
+int main() {
 
-    return 0;
+  int n,i;
+  scanf("%d",&n);
+ for(i=1;i<=n/2;i++)
+ {
+     if(i*i==n)
+     {
+         printf("Yes");
+         return 0;
+     }
+ }
+ printf("No");
+  return 0;
 }
