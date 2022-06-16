5de0058eedc80137d4d500796cb43eff57373177
piyush-a44 <105715836+piyush-a44@users.noreply.github.com>
2022-06-16 22:56:50 +0530

    Update pg156_q023.c

--- pg156_q023.c
+++ pg156_q023.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[] ){
-    /* Write a program to print all factors of a number
-    * input -> a positive integer
-    * Expected output, if number entered is less than 1 print "Invalid input" without quotes
-    * If valid input was received then print all factors of that number in increasing order on a single line separated by a single space
-    * E,g. input -> 7
-    * output -> "1 7 ", without quotes
-    * input -> 10
-    * output -> "1 2 5 10 ", without quotes
-    * Note that there is one space after each number */
+int main() {
 
-    return 0;
+  int n,i;
+  scanf("%d",&n);
+  if(n>=1)
+  {
+      for(i=1;i<=n;i++)
+      {
+          if(n%i==0)
+          {
+              printf("%d ",i);
+          }
+      }
+  }
+  else
+  {
+      printf("Invalid input");
+  }
+  return 0;
 }
