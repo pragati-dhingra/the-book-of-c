bfb8a3ffcbec6739790d4d54d7150cffb7373949
Samarpit Kandhari <105715819+samarpit-a28@users.noreply.github.com>
2022-06-17 10:46:11 +0530

    Update pg108_q012.c

--- pg108_q012.c
+++ pg108_q012.c
@@ -5,7 +5,7 @@ int main(int argc, char * argv[]) {
     
     /* Add checks for both percent and average to be in 0 to 100 range, both inclusive 
      * If found out of range then print "Invalid input" and skip further processing */
-    if(percent<0||percent>100 && average<0 ||average>100){
+    if(percent<0||percent>100) && (average<0 ||average>100){
         printf("Invalid input");
     }
     else{
